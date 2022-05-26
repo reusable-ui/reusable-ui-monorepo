401cc80cf10f530e6a6a98da6d32d039917a348d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-26 22:28:22 +0700

    updated layouts

--- packages/layouts/src/layouts.ts
+++ packages/layouts/src/layouts.ts
@@ -1,7 +1,47 @@
-'use strict';
+import {
+    // styles:
+    style,
+    imports,
+}                           from '@cssfn/cssfn'         // writes css in javascript
 
-module.exports = layouts;
 
-function layouts() {
-    // TODO
-}
+
+/**
+ * A dummy text content, for making parent's height as tall as current text line-height.  
+ * The dummy is also used for calibrating the flex's vertical position.
+ */
+export const fillTextLineHeightLayout = () => style({
+    // layouts:
+    content    : '"\xa0"',       // &nbsp;
+    display    : 'inline-block', // use inline-block, so we can kill the width
+    
+    
+    
+    // appearances:
+    overflow   : 'hidden',       // crop the text width (&nbsp;)
+    visibility : 'hidden',       // hide the element, but still consumes the dimension
+    
+    
+    
+    // sizes:
+    inlineSize : 0,              // kill the width, we just need the height
+});
+
+/**
+ * A dummy text content, for making parent's width as wide as current text line-height (rotated 90°).  
+ * The dummy is also used for calibrating the flex's vertical position.
+ */
+export const fillTextLineWidthLayout = () => style({
+    ...imports([
+        fillTextLineHeightLayout(),
+    ]),
+    ...style({
+        // layouts:
+        writingMode : 'vertical-lr', // rotate the element 90°
+        
+        
+        
+        // appearances:
+        overflow    : 'unset',       // fix Firefox bug
+    }),
+});
