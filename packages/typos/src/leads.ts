4c6a6313a3512ccf576cb842717c555d8f389f88
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-28 00:05:35 +0700

    updated typos

---
+++ packages/typos/src/leads.ts
@@ -0,0 +1,103 @@
+// cssfn:
+import {
+    // rules:
+    rule,
+    ifFirstChild,
+    ifLastChild,
+    
+    
+    
+    // scopes:
+    globalScope,
+    
+    
+    
+    // style sheets:
+    styleSheets,
+}                           from '@cssfn/cssfn'         // writes css in javascript
+import {
+    createCssConfig,
+    
+    
+    
+    // utilities:
+    usesCssProps,
+}                           from '@cssfn/css-config'    // reads/writes css variables configuration
+
+// internals:
+import type {
+    FontSize,
+    FontFamily,
+    FontWeight,
+    FontStyle,
+    TextDecoration,
+    LineHeight,
+    Foreground,
+    Margin,
+}                           from './types.js'
+import {
+    typos,
+}                           from './typos.js'
+import {
+    paragraphs as pargs,
+}                           from './paragraphs.js'
+
+
+
+//#region configs
+const [leads, leadValues, cssLeadConfig] = createCssConfig(() => {
+    return {
+        fontSize       : typos.fontSizeMd      as FontSize,
+        fontFamily     : pargs.fontFamily      as FontFamily,
+        fontWeight     : typos.fontWeightLight as FontWeight,
+        fontStyle      : pargs.fontStyle       as FontStyle,
+        textDecoration : pargs.textDecoration  as TextDecoration,
+        lineHeight     : pargs.lineHeight      as LineHeight,
+        
+        foreg          : pargs.foreg           as Foreground,
+        
+        margin         : pargs.margin          as Margin,
+    };
+}, { prefix: 'lead' });
+export {
+    leads,
+    leads as cssProps,
+    leads as default,
+}
+export {
+    leadValues,
+    leadValues as cssVals,
+}
+export {
+    cssLeadConfig,
+    cssLeadConfig as cssConfig,
+}
+//#endregion configs
+
+
+
+//#region style sheets
+styleSheets([
+    globalScope({
+        ...rule('.lead', {
+            // layouts:
+            display : 'block',
+            
+            
+            
+            // spacings:
+            ...ifFirstChild({
+                marginBlockStart : 0, // kill the top_margin at the first lead
+            }),
+            ...ifLastChild({
+                marginBlockEnd   : 0, // kill the bottom_margin at the last lead
+            }),
+            
+            
+            
+            // customize:
+            ...usesCssProps(leads),
+        }),
+    }),
+]);
+//#endregion style sheets
