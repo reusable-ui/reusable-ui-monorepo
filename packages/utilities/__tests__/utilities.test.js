63f6b2e5da5a824c320a778fe74513598534b543
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-30 13:58:14 +0700

    updated utilities

--- packages/utilities/__tests__/utilities.test.js
+++ packages/utilities/__tests__/utilities.test.js
@@ -1,3 +1,111 @@
-test('nothing to test', () => {
-    expect(true).toBe(true);
-})
+import {
+    parseNumber,
+} from '../dist/utilities.js'
+
+
+
+test('test parseNumber(undefined)', () => {
+    expect(
+        parseNumber(undefined)
+    )
+    .toBe(null);
+});
+test('test parseNumber(null)', () => {
+    expect(
+        parseNumber(null)
+    )
+    .toBe(null);
+});
+
+
+
+test('test parseNumber(number)', () => {
+    expect(
+        parseNumber(123)
+    )
+    .toBe(123);
+});
+test('test parseNumber(number)', () => {
+    expect(
+        parseNumber(-123)
+    )
+    .toBe(-123);
+});
+test('test parseNumber(number)', () => {
+    expect(
+        parseNumber(0)
+    )
+    .toBe(0);
+});
+test('test parseNumber(number)', () => {
+    expect(
+        parseNumber(NaN)
+    )
+    .toBe(null);
+});
+
+
+
+test('test parseNumber(string)', () => {
+    expect(
+        parseNumber('')
+    )
+    .toBe(null);
+});
+test('test parseNumber(string)', () => {
+    expect(
+        parseNumber('123')
+    )
+    .toBe(123);
+});
+test('test parseNumber(string)', () => {
+    expect(
+        parseNumber('-123')
+    )
+    .toBe(-123);
+});
+test('test parseNumber(string)', () => {
+    expect(
+        parseNumber('0')
+    )
+    .toBe(0);
+});
+test('test parseNumber(string)', () => {
+    expect(
+        parseNumber('meh')
+    )
+    .toBe(null);
+});
+
+
+
+test('test parseNumber([])', () => {
+    expect(
+        parseNumber([''])
+    )
+    .toBe(null);
+});
+test('test parseNumber([string])', () => {
+    expect(
+        parseNumber(['123'])
+    )
+    .toBe(123);
+});
+test('test parseNumber([string])', () => {
+    expect(
+        parseNumber(['-123'])
+    )
+    .toBe(-123);
+});
+test('test parseNumber([string])', () => {
+    expect(
+        parseNumber(['0'])
+    )
+    .toBe(0);
+});
+test('test parseNumber([string])', () => {
+    expect(
+        parseNumber(['meh'])
+    )
+    .toBe(null);
+});
--- packages/utilities/src/utilities.ts
+++ packages/utilities/src/utilities.ts
@@ -45,13 +45,15 @@ export const setRef = <TValue>(ref: React.Ref<TValue>|undefined, value: TValue|n
 
 const isSingleValue = (expression: string|ReadonlyArray<string>): expression is string => (typeof(expression) === 'string') || (Array.isArray(expression) && (expression.length === 1));
 export const parseNumber = (expression: number|string|ReadonlyArray<string>|null|undefined): number|null => {
-    if (typeof(expression) === 'number') return expression;
+    if (typeof(expression) === 'number') {
+        if (isNaN(expression)) return null;
+        return expression;
+    } // if
     if (!expression) return null;
     
     
     
     if (!isSingleValue(expression)) return null;
-    if (!expression) return null;
     
     
     
