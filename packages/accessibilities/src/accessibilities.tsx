a0ed01d52147b6d6787a4f84791c2abad0aa238f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-30 14:21:06 +0700

    updated accessibilities & validations

--- packages/accessibilities/src/accessibilities.tsx
+++ packages/accessibilities/src/accessibilities.tsx
@@ -2,7 +2,238 @@
 import {
     // react:
     default as React,
+    
+    
+    
+    // contexts:
+    createContext,
+    useContext,
 }                           from 'react'
 
 
 
+// defaults:
+const _defaultEnabled         = true;
+const _defaultReadOnly        = false;
+const _defaultActive          = false;
+
+const _defaultInheritEnabled  = true;
+const _defaultInheritReadOnly = true;
+const _defaultInheritActive   = false;
+
+
+
+// contexts:
+
+export interface TAccessibility<TDefaultEnabled = boolean, TDefaultReadOnly = boolean, TDefaultActive = boolean> {
+    enabled  : boolean|TDefaultEnabled
+    readOnly : boolean|TDefaultReadOnly
+    active   : boolean|TDefaultActive
+}
+
+/**
+ * Contains accessibility props.
+ */
+export interface Accessibility extends TAccessibility {
+    /**
+     * `true`      : component is enabled  - responses any user interaction.  
+     * `false`     : component is disabled - ignores any user interaction.
+     */
+    enabled  : boolean
+
+    /**
+     * `true`      : component is readOnly - ignores any user editing.  
+     * `false`     : component is editable - responses any user editing.
+     */
+    readOnly : boolean
+
+    /**
+     * `true`      : component is in active state.  
+     * `false`     : component is in normal state.
+     */
+    active   : boolean
+}
+
+/**
+ * A react context for accessibility stuff.
+ */
+export const Context = createContext<Accessibility>(/*defaultValue :*/{
+    enabled  : _defaultEnabled,
+    readOnly : _defaultReadOnly,
+    active   : _defaultActive,
+});
+Context.displayName  = 'Accessibility';
+
+
+
+// hooks:
+export const usePropAccessibility = <TDefaultEnabled extends unknown = boolean, TDefaultReadOnly = boolean, TDefaultActive = boolean>(props: AccessibilityProps, defaultEnabled: boolean|TDefaultEnabled = _defaultEnabled, defaultReadOnly: boolean|TDefaultReadOnly = _defaultReadOnly, defaultActive: boolean|TDefaultActive = _defaultActive): Accessibility|TAccessibility<TDefaultEnabled, TDefaultReadOnly, TDefaultActive> => {
+    // contexts:
+    const accessContext = useContext(Context);
+
+
+
+    return {
+        enabled: (
+            (
+                (props.inheritEnabled ?? _defaultInheritEnabled)
+                ?
+                accessContext.enabled  // inherit
+                :
+                true                   // independent
+            )
+            &&
+            (props.enabled ?? defaultEnabled)
+        ),
+        readOnly: (
+            (
+                (props.inheritReadOnly ?? _defaultInheritReadOnly)
+                ?
+                accessContext.readOnly // inherit
+                :
+                false                  // independent
+            )
+            ||
+            (props.readOnly ?? defaultReadOnly)
+        ),
+        active: (
+            (
+                (props.inheritActive ?? _defaultInheritActive)
+                ?
+                accessContext.active   // inherit
+                :
+                false                  // independent
+            )
+            ||
+            (props.active ?? defaultActive)
+        ),
+    };
+};
+
+export const usePropEnabled = <TDefaultEnabled extends unknown = boolean>(props: AccessibilityProps, defaultEnabled: boolean|TDefaultEnabled = _defaultEnabled): boolean|TDefaultEnabled => {
+    // contexts:
+    const accessContext = useContext(Context);
+
+
+
+    return (
+        (
+            (props.inheritEnabled ?? _defaultInheritEnabled)
+            ?
+            accessContext.enabled  // inherit
+            :
+            true                   // independent
+        )
+        &&
+        (props.enabled ?? defaultEnabled)
+    );
+};
+
+export const usePropReadOnly = <TDefaultReadOnly extends unknown = boolean>(props: AccessibilityProps, defaultReadOnly: boolean|TDefaultReadOnly = _defaultReadOnly): boolean|TDefaultReadOnly => {
+    // contexts:
+    const accessContext = useContext(Context);
+
+
+
+    return (
+        (
+            (props.inheritReadOnly ?? _defaultInheritReadOnly)
+            ?
+            accessContext.readOnly // inherit
+            :
+            false                  // independent
+        )
+        ||
+        (props.readOnly ?? defaultReadOnly)
+    );
+};
+
+export const usePropActive = <TDefaultActive extends unknown = boolean>(props: AccessibilityProps, defaultActive: boolean|TDefaultActive = _defaultActive): boolean|TDefaultActive => {
+    // contexts:
+    const accessContext = useContext(Context);
+
+
+
+    return (
+        (
+            (props.inheritActive ?? _defaultInheritActive)
+            ?
+            accessContext.active   // inherit
+            :
+            false                  // independent
+        )
+        ||
+        (props.active ?? defaultActive)
+    );
+};
+
+
+
+// react components:
+
+export interface AccessibilityProps extends Partial<Accessibility>
+{
+    /**
+     * `undefined` : same as `true`.  
+     * `true`      : component is enabled  - responses any user interaction.  
+     * `false`     : component is disabled - ignores any user interaction.
+     */
+    enabled?         : boolean
+
+    /**
+     * `undefined` : same as `true`.  
+     * `true`      : inherits `enabled` from parent (`AccessibilityProvider` context).  
+     * `false`     : independent `enabled`.
+     */
+    inheritEnabled?  : boolean
+
+
+
+    /**
+     * `undefined` : same as `false`.  
+     * `true`      : component is readOnly - ignores any user editing.  
+     * `false`     : component is editable - responses any user editing.
+     */
+    readOnly?        : boolean
+
+    /**
+     * `undefined` : same as `true`.  
+     * `true`      : inherits `readOnly` from parent (`AccessibilityProvider` context).  
+     * `false`     : independent `readOnly`.
+     */
+    inheritReadOnly? : boolean
+
+    
+    
+    /**
+     * `undefined` : same as `false`.  
+     * `true`      : component is in active state.  
+     * `false`     : component is in normal state.
+     */
+    active?          : boolean
+
+    /**
+     * `undefined` : same as `false`.  
+     * `true`      : inherits `active` from parent (`AccessibilityProvider` context).  
+     * `false`     : independent `active`.
+     */
+    inheritActive?   : boolean
+
+
+
+    // children:
+    children?        : React.ReactNode
+}
+export function AccessibilityProvider(props: AccessibilityProps) {
+    // fn props:
+    const propAccess = usePropAccessibility(props);
+    
+    
+    
+    return (
+        <Context.Provider value={propAccess}>
+            {props.children}
+        </Context.Provider>
+    );
+}
+export { AccessibilityProvider as default }
--- packages/validations/src/validations.tsx
+++ packages/validations/src/validations.tsx
@@ -2,7 +2,173 @@
 import {
     // react:
     default as React,
+    
+    
+    
+    // contexts:
+    createContext,
+    useContext,
 }                           from 'react'
 
 
 
+// defaults:
+const _defaultEnableRootValidation = false;
+const _defaultEnableValidation     = true;
+const _defaultIsValid              = undefined as (Result|undefined);
+
+const _defaultInheritValidation    = true;
+
+
+
+// validation results:
+
+/**
+ * Validation was skipped because its not required. Neither success nor error shown.
+ */
+export type Uncheck = null
+/**
+ * Validation was failed because the value did not meet the criteria.
+ */
+export type Error   = false
+/**
+ * Validation was successful and the value meet the criteria.
+ */
+export type Success = true
+export type Result  = Uncheck|Error|Success;
+
+
+
+// contexts:
+
+/**
+ * Contains validation props.
+ */
+export interface Validation {
+    /**
+     * `true`      : validation is enabled  - implements `isValid` prop.  
+     * `false`     : validation is disabled - equivalent as `isValid = null` (uncheck).
+     */
+    enableValidation  : boolean
+
+    /**
+     * `undefined` : *automatic* detect valid/invalid state.  
+     * `null`      : force validation state to *uncheck*.  
+     * `true`      : force validation state to *valid*.  
+     * `false`     : force validation state to *invalid*.
+     */
+    isValid?          : Result
+}
+interface ValidationRoot {
+    atRoot?           : true|undefined
+}
+
+/**
+ * A react context for validation stuff.
+ */
+export const Context = createContext<Validation & ValidationRoot>(/*defaultValue :*/{
+    enableValidation  : _defaultEnableValidation,
+    isValid           : _defaultIsValid,
+    
+    atRoot            : true,
+});
+Context.displayName  = 'Validation';
+
+
+
+// hooks:
+
+export const usePropValidation = (props: ValidationProps): Validation & ValidationRoot => {
+    // contexts:
+    const valContext = useContext(Context);
+    const atRoot     = valContext.atRoot;
+
+
+
+    const inheritValidation : boolean = (props.inheritValidation ?? _defaultInheritValidation);
+    const enableValidation = atRoot ? (props.enableValidation ?? _defaultEnableRootValidation) : (
+        (
+            inheritValidation
+            ?
+            valContext.enableValidation // inherit
+            :
+            true                        // independent
+        )
+        &&
+        (props.enableValidation ?? _defaultEnableValidation)
+    );
+    const isValid = ((): Result|undefined => {
+        if (!enableValidation) return null; // if validation was disabled => treat validity as `uncheck` (null)
+
+        
+        
+        const contextIsValid = (
+            inheritValidation
+            ?
+            valContext.isValid          // inherit
+            :
+            undefined                   // independent
+        );
+        if (contextIsValid !== undefined) return contextIsValid; // if the context's validity was set other than `auto` (undefined) => use it
+        
+        
+        
+        return props.isValid;                                    // otherwise => use the component's validity
+    })();
+    return {
+        enableValidation,
+        isValid,
+        atRoot,
+    };
+};
+
+
+
+// react components:
+
+export interface ValidationProps extends Partial<Validation>
+{
+    /**
+     * `undefined` : same as `true`.  
+     * `true`      : validation is enabled  - implements `isValid` prop.  
+     * `false`     : validation is disabled - equivalent as `isValid = null` (uncheck).
+     */
+    enableValidation?  : boolean
+
+    /**
+     * `undefined` : *automatic* detect valid/invalid state.  
+     * `null`      : force validation state to *uncheck*.  
+     * `true`      : force validation state to *valid*.  
+     * `false`     : force validation state to *invalid*.
+     */
+    isValid?           : Result
+
+    /**
+     * `undefined` : same as `true`.  
+     * `true`      : inherits `enableValidation` & `isValid` from parent (`ValidationProvider` context).  
+     * `false`     : independent `enableValidation` & `isValid`.
+     */
+    inheritValidation? : boolean
+
+
+
+    // children:
+    children?          : React.ReactNode
+}
+export function ValidationProvider(props: ValidationProps) {
+    // fn props:
+    const { atRoot, ...propValidation } = usePropValidation(props);
+    if (atRoot) {
+        propValidation.enableValidation = props.enableValidation ?? _defaultEnableValidation;
+        propValidation.isValid          = props.isValid          ?? _defaultIsValid;
+    } // if
+    
+    
+    
+    return (
+        <Context.Provider value={propValidation}>
+            {props.children}
+        </Context.Provider>
+    );
+}
+export { ValidationProvider as default }
