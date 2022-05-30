0d580b5a48d713fc8fe1ccf9cfcf7b7c6b4e1442
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-30 13:33:13 +0700

    added utilities

---
+++ packages/utilities/src/utilities.ts
@@ -0,0 +1,61 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+
+
+// utilities:
+
+export const isTypeOf = <TProps>(element: React.ReactNode, funcComponent: React.JSXElementConstructor<TProps>): element is React.ReactElement<TProps, React.JSXElementConstructor<TProps>> => {
+    return (
+        React.isValidElement<TProps>(element)
+        &&
+        (
+            (element.type === funcComponent)
+            ||
+            (
+                (typeof(element.type) === 'function')
+                &&
+                element.type.prototype
+                &&
+                funcComponent.prototype
+                &&
+                (
+                    (element.type.prototype === funcComponent.prototype)
+                    ||
+                    (element.type.prototype instanceof funcComponent)
+                )
+            )
+        )
+    );
+};
+
+export const setRef = <TValue>(ref: React.Ref<TValue>|undefined, value: TValue|null): void => {
+    if (ref) {
+        if (typeof(ref) === 'function') {
+            ref(value);
+        }
+        else {
+            (ref as React.MutableRefObject<TValue|null>).current = value;
+        } // if
+    } // if
+};
+
+const isSingleValue = (expression: string|ReadonlyArray<string>): expression is string => (typeof(expression) === 'string') || (Array.isArray(expression) && (expression.length === 1));
+export const parseNumber = (expression: number|string|ReadonlyArray<string>|null|undefined): number|null => {
+    if (typeof(expression) === 'number') return expression;
+    if (!expression) return null;
+    
+    
+    
+    if (!isSingleValue(expression)) return null;
+    if (!expression) return null;
+    
+    
+    
+    const result = Number.parseFloat(expression);
+    if (isNaN(result)) return null;
+    return result;
+};
