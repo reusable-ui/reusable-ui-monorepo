8fa7ceb425b6a91ce51fb299c46f2e895f1c3e4b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-30 13:07:31 +0700

    added hooks

---
+++ packages/hooks/src/hooks.ts
@@ -0,0 +1,38 @@
+// react:
+import {
+    // hooks:
+    useEffect,
+    useLayoutEffect,
+    useReducer,
+}                           from 'react'
+
+// other libs:
+import {
+    // tests:
+    isBrowser,
+    isJsDom,
+}                           from 'is-in-browser'
+
+
+
+// utilities:
+export const isClientSide : boolean = isBrowser || isJsDom;
+
+
+
+// hooks:
+
+/**
+ * A React helper hook for using `useLayoutEffect` with a fallback to a regular `useEffect` for environments where `useLayoutEffect` should not be used (such as server-side rendering).
+ */
+export const useIsomorphicLayoutEffect = isClientSide ? useLayoutEffect : useEffect;
+
+
+
+const triggerRenderReducer = (_currentGeneration: object, _newGeneration: void): object => {
+    return {}; // update with a new object
+};
+export const useTriggerRender = () => {
+    const [generation, setState] = useReducer(triggerRenderReducer, {});
+    return [setState, generation] as const;
+};
