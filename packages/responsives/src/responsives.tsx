4283be71da07f66439d7dee430d8352e00623b08
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-31 18:57:47 +0700

    updated responsives

--- packages/responsives/src/responsives.tsx
+++ packages/responsives/src/responsives.tsx
@@ -18,5 +18,128 @@ import {
     useContext,
 }                           from 'react'
 
+// reusable-ui:
+import {
+    // utilities:
+    setRef,
+    parseNumber,
+}                           from '@reusable-ui/utilities'   // react helper hooks
 
 
+
+// contexts:
+
+/**
+ * Contains responsive props.
+ */
+export interface Responsive<TFallback> {
+    currentFallback : TFallback
+}
+
+/**
+ * A react context for responsive stuff.
+ */
+export const ResponsiveContext = createContext<Responsive<any>>(/*defaultValue :*/{
+    currentFallback : undefined,
+});
+ResponsiveContext.displayName  = 'Responsive';
+
+
+
+// hooks:
+export const useResponsiveCurrentFallback = <TFallback,>() => {
+    // contexts:
+    const responsiveContext = useContext(ResponsiveContext);
+    return responsiveContext.currentFallback as TFallback;
+};
+
+
+
+// utilities:
+const isOverflowable = (element: Element): boolean => {
+    const {
+        display,
+        // overflowX,
+        // overflowY,
+    } = getComputedStyle(element); // warning: force reflow => major performance bottleneck!
+    if (display === 'none') return false; // hidden element => not overflowable
+    // if (
+    //     (overflowX !== 'visible')
+    //     &&
+    //     (overflowY !== 'visible')
+    // )                       return false; // hidden/scroll/clip/overlay/auto/ => not overflowable
+    
+    return true; // all checks passed
+};
+
+const hasOverflowedDescendant = (element: Element, minLeft: number|null, minTop: number|null, maxRight: number|null, maxBottom: number|null): boolean => {
+    if (Array.from(element.children).some((child) => {
+        if (!isOverflowable(child)) return false;
+        
+        
+        
+        let {
+            left   : childLeft,
+            top    : childTop,
+            right  : childRight,
+            bottom : childBottom,
+        } = child.getBoundingClientRect(); // warning: force reflow => major performance bottleneck!
+        childLeft   = Math.round(childLeft  );
+        childTop    = Math.round(childTop   );
+        childRight  = Math.round(childRight );
+        childBottom = Math.round(childBottom);
+        
+        
+        
+        const {
+            marginLeft   : marginLeftStr,
+            marginTop    : marginTopStr,
+            marginRight  : marginRightStr,
+            marginBottom : marginBottomStr,
+        } = getComputedStyle(child); // warning: force reflow => major performance bottleneck!
+        const marginLeft     = (parseNumber(marginLeftStr  ) ?? 0);
+        const marginTop      = (parseNumber(marginTopStr   ) ?? 0);
+        const marginRight    = (parseNumber(marginRightStr ) ?? 0);
+        const marginBottom   = (parseNumber(marginBottomStr) ?? 0);
+        const minLeftShift   = (minLeft   === null) ? null : Math.round(minLeft   + marginLeft  );
+        const minTopShift    = (minTop    === null) ? null : Math.round(minTop    + marginTop   );
+        const maxRightShift  = (maxRight  === null) ? null : Math.round(maxRight  - marginRight );
+        const maxBottomShift = (maxBottom === null) ? null : Math.round(maxBottom - marginBottom);
+        
+        
+        
+        if (
+            (
+                (minLeftShift !== null)
+                &&
+                (childLeft  < minLeftShift)    // smaller than minimum => overflowed
+            )
+            ||
+            (
+                (minTopShift !== null)
+                &&
+                (childTop  < minTopShift)      // smaller than minimum => overflowed
+            )
+            ||
+            (
+                (maxRightShift !== null)
+                &&
+                (childRight  > maxRightShift)  // bigger than maximum => overflowed
+            )
+            ||
+            (
+                (maxBottomShift !== null)
+                &&
+                (childBottom > maxBottomShift) // bigger than maximum => overflowed
+            )
+        ) {
+            return true; // found
+        } // if
+        
+        
+        
+        return hasOverflowedDescendant(child, minLeftShift, minTopShift, maxRightShift, maxBottomShift); // nested search
+    })) return true; // found
+    
+    return false; // not found
+};
