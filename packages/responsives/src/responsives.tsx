45ab031d0f60e561309dcaa5eaca25e4cc4f93db
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-05-31 18:23:23 +0700

    added responsives

---
+++ packages/responsives/src/responsives.tsx
@@ -0,0 +1,22 @@
+// react:
+import {
+    // react:
+    default as React,
+    
+    
+    
+    // hooks:
+    useState,
+    useRef,
+    useRef as _useRef, // avoids eslint check
+    useCallback,
+    
+    
+    
+    // contexts:
+    createContext,
+    useContext,
+}                           from 'react'
+
+
+
