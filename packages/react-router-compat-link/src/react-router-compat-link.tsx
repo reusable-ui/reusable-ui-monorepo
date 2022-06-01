7a6c35a9b785d4f714f73d410689ad4f7ca5624f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-01 18:40:25 +0700

    updated react-router-compat-link

--- packages/react-router-compat-link/src/react-router-compat-link.tsx
+++ packages/react-router-compat-link/src/react-router-compat-link.tsx
@@ -6,14 +6,9 @@ import {
 
 // react router:
 import {
-    // types:
-    To,
-    
-    
-    
     // hooks:
     useHref,
-}                        from 'react-router'
+}                           from 'react-router'
 import {
     // types:
     LinkProps as BaseLinkProps,
@@ -22,7 +17,7 @@ import {
     
     // hooks:
     useLinkClickHandler,
-}                        from 'react-router-dom'
+}                           from 'react-router-dom'
 
 
 
@@ -38,25 +33,94 @@ export interface LinkProps extends BaseLinkProps
  *
  * @see https://reactrouter.com/docs/en/v6/components/link
  */
-export const Link = React.forwardRef(function LinkWithRef(props: LinkProps, ref: React.ForwardedRef<HTMLAnchorElement>): JSX.Element|null {
+const Link = React.forwardRef(function LinkWithRef(props: LinkProps, ref: React.ForwardedRef<HTMLAnchorElement>): JSX.Element|null {
     // rest props:
     const {
         // react router links:
-        reloadDocument,
-        replace = false,
+        reloadDocument = false,
+        replace        = false,
         state,
         to,
         
-        component = undefined,
-        passHref  = false,
+        component      = undefined,
+        passHref       = false,
+        
         
         
         // links:
         target,
-    ...restProps} = props;
+    ...restAnchorProps} = props;
     
     
     
     // fn props:
     const href = useHref(to);
+    
+    
+    
+    // handlers:
+    const clientSideHandleClick = useLinkClickHandler(to, { replace, state, target });
+    const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
+        props.onClick?.(e); // keeps the original onClick on <Link onClick={...} />
+        
+        if (!e.defaultPrevented && !reloadDocument) {
+            clientSideHandleClick(e);
+        } // if
+    };
+    
+    
+    
+    // jsx:
+    if (component) {
+        return React.cloneElement(component, {
+            // essentials:
+            ref,
+            
+            
+            
+            // links:
+            ...(passHref ? {
+                href,
+                target,
+            } : {}),
+            
+            
+            
+            // events:
+            onClick: ((e) => {
+                component.props.onClick?.(e); // keeps the original onClick on <FooComponent onClick={...} />
+                
+                handleClick(e);
+            }) as React.MouseEventHandler<HTMLAnchorElement>,
+        });
+    }
+    else {
+        return (
+            // eslint-disable-next-line jsx-a11y/anchor-has-content
+            <a
+                // other props:
+                {...restAnchorProps}
+                
+                
+                
+                // essentials:
+                ref={ref}
+                
+                
+                
+                // links:
+                href={href}
+                target={target}
+                
+                
+                
+                // events:
+                onClick={handleClick}
+            />
+        );
+    } // if
 });
+export {
+    Link,
+    Link as default,
+}
