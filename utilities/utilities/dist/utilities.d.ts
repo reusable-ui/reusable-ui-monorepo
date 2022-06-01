1628f7326e0f67ecd88613ae21fa2458bddeffaa
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-01 20:09:01 +0700

    Revert "renamed packages/ to utilities/"
    This reverts commit 04c90cf3749eb870a5c16a4af73e3f90f463337f.

--- utilities/utilities/dist/utilities.d.ts
+++
@@ -1,4 +0,0 @@
-import { default as React } from 'react';
-export declare const isTypeOf: <TProps>(element: React.ReactNode, funcComponent: React.JSXElementConstructor<TProps>) => element is React.ReactElement<TProps, React.JSXElementConstructor<TProps>>;
-export declare const setRef: <TValue>(ref: React.Ref<TValue> | undefined, value: TValue | null) => void;
-export declare const parseNumber: (expression: number | string | ReadonlyArray<string> | null | undefined) => number | null;
