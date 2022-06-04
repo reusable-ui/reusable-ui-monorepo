b27b71c3ed319a2c529823fddd9385bb8026e050
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-04 21:50:22 +0700

    added test-basic

---
+++ tests/test-basic/src/App.tsx
@@ -0,0 +1,44 @@
+import {
+    default as React,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Basic,
+} from '@reusable-ui/basic'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <Basic>
+                    test &lt;Basic&gt;
+                </Basic>
+            </div>
+        </>
+    );
+}
+
+export default App;
