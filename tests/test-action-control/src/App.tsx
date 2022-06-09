962a4d0832ea8bf226bfc9f392e4d89b3c1117a5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-10 02:26:01 +0700

    added test-action-control

---
+++ tests/test-action-control/src/App.tsx
@@ -0,0 +1,53 @@
+import {
+    default as React,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    ActionControl,
+} from '@reusable-ui/action-control'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    const [pressed, setPressed] = useState(false);
+    const handleTogglePressed = () => {
+        setPressed(!pressed);
+    };
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <article className='actions'>
+                    <ActionControl theme='primary' mild={false}>
+                        test &lt;ActionControl&gt;
+                    </ActionControl>
+                    <ActionControl theme='primary' mild={false} pressed={pressed || undefined} onClick={handleTogglePressed}>
+                        toggle press
+                    </ActionControl>
+                </article>
+            </div>
+        </>
+    );
+}
+
+export default App;
