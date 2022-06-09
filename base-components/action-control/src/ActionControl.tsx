56c3a1961d1cc2b13407ab9711277916a233c5b2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-09 23:49:39 +0700

    updated action-control

--- base-components/action-control/src/ActionControl.tsx
+++ base-components/action-control/src/ActionControl.tsx
@@ -117,8 +117,8 @@ import type {
 const defaultActionMouses : number[]|null = [0];       // left click
 const defaultActionKeys   : string[]|null = ['space']; // space key
 
-const defaultTag  : DefaultTag  = [null, 'button', 'a'   ] // uses <div> if the `tag` was not specified
-const defaultRole : DefaultRole = [      'button', 'link'] // uses `role="button"` for `tag` other than `button|a`
+const defaultTag  : DefaultTag  = [null, 'button', 'a'   ] // uses <div>           as the default semantic, fallbacks to <button>, <a>
+const defaultRole : DefaultRole = [      'button', 'link'] // uses [role="button"] as the default semantic, fallbacks to [role="link"]
 
 
 
