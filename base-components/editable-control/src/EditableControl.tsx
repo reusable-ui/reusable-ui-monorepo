5fd2963dde54d2f8db6104607c37da80e71dddd5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-11 16:58:11 +0700

    updated editable-control

--- base-components/editable-control/src/EditableControl.tsx
+++ base-components/editable-control/src/EditableControl.tsx
@@ -274,7 +274,7 @@ export const markValid   = (): CssRule => style({
  * @param themeName The name of valid theme.
  * @returns A `CssRule` represents the conditional color definitions at valid state.
  */
-export const usesThemeValid   = (themeName: ThemeName|null = 'success') => usesThemeImpt(themeName);
+export const usesThemeValid   = (themeName: ThemeName|null = 'success'): CssRule => usesThemeImpt(themeName);
 
 export const markInvalid = (): CssRule => style({
     ...imports([
@@ -286,7 +286,7 @@ export const markInvalid = (): CssRule => style({
  * @param themeName The name of invalid theme.
  * @returns A `CssRule` represents the conditional color definitions at invalid state.
  */
-export const usesThemeInvalid = (themeName: ThemeName|null = 'danger' ) => usesThemeImpt(themeName);
+export const usesThemeInvalid = (themeName: ThemeName|null = 'danger' ): CssRule => usesThemeImpt(themeName);
 
 
 
