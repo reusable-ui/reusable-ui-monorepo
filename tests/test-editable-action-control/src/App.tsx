5072f7314dd2932611baa16a6d473ee88b7a28a8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-13 19:07:32 +0700

    added test-editable-action-control

---
+++ tests/test-editable-action-control/src/App.tsx
@@ -0,0 +1,45 @@
+import {
+    default as React,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    EditableActionControl,
+} from '@reusable-ui/editable-action-control'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <article className='actions'>
+                    <EditableActionControl theme='primary' enableValidation={true}>
+                        Test &lt;EditableActionControl&gt;
+                    </EditableActionControl>
+                </article>
+            </div>
+        </>
+    );
+}
+
+export default App;
