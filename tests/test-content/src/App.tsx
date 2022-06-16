f0509847896ab29d3ee79813abe36ab0db18a084
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-16 18:21:09 +0700

    added test-content

---
+++ tests/test-content/src/App.tsx
@@ -0,0 +1,54 @@
+import {
+    default as React,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Content,
+} from '@reusable-ui/content'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <Content theme='primary'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aut deserunt nulla iusto quod a est debitis tenetur dolorem? Molestiae unde nulla amet odio eveniet, quis eum libero aperiam natus?
+                    </p>
+                    <img alt='lorem image' src='/images/lorem-image-1.svg' style={{ height: '150px' }} />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aut deserunt nulla iusto quod a est debitis tenetur dolorem? Molestiae unde nulla amet odio eveniet, quis eum libero aperiam natus?
+                    </p>
+                    <img alt='lorem image' src='/images/lorem-image-1.svg' style={{ height: '150px' }} />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aut deserunt nulla iusto quod a est debitis tenetur dolorem? Molestiae unde nulla amet odio eveniet, quis eum libero aperiam natus?
+                    </p>
+                </Content>
+            </div>
+        </>
+    );
+}
+
+export default App;
