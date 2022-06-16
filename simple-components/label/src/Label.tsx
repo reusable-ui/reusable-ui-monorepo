0396cf0b48425eb7a29e92da8d8bf1c7dd68c415
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-16 23:30:06 +0700

    updated label

--- simple-components/label/src/Label.tsx
+++ simple-components/label/src/Label.tsx
@@ -5,15 +5,10 @@ import {
 }                           from 'react'
 
 // cssfn:
-import type {
-    // css known (standard) properties:
-    CssKnownProps,
-}                           from '@cssfn/css-types'             // cssfn css specific types
 import {
     // rules:
     rule,
     variants,
-    states,
     
     
     
@@ -32,77 +27,34 @@ import {
     
     // utilities:
     usesCssProps,
-    usesPrefixedProps,
 }                           from '@cssfn/css-config'            // reads/writes css variables configuration
 
 // reusable-ui:
-import {
-    // configs:
-    borderRadiuses,
-}                           from '@reusable-ui/borders'         // a border (stroke) management system
-import {
-    // configs:
-    spacers,
-}                           from '@reusable-ui/spacers'         // a spacer (gap) management system
 import {
     // hooks:
     useMergeClasses,
 }                           from '@reusable-ui/hooks'           // react helper hooks
 import {
-    // types:
-    DefaultTag,
-    DefaultRole,
-    
-    
-    
-    // hooks:
-    useTestSemantic,
-}                           from '@reusable-ui/generic'         // a base component
-import {
     // hooks:
     usesSizeVariant,
-    OrientationName,
-    OrientationRuleOptions,
-    defaultInlineOrientationRuleOptions,
-    normalizeOrientationRule,
-    usesOrientationRule,
-    OrientationVariant,
-    useOrientationVariant,
-    gradientOf,
-    ifNotOutlined,
-    outlinedOf,
-    usesBorder,
-    usesPadding,
-}                           from '@reusable-ui/basic'           // a base component
-import {
-    // hooks:
-    ifActive,
-}                           from '@reusable-ui/indicator'       // a base component
-import {
-    // hooks:
-    usesThemeActive,
-    ifFocus,
-    ifArrive,
-    ifLeave,
-}                           from '@reusable-ui/control'         // a base component
-import {
-    // hooks:
-    ifPress,
-    isClientSideLink,
     
     
     
     // styles:
-    usesActionControlLayout,
-    usesActionControlVariants,
-    usesActionControlStates,
+    usesBasicLayout,
+    usesBasicVariants,
     
     
     
     // react components:
-    ActionControlProps,
-    ActionControl,
-}                           from '@reusable-ui/action-control'  // a base component
+    BasicProps,
+    Basic,
+}                           from '@reusable-ui/basic'           // a base component
+import {
+    // styles:
+    usesContentBasicLayout,
+    usesContentBasicVariants,
+}                           from '@reusable-ui/content'         // a base component
 
 
 
@@ -111,13 +63,13 @@ import {
 // appearances:
 
 //#region label style
-export type LabelStyle = 'link'|'icon'|'ghost' // might be added more styles in the future
+export type LabelStyle = 'content' // might be added more styles in the future
 export interface LabelVariant {
-    btnStyle ?: LabelStyle
+    labelStyle ?: LabelStyle
 }
 export const useLabelVariant = (props: LabelVariant) => {
     return {
-        class: props.btnStyle ?? null,
+        class: props.labelStyle ?? null,
     };
 };
 //#endregion label style
@@ -125,45 +77,34 @@ export const useLabelVariant = (props: LabelVariant) => {
 
 
 // styles:
-export const usesLabelLayout = (options?: OrientationRuleOptions) => {
-    // options:
-    options = normalizeOrientationRule(options, defaultOrientationRuleOptions);
-    const [orientationInlineSelector, orientationBlockSelector] = usesOrientationRule(options);
-    
-    
-    
+export const usesLabelLayout = () => {
     return style({
         ...imports([
             // layouts:
-            usesActionControlLayout(),
+            usesBasicLayout(),
         ]),
         ...style({
             // layouts:
-            display           : 'inline-flex', // use inline flexbox, so it takes the width & height as we set
-            ...rule(orientationInlineSelector, { // inline
-                flexDirection : 'row',         // items are stacked horizontally
-            }),
-            ...rule(orientationBlockSelector,  { // block
-                flexDirection : 'column',      // items are stacked vertically
-            }),
-            justifyContent    : 'center',      // center items (text, icon, etc) horizontally
-            alignItems        : 'center',      // center items (text, icon, etc) vertically
-            flexWrap          : 'wrap',        // allows the items (text, icon, etc) to wrap to the next row if no sufficient width available
+            display        : 'inline-flex',  // use inline flexbox, so it takes the width & height as we set
+            flexDirection  : 'row',          // items are stacked horizontally
+            justifyContent : 'center',       // center items (text, icon, etc) horizontally
+            alignItems     : 'center',       // center items (text, icon, etc) vertically
+            flexWrap       : 'wrap',         // allows the items (text, icon, etc) to wrap to the next row if no sufficient width available
             
             
             
             // positions:
-            verticalAlign     : 'baseline',    // label's text should be aligned with sibling text, so the label behave like <span> wrapper
+            verticalAlign  : 'baseline',     // label's text should be aligned with sibling text, so the label behave like <span> wrapper
             
             
             
             // typos:
-            textAlign         : 'center',
+            textAlign      : 'start',        // flow to the document's writing flow
             
             
             
             // customize:
-            ...usesCssProps(labels), // apply config's cssProps
+            ...usesCssProps(labels),         // apply config's cssProps
         }),
     });
 };
@@ -178,25 +119,21 @@ export const usesLabelVariants = () => {
     return style({
         ...imports([
             // variants:
-            usesActionControlVariants(),
+            usesBasicVariants(),
             
             // layouts:
             sizesRule,
         ]),
         ...variants([
-            rule(['.link', '.icon', '.ghost'], {
+            rule('.content', { // content
                 ...imports([
-                    noBackground(),
+                    // layouts:
+                    usesContentBasicLayout(),
+                    
+                    // variants:
+                    usesContentBasicVariants(),
                 ]),
             }),
-        ]),
-    });
-};
-export const usesLabelStates = () => {
-    return style({
-        ...imports([
-            // states:
-            usesActionControlStates(),
         ]),
     });
 };
@@ -208,9 +145,6 @@ export const useLabelStyleSheet = createUseStyleSheet(() => ({
         
         // variants:
         usesLabelVariants(),
-        
-        // states:
-        usesLabelStates(),
     ]),
 }), { id: 'si01upz9vr' }); // a unique salt for SSR support, ensures the server-side & client-side have the same generated class names
 
@@ -219,54 +153,32 @@ export const useLabelStyleSheet = createUseStyleSheet(() => ({
 // configs:
 export const [labels, labelValues, cssLabelConfig] = cssConfig(() => {
     return {
-        // spacings:
-        gapInline          : spacers.sm as CssKnownProps['gapInline'],
-        gapBlock           : spacers.sm as CssKnownProps['gapBlock' ],
-        gapInlineSm        : spacers.xs as CssKnownProps['gapInline'],
-        gapBlockSm         : spacers.xs as CssKnownProps['gapBlock' ],
-        gapInlineLg        : spacers.md as CssKnownProps['gapInline'],
-        gapBlockLg         : spacers.md as CssKnownProps['gapBlock' ],
-        
-        
-        
-        // typos:
-        whiteSpace         : 'normal'   as CssKnownProps['whiteSpace'],
-        
-        
-        
-        // ghost style:
-        ghostOpacity       : 0.5        as CssKnownProps['opacity'],
-        ghostOpacityArrive : 1          as CssKnownProps['opacity'],
+        /* no config props yet */
     };
 }, { prefix: 'lb' });
 
 
 
 // react components:
-export interface LabelProps
+export interface LabelProps<TElement extends Element = Element>
     extends
         // bases:
-        SemanticLabelProps<HTMLLabelElement>,
+        BasicProps<TElement>,
         
         // appearances:
         LabelVariant
 {
-    // accessibilities:
-    label    ?: string
-    
-    
-    
     // children:
     children ?: React.ReactNode
 }
-const Label = (props: LabelProps): JSX.Element|null => {
+const Label = <TElement extends Element = Element>(props: LabelProps<TElement>): JSX.Element|null => {
     // styles:
-    const styleSheet         = useLabelStyleSheet();
+    const styleSheet   = useLabelStyleSheet();
     
     
     
     // variants:
-    const labelVariant      = useLabelVariant(props);
+    const labelVariant = useLabelVariant(props);
     
     
     
@@ -274,26 +186,9 @@ const Label = (props: LabelProps): JSX.Element|null => {
     const {
         // remove props:
         
-        // layouts:
-        orientation : _orientation,
-        
-        
-        
         // appearances:
-        btnStyle    : _btnStyle,
-        
-        
-        
-        // accessibilities:
-        label,
-        pressed,
-        
-        
-        
-        // variants:
-        outlined = _defaultOutlined,
-        mild     = _defaultMild,
-    ...restActionControlProps} = props;
+        labelStyle : _labelStyle,
+    ...restBasicProps} = props;
     
     
     
@@ -312,44 +207,26 @@ const Label = (props: LabelProps): JSX.Element|null => {
     
     // jsx:
     return (
-        <ActionControl<HTMLLabelElement>
+        <Basic<TElement>
             // other props:
-            {...restActionControlProps}
+            {...restBasicProps}
             
             
             
             // semantics:
-            defaultTag  = {defaultTag }
-            defaultRole = {defaultRole}
-            tag         = {tag}
-            role        = {role}
-            aria-label  = {props['aria-label'] ?? label}
-            
-            
-            
-            // accessibilities:
-            enabled={props.enabled ?? !(props.disabled ?? false)}
-            pressed={pressedFn}
+            tag={props.tag ?? 'span'}
             
             
             
             // variants:
-            outlined={outlined}
-            mild={mild}
+            theme={props.theme ?? 'secondary'}
+            mild={props.mild ?? true}
             
             
             
             // classes:
             mainClass={props.mainClass ?? styleSheet.main}
             variantClasses={variantClasses}
-            
-            
-            
-            // Label props:
-            {...{
-                // actions:
-                type,
-            }}
         />
     );
 };
