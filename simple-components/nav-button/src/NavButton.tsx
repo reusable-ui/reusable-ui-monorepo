def1da791883a7e293415ed80883cbe84bdfb1e6
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-17 02:57:13 +0700

    updated nav-button

--- simple-components/nav-button/src/NavButton.tsx
+++ simple-components/nav-button/src/NavButton.tsx
@@ -4,95 +4,8 @@ import {
     default as React,
 }                           from 'react'
 
-// cssfn:
-import type {
-    // css known (standard) properties:
-    CssKnownProps,
-}                           from '@cssfn/css-types'             // cssfn css specific types
-import {
-    // rules:
-    rule,
-    variants,
-    states,
-    
-    
-    
-    // styles:
-    style,
-    imports,
-}                           from '@cssfn/cssfn'                 // writes css in javascript
-import {
-    // style sheets:
-    createUseStyleSheet,
-}                           from '@cssfn/cssfn-react'           // writes css in react hook
-import {
-    cssConfig,
-    
-    
-    
-    // utilities:
-    usesCssProps,
-    usesPrefixedProps,
-}                           from '@cssfn/css-config'            // reads/writes css variables configuration
-
 // reusable-ui:
 import {
-    // configs:
-    borderRadiuses,
-}                           from '@reusable-ui/borders'         // a border (stroke) management system
-import {
-    // configs:
-    spacers,
-}                           from '@reusable-ui/spacers'         // a spacer (gap) management system
-import {
-    // hooks:
-    useMergeClasses,
-}                           from '@reusable-ui/hooks'           // react helper hooks
-import {
-    // types:
-    DefaultTag,
-    DefaultRole,
-    
-    
-    
-    // hooks:
-    useTestSemantic,
-}                           from '@reusable-ui/generic'         // a base component
-import {
-    // hooks:
-    usesSizeVariant,
-    OrientationName,
-    OrientationRuleOptions,
-    defaultInlineOrientationRuleOptions,
-    normalizeOrientationRule,
-    usesOrientationRule,
-    OrientationVariant,
-    useOrientationVariant,
-    gradientOf,
-    ifNotOutlined,
-    outlinedOf,
-    usesBorder,
-    usesPadding,
-}                           from '@reusable-ui/basic'           // a base component
-import {
-    // hooks:
-    ifActive,
-}                           from '@reusable-ui/indicator'       // a base component
-import {
-    // hooks:
-    usesThemeActive,
-    ifFocus,
-    ifArrive,
-    ifLeave,
-}                           from '@reusable-ui/control'         // a base component
-import {
-    // styles:
-    usesButtonLayout,
-    usesButtonVariants,
-    usesButtonStates,
-    
-    
-    
     // react components:
     ButtonProps,
     Button,
@@ -106,38 +19,11 @@ export interface NavButtonProps
         // bases:
         ButtonProps
 {
-    // accessibilities:
-    label    ?: string
-    
-    
-    
-    // children:
-    children ?: React.ReactNode
 }
 const NavButton = (props: NavButtonProps): JSX.Element|null => {
     // rest props:
     const {
         // remove props:
-        
-        // layouts:
-        orientation : _orientation,
-        
-        
-        
-        // appearances:
-        btnStyle    : _btnStyle,
-        
-        
-        
-        // accessibilities:
-        label,
-        pressed,
-        
-        
-        
-        // variants:
-        outlined = _defaultOutlined,
-        mild     = _defaultMild,
     ...restButtonProps} = props;
     
     
@@ -147,18 +33,6 @@ const NavButton = (props: NavButtonProps): JSX.Element|null => {
         <Button
             // other props:
             {...restButtonProps}
-            
-            
-            
-            // accessibilities:
-            enabled={props.enabled ?? !(props.disabled ?? false)}
-            pressed={pressedFn}
-            
-            
-            
-            // variants:
-            outlined={outlined}
-            mild={mild}
         />
     );
 };
