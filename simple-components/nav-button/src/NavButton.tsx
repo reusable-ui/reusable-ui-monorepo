0a355f2dbdc7f91548e78882149cda261eaea277
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-17 15:56:07 +0700

    updated nav-button

--- simple-components/nav-button/src/NavButton.tsx
+++ simple-components/nav-button/src/NavButton.tsx
@@ -2,9 +2,22 @@
 import {
     // react:
     default as React,
+    
+    
+    
+    // hooks:
+    useState,
 }                           from 'react'
 
 // reusable-ui:
+import {
+    // hooks:
+    useIsomorphicLayoutEffect,
+}                           from '@reusable-ui/hooks'           // react helper hooks
+import {
+    // hooks:
+    isClientSideLink,
+}                           from '@reusable-ui/action-control'  // a base component
 import {
     // react components:
     ButtonProps,
@@ -17,6 +30,11 @@ import {
     To,
     parsePath,
 }                           from 'history'
+import {
+    // tests:
+    isBrowser,
+    isJsDom,
+}                           from 'is-in-browser'
 
 
 
@@ -86,6 +104,85 @@ const resolveRelativePath = (fromPathname: Pathname, relativePath: Pathname): Pa
     // merge the segment(s) to a final absolute pathName:
     if (segments.length > 1) return segments.join('/');
     return '/';
+};
+
+
+
+// hooks:
+export interface CurrentActiveProps {
+    // nav matches:
+    caseSensitive ?: boolean
+    end           ?: boolean
+    
+    
+    
+    // children:
+    children      ?: React.ReactNode
+}
+export const useCurrentActive = (props: CurrentActiveProps): boolean|undefined => {
+    /* server side rendering support */
+    /* always return `undefined` on the first render */
+    /* so the DOM is __always_the_same__ at the server side & client side */
+    const [loaded, setLoaded] = useState(false);
+    useIsomorphicLayoutEffect(() => {
+        // setups:
+        setLoaded(true); // trigger to re-render
+    }, []); // runs once on startup
+    
+    
+    
+    /* conditionally return - ALWAYS return on server side - NEVER return on client side - it's safe */
+    if (typeof(globalThis.location) === 'undefined') return undefined; // server side rendering => not supported yet
+    
+    
+    
+    const to = ((): To|undefined => {
+        const childProps = React.Children.toArray(props.children).find(isClientSideLink)?.props;
+        if (childProps) return childProps.to ?? childProps.href;
+        return undefined;
+    })();
+    /* conditionally return - ASSUMES the children are never changed - ALWAYS return the same - it's 99% safe */
+    if (to === undefined) return undefined; // client side <Link> does not exist
+    
+    
+    
+    // let currentPathname = useLocation().pathname;            // only works in react-router
+    let currentPathname = globalThis?.location?.pathname ?? ''; // works both in react-router & nextjs
+    let targetPathname = _useInRouterContext() ? _useResolvedPath(to).pathname : resolvePath(to, currentPathname);
+    
+    
+    
+    /* conditionally return AFTER hooks - it's safe */
+    if (!loaded) return undefined; // always return `undefined` on the first render
+    
+    
+    
+    if (!(props.caseSensitive ?? false)) {
+        currentPathname = currentPathname.toLocaleLowerCase();
+        targetPathname  = targetPathname.toLocaleLowerCase();
+    } // if
+    
+    
+    
+    return (
+        (currentPathname === targetPathname) // exact match
+        ||
+        (
+            !(
+                // user defined:
+                props.end
+                ??
+                // default:
+                (targetPathname === '/') // for home page always exact match, otherwise sub match
+            ) // sub match
+            &&
+            (
+                currentPathname.startsWith(targetPathname)
+                &&
+                (currentPathname.charAt(targetPathname.length) === '/') // sub segment
+            )
+        )
+    );
 };
 
 
