af627611f26cfb8ffee4978ea5c20e2961fa2020
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-20 02:25:26 +0700

    added close-button

---
+++ simple-components/close-button/src/CloseButton.tsx
@@ -0,0 +1,61 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// reusable-ui:
+import {
+    // hooks:
+    OrientationName,
+    OrientationVariant,
+    
+    ButtonStyle,
+    ButtonVariant,
+    ButtonType,
+    IconPosition,
+    
+    
+    
+    // react components:
+    ButtonIconProps,
+    ButtonIcon,
+}                           from '@reusable-ui/button-icon'     // a base component
+
+
+
+// react components:
+export interface CloseButtonProps
+    extends
+        // bases:
+        ButtonIconProps
+{
+}
+const CloseButton = (props: CloseButtonProps): JSX.Element|null => {
+    // jsx:
+    return (
+        <ButtonIcon
+            // other props:
+            {...props}
+            
+            
+            
+            // appearances:
+            icon={props.icon ?? 'close'}
+            btnStyle={props.btnStyle ?? 'icon'}
+            
+            
+            
+            // accessibilities:
+            label={props.label ?? 'Close'}
+        />
+    );
+};
+export {
+    CloseButton,
+    CloseButton as default,
+}
+
+export type { OrientationName, OrientationVariant }
+
+export type { ButtonStyle, ButtonVariant, ButtonType, IconPosition }
