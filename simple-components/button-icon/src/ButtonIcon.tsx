1336ea233653b274a16004863a05b8ce7704db04
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-19 13:57:48 +0700

    updated button-icon

--- simple-components/button-icon/src/ButtonIcon.tsx
+++ simple-components/button-icon/src/ButtonIcon.tsx
@@ -8,17 +8,18 @@ import {
 import type {
     // css known (standard) properties:
     CssKnownProps,
+    
+    
+    
+    // cssfn properties:
+    CssRule,
+    
+    CssStyleCollection,
 }                           from '@cssfn/css-types'             // cssfn css specific types
 import {
-    // rules:
-    rule,
-    variants,
-    states,
-    
-    
-    
     // styles:
     style,
+    vars,
     imports,
 }                           from '@cssfn/cssfn'                 // writes css in javascript
 import {
@@ -26,66 +27,56 @@ import {
     createUseStyleSheet,
 }                           from '@cssfn/cssfn-react'           // writes css in react hook
 import {
+    // types:
+    CssConfigProps,
+    Refs,
+    
+    
+    
     cssConfig,
     
     
     
     // utilities:
     usesCssProps,
-    usesPrefixedProps,
 }                           from '@cssfn/css-config'            // reads/writes css variables configuration
 
 // reusable-ui:
 import {
     // configs:
-    borderRadiuses,
-}                           from '@reusable-ui/borders'         // a border (stroke) management system
-import {
-    // configs:
-    spacers,
-}                           from '@reusable-ui/spacers'         // a spacer (gap) management system
-import {
-    // hooks:
-    useMergeClasses,
-}                           from '@reusable-ui/hooks'           // react helper hooks
+    typos,
+}                           from '@reusable-ui/typos'           // a typography management system
 import {
     // types:
-    DefaultTag,
-    DefaultRole,
+    VariantMixin,
     
     
     
     // hooks:
-    useTestSemantic,
-}                           from '@reusable-ui/generic'         // a base component
-import {
-    // hooks:
-    usesSizeVariant,
+    SizeVars,
+    ifSize          as basicIfSize,
+    usesSizeVariant as basicUsesSizeVariant,
+    useSizeVariant  as basicUseSizeVariant,
+    
     OrientationName,
     OrientationRuleOptions,
-    defaultInlineOrientationRuleOptions,
-    normalizeOrientationRule,
-    usesOrientationRule,
     OrientationVariant,
-    useOrientationVariant,
-    gradientOf,
-    ifNotOutlined,
-    outlinedOf,
-    usesBorder,
-    usesPadding,
+    extendsBorder,
+    extendsPadding,
+    
+    
+    
+    // configs:
+    basics,
 }                           from '@reusable-ui/basic'           // a base component
 import {
     // hooks:
-    ifActive,
-}                           from '@reusable-ui/indicator'       // a base component
-import {
-    // hooks:
-    usesThemeActive,
-    ifFocus,
-    ifArrive,
-    ifLeave,
-}                           from '@reusable-ui/control'         // a base component
-import {
+    ButtonStyle,
+    ButtonVariant,
+    ButtonType,
+    
+    
+    
     // styles:
     usesButtonLayout,
     usesButtonVariants,
@@ -93,10 +84,25 @@ import {
     
     
     
+    // configs:
+    buttons,
+    
+    
+    
     // react components:
     ButtonProps,
     Button,
 }                           from '@reusable-ui/button'          // a base component
+import {
+    // configs:
+    icons,
+    
+    
+    
+    // react components:
+    IconProps as IconIconProps,
+    Icon,
+}                           from '@reusable-ui/icon'            // an icon set
 
 
 
@@ -104,95 +110,72 @@ import {
 
 // layouts:
 
+//#region sizes
+export type SizeName = 'xs'|'sm'|'lg'|'xl' | (string & {})
+export type { SizeVars }
+
+
+
+export const ifSize = (sizeName: SizeName, styles: CssStyleCollection): CssRule => basicIfSize(sizeName, styles);
+
+
+
+/**
+ * Uses button icon sizes.  
+ * For example: `xs`, `sm`, `lg`, `xl`.
+ * @param configProps Customize the sizing definitions from configuration for each size in `options`.
+ * @param options Customize the size options.
+ * @returns A `VariantMixin<SizeVars>` represents sizing definitions for each size in `options`.
+ */
+export const usesSizeVariant = <TConfigProps extends CssConfigProps>(configProps : Refs<TConfigProps>, options = sizeOptions()): VariantMixin<SizeVars> => basicUsesSizeVariant(configProps, options);
+
+/**
+ * Gets all available size options.
+ * @returns A `SizeName[]` represents all available size options.
+ */
+export const sizeOptions = (): SizeName[] => ['xs', 'sm', 'lg', 'xl'];
+
+
+
+export interface SizeVariant {
+    size ?: SizeName
+}
+export const useSizeVariant = (props: SizeVariant) => basicUseSizeVariant(props);
+//#endregion sizes
+
 
 
 // styles:
-export const noBackground = () => {
-    // dependencies:
-    
-    // borders:
-    const [, borders] = usesBorder();
-    
-    
-    
-    return style({
-        ...variants([
-            ifNotOutlined({
-                // borders:
-                [borders.borderWidth]: '0px', // no_border if not explicitly `.outlined`
-            }),
-        ]),
-        ...states([
-            ifActive({
-                ...imports([
-                    outlinedOf(true), // keeps outlined (no background) variant
-                ]),
-            }),
-            ifFocus({
-                ...imports([
-                    outlinedOf(true), // keeps outlined (no background) variant
-                ]),
-            }),
-            ifArrive({
-                ...imports([
-                    outlinedOf(true), // keeps outlined (no background) variant
-                ]),
-            }),
-            ifPress({
-                ...imports([
-                    outlinedOf(true), // keeps outlined (no background) variant
-                ]),
-            }),
-        ]),
-        ...variants([
-            ifNotOutlined({
-                ...imports([
-                    outlinedOf(true), // keeps outlined (no background) variant
-                ]),
-            }),
-        ], { minSpecificityWeight: 4 }), // force to win with states' specificity weight
-    });
-};
-
 export const usesButtonIconLayout = (options?: OrientationRuleOptions) => {
-    // options:
-    options = normalizeOrientationRule(options, defaultOrientationRuleOptions);
-    const [orientationInlineSelector, orientationBlockSelector] = usesOrientationRule(options);
-    
-    
-    
     return style({
         ...imports([
             // layouts:
-            usesButtonLayout(),
+            usesButtonLayout(options),
         ]),
         ...style({
-            // layouts:
-            display           : 'inline-flex', // use inline flexbox, so it takes the width & height as we set
-            ...rule(orientationInlineSelector, { // inline
-                flexDirection : 'row',         // items are stacked horizontally
-            }),
-            ...rule(orientationBlockSelector,  { // block
-                flexDirection : 'column',      // items are stacked vertically
-            }),
-            justifyContent    : 'center',      // center items (text, icon, etc) horizontally
-            alignItems        : 'center',      // center items (text, icon, etc) vertically
-            flexWrap          : 'wrap',        // allows the items (text, icon, etc) to wrap to the next row if no sufficient width available
-            
-            
-            
-            // positions:
-            verticalAlign     : 'baseline',    // buttonIcon's text should be aligned with sibling text, so the buttonIcon behave like <span> wrapper
-            
-            
-            
-            // typos:
-            textAlign         : 'center',
-            
-            
-            
             // customize:
             ...usesCssProps(buttonIcons), // apply config's cssProps
+            
+            
+            
+            // borders:
+            
+            // let's Reusable-UI system to manage borderColor, borderStroke & borderRadius:
+            ...extendsBorder(buttonIcons),
+            
+            
+            
+            // spacings:
+            
+            // let's Reusable-UI system to manage paddingInline & paddingBlock:
+            ...extendsPadding(buttonIcons),
+        }),
+        ...vars({
+            //#region <Icon>
+            // sizes:
+            // fills the entire parent text's height:
+            [icons.size]  : `calc(1em * var(${basics.lineHeight}, ${typos.lineHeight}))`,
+            //#endregion <Icon>
         }),
     });
 };
@@ -211,36 +194,6 @@ export const usesButtonIconVariants = () => {
             
             // layouts:
             sizesRule,
-        ]),
-        ...variants([
-            rule(['.link', '.icon', '.ghost'], {
-                ...imports([
-                    noBackground(),
-                ]),
-            }),
-            rule('.ghost', {
-                ...style({
-                    // borders:
-                    boxShadow : ['none', '!important'], // no focus animation
-                    
-                    
-                    
-                    // customize:
-                    ...usesCssProps(usesPrefixedProps(buttonIcons, 'ghost')), // apply config's cssProps starting with ghost***
-                }),
-                ...states([
-                    ifArrive({
-                        // appearances:
-                        opacity: buttonIcons.ghostOpacityArrive, // increase the opacity to increase visibility
-                    }),
-                    ifLeave({
-                        ...imports([
-                            // backgrounds:
-                            gradientOf(false), // hides the gradient to increase invisibility
-                        ]),
-                    }),
-                ]),
-            }),
         ]),
     });
 };
@@ -271,54 +224,76 @@ export const useButtonIconStyleSheet = createUseStyleSheet(() => ({
 // configs:
 export const [buttonIcons, buttonIconValues, cssButtonIconConfig] = cssConfig(() => {
     return {
+        // borders:
+        borderRadius      : basics.borderRadius     as CssKnownProps['borderRadius'],
+        borderRadiusXs    : basics.borderRadiusSm   as CssKnownProps['borderRadius'],
+        borderRadiusSm    : basics.borderRadiusSm   as CssKnownProps['borderRadius'],
+        borderRadiusLg    : basics.borderRadiusLg   as CssKnownProps['borderRadius'],
+        borderRadiusXl    : basics.borderRadiusLg   as CssKnownProps['borderRadius'],
+        
+        
+        
         // spacings:
-        gapInline          : spacers.sm as CssKnownProps['gapInline'],
-        gapBlock           : spacers.sm as CssKnownProps['gapBlock' ],
-        gapInlineSm        : spacers.xs as CssKnownProps['gapInline'],
-        gapBlockSm         : spacers.xs as CssKnownProps['gapBlock' ],
-        gapInlineLg        : spacers.md as CssKnownProps['gapInline'],
-        gapBlockLg         : spacers.md as CssKnownProps['gapBlock' ],
+        paddingInline     : basics.paddingInline    as CssKnownProps['paddingInline'],
+        paddingBlock      : basics.paddingBlock     as CssKnownProps['paddingBlock' ],
+        paddingInlineXs   : basics.paddingInlineSm  as CssKnownProps['paddingInline'],
+        paddingBlockXs    : basics.paddingBlockSm   as CssKnownProps['paddingBlock' ],
+        paddingInlineSm   : basics.paddingInlineSm  as CssKnownProps['paddingInline'],
+        paddingBlockSm    : basics.paddingBlockSm   as CssKnownProps['paddingBlock' ],
+        paddingInlineLg   : basics.paddingInlineLg  as CssKnownProps['paddingInline'],
+        paddingBlockLg    : basics.paddingBlockLg   as CssKnownProps['paddingBlock' ],
+        paddingInlineXl   : basics.paddingInlineLg  as CssKnownProps['paddingInline'],
+        paddingBlockXl    : basics.paddingBlockLg   as CssKnownProps['paddingBlock' ],
+        
+        gapInline         : buttons.gapInline       as CssKnownProps['gapInline'],
+        gapBlock          : buttons.gapBlock        as CssKnownProps['gapBlock' ],
+        gapInlineXs       : buttons.gapInlineSm     as CssKnownProps['gapInline'],
+        gapBlockXs        : buttons.gapBlockSm      as CssKnownProps['gapBlock' ],
+        gapInlineSm       : buttons.gapInlineSm     as CssKnownProps['gapInline'],
+        gapBlockSm        : buttons.gapBlockSm      as CssKnownProps['gapBlock' ],
+        gapInlineLg       : buttons.gapInlineLg     as CssKnownProps['gapInline'],
+        gapBlockLg        : buttons.gapBlockLg      as CssKnownProps['gapBlock' ],
+        gapInlineXl       : buttons.gapInlineLg     as CssKnownProps['gapInline'],
+        gapBlockXl        : buttons.gapBlockLg      as CssKnownProps['gapBlock' ],
         
         
         
         // typos:
-        whiteSpace         : 'normal'   as CssKnownProps['whiteSpace'],
-        
-        
-        
-        // ghost style:
-        ghostOpacity       : 0.5        as CssKnownProps['opacity'],
-        ghostOpacityArrive : 1          as CssKnownProps['opacity'],
+        fontSize          : typos.fontSizeNm                                                as CssKnownProps['fontSize'],
+        fontSizeXs        : typos.fontSizeSm                                                as CssKnownProps['fontSize'],
+        fontSizeSm        : [['calc((', typos.fontSizeSm, '+', typos.fontSizeNm, ')/2)']]   as CssKnownProps['fontSize'],
+        fontSizeLg        : typos.fontSizeMd                                                as CssKnownProps['fontSize'],
+        fontSizeXl        : typos.fontSizeLg                                                as CssKnownProps['fontSize'],
     };
 }, { prefix: 'btni' });
 
 
 
 // react components:
+
+export type IconPosition = 'start'|'end'
+export interface IconProps
+    extends
+        // bases:
+        Partial<Pick<IconIconProps, 'icon'>>
+{
+    // appearances:
+    iconPosition ?: IconPosition
+}
+
 export interface ButtonIconProps
     extends
         // bases:
-        ButtonProps,
+        Omit<ButtonProps, 'size'>,
+        IconProps,
         
         // layouts:
-        OrientationVariant
+        SizeVariant
 {
-    // accessibilities:
-    label    ?: string
-    
-    
-    
-    // children:
-    children ?: React.ReactNode
 }
 const ButtonIcon = (props: ButtonIconProps): JSX.Element|null => {
     // styles:
-    const styleSheet         = useButtonIconStyleSheet();
-    
-    
-    
-    // variants:
-    const orientationVariant = useOrientationVariant(props);
+    const styleSheet = useButtonIconStyleSheet();
     
     
     
@@ -326,39 +301,15 @@ const ButtonIcon = (props: ButtonIconProps): JSX.Element|null => {
     const {
         // remove props:
         
-        // layouts:
-        orientation : _orientation,
-        
-        
-        
         // appearances:
-        btnStyle    : _btnStyle,
+        icon,
+        iconPosition = 'start',
         
         
         
-        // accessibilities:
-        label,
-        pressed,
-        
-        
-        
-        // variants:
-        outlined = _defaultOutlined,
-        mild     = _defaultMild,
+        // children:
+        children,
     ...restButtonProps} = props;
-    
-    
-    
-    // classes:
-    const variantClasses = useMergeClasses(
-        // preserves the original `variantClasses`:
-        props.variantClasses,
-        
-        
-        
-        // variants:
-        orientationVariant.class,
-    );
     
     
     
@@ -370,22 +321,13 @@ const ButtonIcon = (props: ButtonIconProps): JSX.Element|null => {
             
             
             
-            // accessibilities:
-            enabled={props.enabled ?? !(props.disabled ?? false)}
-            pressed={pressedFn}
-            
-            
-            
-            // variants:
-            outlined={outlined}
-            mild={mild}
-            
-            
-            
             // classes:
             mainClass={props.mainClass ?? styleSheet.main}
-            variantClasses={variantClasses}
-        />
+        >
+            { icon && (iconPosition === 'start') && <Icon icon={icon} /> }
+            { children }
+            { icon && (iconPosition === 'end'  ) && <Icon icon={icon} /> }
+        </Button>
     );
 };
 export {
@@ -394,3 +336,5 @@ export {
 }
 
 export type { OrientationName, OrientationVariant }
+
+export type { ButtonStyle, ButtonVariant, ButtonType }
