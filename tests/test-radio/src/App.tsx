199371aeac990e6a0f14089b563e31523b00fcbd
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-27 03:01:56 +0700

    added test-radio

---
+++ tests/test-radio/src/App.tsx
@@ -0,0 +1,57 @@
+import {
+    default as React,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Radio,
+} from '@reusable-ui/radio'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+const handleChange : React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log('checked = ', event.target.checked);
+}
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <article className='actions'>
+                    <Radio name='the-value' theme='primary' onChange={handleChange}>
+                        test &lt;Radio&gt;
+                    </Radio>
+                    <Radio name='the-value' theme='success' checkStyle='btn' nude={false}>
+                        test &lt;Radio&gt;
+                    </Radio>
+                    <Radio name='the-value' theme='danger' checkStyle='switch' nude={false}>
+                        test &lt;Radio&gt;
+                    </Radio>
+                </article>
+            </div>
+        </>
+    );
+}
+
+export default App;
