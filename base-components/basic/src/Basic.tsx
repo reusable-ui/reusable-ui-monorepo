a42fb678358d0cf1482a7ebcd5c77366065fdeaf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-26 13:38:08 +0700

    updated basic

--- base-components/basic/src/Basic.tsx
+++ base-components/basic/src/Basic.tsx
@@ -246,10 +246,12 @@ export interface NudeVars {
 const [nudes] = cssVar<NudeVars>();
 
 
+
+// by design: grandpa's `.nude` does not affect current `.nude`
 // parent not `.nude` -and- current not `.nude`:
-export const ifNotNude = (styles: CssStyleCollection): CssRule => rule(':not(:is(.nude&, &.nude))', styles);
+export const ifNotNude = (styles: CssStyleCollection): CssRule => rule(':not(.nude)&:where(:not(.nude))', styles);
 // parent is  `.nude` -or-  current is  `.nude`:
-export const ifNude    = (styles: CssStyleCollection): CssRule => rule(     ':is(.nude&, &.nude)' , styles);
+export const ifNude    = (styles: CssStyleCollection): CssRule => rule(':is(.nude&, &.nude)'            , styles);
 
 
 
