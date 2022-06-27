44cb057819a44c2d84b6d245a1cfdedcb485e43e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-28 03:51:59 +0700

    added toggle-menu-button

---
+++ simple-components/toggle-menu-button/src/ToggleMenuButton.tsx
@@ -0,0 +1,56 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// reusable-ui:
+import {
+    // hooks:
+    useEvent,
+    useMergeEvents,
+}                           from '@reusable-ui/hooks'           // react helper hooks
+export type {
+    // hooks:
+    OrientationName,
+    OrientationVariant,
+}                           from '@reusable-ui/basic'           // a base component
+import {
+    // hooks:
+    ToggleActiveProps,
+    useToggleActive,
+}                           from '@reusable-ui/indicator'       // a base component
+export {
+    // hooks:
+    ButtonStyle,
+    ButtonVariant,
+    ButtonType,
+}                           from '@reusable-ui/button'          // a base component
+import {
+    // react components:
+    ToggleButtonProps,
+    ToggleButton,
+}                           from '@reusable-ui/toggle-button'   // a base component
+
+
+
+// react components:
+export interface ToggleMenuButtonProps
+    extends
+        // bases:
+        ToggleButtonProps
+{
+}
+const ToggleMenuButton = (props: ToggleMenuButtonProps): JSX.Element|null => {
+    // jsx:
+    return (
+        <ToggleButton
+            // other props:
+            {...props}
+        />
+    );
+};
+export {
+    ToggleMenuButton,
+    ToggleMenuButton as default,
+}
