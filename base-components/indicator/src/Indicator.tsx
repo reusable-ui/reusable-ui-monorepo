45d9327f9a9f96c1ba797847c21a062d4ecfa3e2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-06-27 07:40:09 +0700

    updated indicator

--- base-components/indicator/src/Indicator.tsx
+++ base-components/indicator/src/Indicator.tsx
@@ -501,9 +501,6 @@ export const useTogglerActive = <TActiveChangeArg extends unknown = unknown>(pro
             
             // fire change dom event:
             if (changeEventTarget?.current) {
-                // *invert* the [checked] prop in order to the `onChange` event can be triggered *correctly*:
-                changeEventTarget.current.checked = !activeFn;
-                
                 // *hack*: trigger `onChange` event:
                 // side effect: toggles the [checked] prop:
                 changeEventTarget.current.dispatchEvent(new PointerEvent('click', { bubbles: true, cancelable: true, composed: true }));
