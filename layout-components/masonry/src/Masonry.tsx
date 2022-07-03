2a8d3bd7c747934e0350afbf95a0dd50e45feb1c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-03 19:25:15 +0700

    updated masonry

--- layout-components/masonry/src/Masonry.tsx
+++ layout-components/masonry/src/Masonry.tsx
@@ -17,6 +17,10 @@ import {
 }                           from 'react'
 
 // cssfn:
+import type {
+    // css known (standard) properties:
+    CssKnownProps,
+}                           from '@cssfn/css-types'                 // cssfn css specific types
 import {
     // rules:
     rule,
@@ -49,6 +53,10 @@ import {
 
 // reusable-ui:
 import {
+    // configs:
+    spacers,
+}                           from '@reusable-ui/spacers'             // a spacer (gap) management system
+import {
     // hooks:
     useTriggerRender,
     useEvent,
@@ -56,16 +64,6 @@ import {
     useMergeRefs,
     useMergeClasses,
 }                           from '@reusable-ui/hooks'               // react helper hooks
-import {
-    // hooks:
-    Result as ValResult,
-    
-    
-    
-    // react components:
-    ValidationProps,
-    ValidationProvider,
-}                           from '@reusable-ui/validations'         // a validation management system
 import {
     // hooks:
     usesSizeVariant,
@@ -93,16 +91,6 @@ import {
     ContentProps,
     Content,
 }                           from '@reusable-ui/content'             // a base component
-import {
-    // hooks:
-    ifValid,
-    ifInvalid,
-    usesValidInvalidState,
-    markValid,
-    markInvalid,
-    ValidatorHandler,
-    useValidInvalidState,
-}                           from '@reusable-ui/editable-control'    // a base component
 
 
 
@@ -136,45 +124,72 @@ export const usesMasonryLayout = (options?: OrientationVariantOptions) => {
             ...rule(orientationInlineSelector, { // inline
                 // layouts:
                 display             : 'inline-grid', // use css inline grid for layouting, the core of our Masonry layout
-                gridAutoFlow        : 'column', // items direction is to block & masonry's direction is to inline
-                gridAutoColumns     : masonries.itemsRaiseSize,
-                gridTemplateRows    : `repeat(auto-fill, minmax(${masonries.itemsMinColumnSize}, 1fr))`,
+                gridAutoFlow        : 'column',      // items direction is to block & masonry's direction is to inline
+                gridAutoColumns     : masonries.itemsRaiseRowHeight,
+                gridTemplateRows    : `repeat(auto-fill, minmax(${masonries.itemsMinColumnWidth}, 1fr))`,
                 
                 // child default sizes:
-                alignItems          : 'stretch', // each item fills the entire Masonry's column height
-             // justifyItems        : 'stretch', // distorting the item's width a bit for consistent multiplies of `itemsRaiseSize` // causing the ResizeObserver doesn't work
-                justifyItems        : 'start',   // let's the item to resize so the esizeObserver will work
+                alignItems          : 'stretch',     // each item fills the entire Masonry's column height
+             // justifyItems        : 'stretch',     // distorting the item's width a bit for consistent multiplies of `itemsRaiseRowHeight` // causing the ResizeObserver doesn't work
+                justifyItems        : 'start',       // let's the item to resize so the esizeObserver will work
             }),
             ...rule(orientationBlockSelector , { // block
                 // layouts:
-                display             : 'grid', // use css block grid for layouting, the core of our Masonry layout
-                gridAutoFlow        : 'row', // items direction is to inline & masonry's direction is to block
-                gridAutoRows        : masonries.itemsRaiseSize,
-                gridTemplateColumns : `repeat(auto-fill, minmax(${masonries.itemsMinColumnSize}, 1fr))`,
+                display             : 'grid',        // use css block grid for layouting, the core of our Masonry layout
+                gridAutoFlow        : 'row',         // items direction is to inline & masonry's direction is to block
+                gridAutoRows        : masonries.itemsRaiseRowHeight,
+                gridTemplateColumns : `repeat(auto-fill, minmax(${masonries.itemsMinColumnWidth}, 1fr))`,
                 
                 // child default sizes:
-                justifyItems        : 'stretch', // each item fills the entire Masonry's column width
-             // alignItems          : 'stretch', // distorting the item's height a bit for consistent multiplies of `itemsRaiseSize` // causing the ResizeObserver doesn't work
-                alignItems          : 'start',   // let's the item to resize so the esizeObserver will work
+                justifyItems        : 'stretch',     // each item fills the entire Masonry's column width
+             // alignItems          : 'stretch',     // distorting the item's height a bit for consistent multiplies of `itemsRaiseRowHeight` // causing the ResizeObserver doesn't work
+                alignItems          : 'start',       // let's the item to resize so the esizeObserver will work
             }),
             
             
             
             // spacings:
             ...rule(orientationInlineSelector, { // inline
-                columnGap           : [[0], '!important'], // strip out the `columnGap` because it will conflict with masonry's direction
+                gapInline           : [0, '!important'], // strip out the `gapInline` because it will conflict with masonry's direction
             }),
             ...rule(orientationBlockSelector , { // block
-                rowGap              : [[0], '!important'], // strip out the `rowGap` because it will conflict with masonry's direction
+                gapBlock            : [0, '!important'], // strip out the `gapBlock`  because it will conflict with masonry's direction
             }),
             
             
             
             // children:
             ...children('*', {
+                // sizes:
                 ...rule(parentOrientationInlineSelector, { // inline
+                    gridRowEnd    : ['unset', '!important'], // clear from residual effect from blockStyle (if was)
                 }),
                 ...rule(parentOrientationBlockSelector , { // block
+                    gridColumnEnd : ['unset', '!important'], // clear from residual effect from inlineStyle (if was)
+                }),
+                
+                
+                
+                // spacings:
+                ...rule(':not(.firstRow)', {
+                    ...rule(parentOrientationInlineSelector, { // inline
+                        /*
+                        * we use `marginInlineStart` as the replacement of the stripped out `gapInline`
+                        * we use `marginInlineStart` instead of `marginInlineEnd`
+                        * because finding grid's items at the first row is much easier than at the last row
+                        * (we don't need to count the number of grid's item)
+                        */
+                        marginInlineStart : masonries.gapBlock,
+                    }),
+                    ...rule(parentOrientationBlockSelector , { // block
+                        /*
+                        * we use `marginBlockStart` as the replacement of the stripped out `gapBlock`
+                        * we use `marginBlockStart` instead of `marginBlockEnd`
+                        * because finding grid's items at the first row is much easier than at the last row
+                        * (we don't need to count the number of grid's item)
+                        */
+                        marginBlockStart  : masonries.gapBlock,
+                    }),
                 }),
             }),
             
@@ -203,33 +218,6 @@ export const usesMasonryVariants = () => {
         ]),
     });
 };
-export const usesMasonryStates = () => {
-    // dependencies:
-    
-    // states:
-    const [validInvalidStateRule] = usesValidInvalidState();
-    
-    
-    
-    return style({
-        ...imports([
-            // validations:
-            validInvalidStateRule,
-        ]),
-        ...states([
-            ifValid({
-                ...imports([
-                    markValid(),
-                ]),
-            }),
-            ifInvalid({
-                ...imports([
-                    markInvalid(),
-                ]),
-            }),
-        ]),
-    });
-};
 
 export const useMasonryStyleSheet = createUseStyleSheet(() => ({
     ...imports([
@@ -238,9 +226,6 @@ export const useMasonryStyleSheet = createUseStyleSheet(() => ({
         
         // variants:
         usesMasonryVariants(),
-        
-        // states:
-        usesMasonryStates(),
     ]),
 }), { id: 'fiuyy1jxpx' }); // a unique salt for SSR support, ensures the server-side & client-side have the same generated class names
 
@@ -249,7 +234,24 @@ export const useMasonryStyleSheet = createUseStyleSheet(() => ({
 // configs:
 export const [masonries, masonryValues, cssMasonryConfig] = cssConfig(() => {
     return {
-        /* no config props yet */
+        // sizes:
+        itemsRaiseRowHeight   : '3px'               as CssKnownProps['blockSize'],
+        itemsRaiseRowHeightSm : '2px'               as CssKnownProps['blockSize'],
+        itemsRaiseRowHeightLg : '4px'               as CssKnownProps['blockSize'],
+        
+        itemsMinColumnWidth   : 'calc(5 * 40px)'    as CssKnownProps['columnWidth'],
+        itemsMinColumnWidthSm : 'calc(3 * 40px)'    as CssKnownProps['columnWidth'],
+        itemsMinColumnWidthLg : 'calc(8 * 40px)'    as CssKnownProps['columnWidth'],
+        
+        
+        
+        // spacings:
+        gapInline             : spacers.sm          as CssKnownProps['gapInline'],
+        gapInlineSm           : spacers.xs          as CssKnownProps['gapInline'],
+        gapInlineLg           : spacers.md          as CssKnownProps['gapInline'],
+        gapBlock              : spacers.sm          as CssKnownProps['gapBlock' ],
+        gapBlockSm            : spacers.xs          as CssKnownProps['gapBlock' ],
+        gapBlockLg            : spacers.md          as CssKnownProps['gapBlock' ],
     };
 }, { prefix: 'msry' });
 
