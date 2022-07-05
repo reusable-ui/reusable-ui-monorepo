4242771654dafe2bc13afe54e08a2aaac18dc983
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-06 05:03:51 +0700

    added test-badge

---
+++ tests/test-badge/src/App.tsx
@@ -0,0 +1,51 @@
+import {
+    default as React,
+    useState,
+    useRef,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Badge,
+} from '@reusable-ui/badge'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    const btnRef = useRef<HTMLButtonElement>(null);
+    
+
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea inventore debitis, tempore sapiente possimus ratione velit voluptatibus quidem accusamus odio illo voluptate esse delectus et fugiat voluptatum voluptatem. Fuga, provident.</p>
+                <button ref={btnRef}>
+                    I'm here
+                </button>
+                <Badge targetRef={btnRef} theme='danger' active={true} popupPlacement='right-start' popupOffset={-10} popupShift={-10}>
+                    new!
+                </Badge>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea inventore debitis, tempore sapiente possimus ratione velit voluptatibus quidem accusamus odio illo voluptate esse delectus et fugiat voluptatum voluptatem. Fuga, provident.</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
