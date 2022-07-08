2dbc234202ae557a1ee112c467a4b2d585c2abb9
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-08 19:17:39 +0700

    added test-tooltip

---
+++ tests/test-tooltip/src/App.tsx
@@ -0,0 +1,51 @@
+import {
+    default as React,
+    useState,
+    useRef,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Tooltip,
+} from '@reusable-ui/tooltip'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    const btnRef = useRef<HTMLButtonElement>(null);
+    
+
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea inventore debitis, tempore sapiente possimus ratione velit voluptatibus quidem accusamus odio illo voluptate esse delectus et fugiat voluptatum voluptatem. Fuga, provident.</p>
+                <button ref={btnRef}>
+                    I'm here
+                </button>
+                <Tooltip targetRef={btnRef} theme='warning' active={true}>
+                    This is cool!
+                </Tooltip>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea inventore debitis, tempore sapiente possimus ratione velit voluptatibus quidem accusamus odio illo voluptate esse delectus et fugiat voluptatum voluptatem. Fuga, provident.</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
