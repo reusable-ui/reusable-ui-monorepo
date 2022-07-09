439b12454518cdb994d4a72eba0fd39969e46c41
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-09 20:21:41 +0700

    added test-dropdown

---
+++ tests/test-dropdown/src/App.tsx
@@ -0,0 +1,56 @@
+import {
+    default as React,
+    useState,
+    useRef,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Dropdown,
+} from '@reusable-ui/dropdown'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    const btnRef = useRef<HTMLButtonElement>(null);
+    const [showDropdown, setShowDropdown] = useState<boolean>(false);
+    
+
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea inventore debitis, tempore sapiente possimus ratione velit voluptatibus quidem accusamus odio illo voluptate esse delectus et fugiat voluptatum voluptatem. Fuga, provident.</p>
+                <button ref={btnRef} onClick={() => setShowDropdown(!showDropdown)}>
+                    I'm here
+                </button>
+                <Dropdown targetRef={btnRef} theme='primary' active={showDropdown}>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur</p>
+                        <p>Lorem ipsum dolor sit amet consectetur</p>
+                        <p>Lorem ipsum dolor sit amet consectetur</p>
+                    </div>
+                </Dropdown>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea inventore debitis, tempore sapiente possimus ratione velit voluptatibus quidem accusamus odio illo voluptate esse delectus et fugiat voluptatum voluptatem. Fuga, provident.</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
