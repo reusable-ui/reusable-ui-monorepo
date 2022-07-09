ad424575a7b29dfd0b0dab90174cfce72061ad24
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-10 01:56:31 +0700

    added dropdown-list

---
+++ menu-components/dropdown-list/src/DropdownList.tsx
@@ -0,0 +1,251 @@
+// react:
+import {
+    // react:
+    default as React,
+    
+    
+    
+    // hooks:
+    useRef,
+    useEffect,
+}                           from 'react'
+
+// cssfn:
+import type {
+    // css known (standard) properties:
+    CssKnownProps,
+}                           from '@cssfn/css-types'             // cssfn css specific types
+import {
+    // styles:
+    style,
+    imports,
+}                           from '@cssfn/cssfn'                 // writes css in javascript
+import {
+    // style sheets:
+    createUseStyleSheet,
+}                           from '@cssfn/cssfn-react'           // writes css in react hook
+import {
+    cssConfig,
+    
+    
+    
+    // utilities:
+    usesCssProps,
+}                           from '@cssfn/css-config'            // reads/writes css variables configuration
+
+// reusable-ui:
+import {
+    // hooks:
+    useEvent,
+    useMergeEvents,
+    useMergeRefs,
+}                           from '@reusable-ui/hooks'           // react helper hooks
+import type {
+    // react components:
+    GenericProps,
+}                           from '@reusable-ui/generic'         // a generic component
+import {
+    // hooks:
+    usesSizeVariant,
+    OrientationName,
+    OrientationVariantOptions,
+    OrientationVariant,
+    useOrientationVariant,
+}                           from '@reusable-ui/basic'           // a base component
+import {
+    // hooks:
+    useActivePassiveState,
+    ActiveChangeEvent,
+    ToggleActiveProps,
+}                           from '@reusable-ui/indicator'       // a base component
+export type {
+    // types:
+    PopupPlacement,
+    PopupMiddleware,
+    PopupStrategy,
+    PopupPosition,
+    PopupSide,
+}                           from '@reusable-ui/collapse'        // a base component
+import {
+    // hooks:
+    defaultOrientationRuleOptions,
+    
+    
+    
+    // styles:
+    usesCollapseLayout,
+    usesCollapseVariants,
+    usesCollapseStates,
+    
+    
+    
+    // react components:
+    CollapseProps,
+    Collapse,
+}                           from '@reusable-ui/collapse'        // a base component
+
+
+
+// utilities:
+const isSelfOrDescendantOf = (element: Element, desired: Element): boolean => {
+    let parent: Element|null = element;
+    do {
+        if (parent === desired) return true; // confirmed
+        
+        // let's try again:
+        parent = parent.parentElement;
+    } while (parent);
+    
+    
+    
+    return false; // not the descendant of desired
+};
+
+
+
+// react components:
+
+export type DropdownListCloseType = 'shortcut'|'blur'
+export interface DropdownListActiveChangeEvent extends ActiveChangeEvent {
+    closeType : DropdownListCloseType
+}
+export interface DropdownListAction<TDropdownListActiveChangeEvent extends DropdownListActiveChangeEvent = DropdownListActiveChangeEvent>
+    extends
+        // accessibilities:
+        Pick<ToggleActiveProps<TDropdownListActiveChangeEvent>, 'onActiveChange'>
+{
+}
+
+export interface DropdownListComponentUIProps<TDropdownListActiveChangeEvent extends DropdownListActiveChangeEvent = DropdownListActiveChangeEvent>
+    extends
+        // accessibilities:
+        DropdownListAction<TDropdownListActiveChangeEvent>
+{
+    // accessibilities:
+    tabIndex ?: number
+}
+export interface DropdownListComponentProps<TDropdownListActiveChangeEvent extends DropdownListActiveChangeEvent = DropdownListActiveChangeEvent>
+    extends
+        // component ui:
+        DropdownListComponentUIProps<TDropdownListActiveChangeEvent>
+{
+    // refs:
+    dropdownListRef ?: React.Ref<Element> // setter ref
+    
+    
+    
+    // components:
+    children     : React.ReactElement<(GenericProps<Element>|React.HTMLAttributes<HTMLElement>|React.SVGAttributes<SVGElement>) & DropdownListComponentUIProps<TDropdownListActiveChangeEvent>>
+}
+
+export interface DropdownListProps<TElement extends Element = HTMLElement, TDropdownListActiveChangeEvent extends DropdownListActiveChangeEvent = DropdownListActiveChangeEvent>
+    extends
+        // bases:
+        Omit<CollapseProps<TElement>,
+            // children:
+            |'children' // we use `children` prop as a dropdownList component
+        >,
+        
+        // components:
+        DropdownListComponentProps<TDropdownListActiveChangeEvent>
+{
+}
+const DropdownList = <TElement extends Element = HTMLElement, TDropdownListActiveChangeEvent extends DropdownListActiveChangeEvent = DropdownListActiveChangeEvent>(props: DropdownListProps<TElement, TDropdownListActiveChangeEvent>): JSX.Element|null => {
+    // styles:
+    const styleSheet         = useDropdownListStyleSheet();
+    
+    
+    
+    // variants:
+    const orientationVariant = useOrientationVariant(props);
+    const isOrientationBlock = ((orientationVariant.class || defaultOrientationRuleOptions.defaultOrientation) === 'block');
+    
+    
+    
+    // states:
+    
+    // accessibilities:
+    const activePassiveState = useActivePassiveState<TElement>(props);
+    const isVisible          = activePassiveState.isVisible; // visible = showing, shown, hidding ; !visible = hidden
+    
+    
+    
+    // rest props:
+    const {
+        // accessibilities:
+        onActiveChange,
+        
+        
+        
+        // components:
+        dropdownListRef,
+        tabIndex,
+        children: dropdownListComponent,
+    ...restCollapseProps} = props;
+    
+    
+    
+    // verifies:
+    React.Children.only(dropdownListComponent);
+    if (!React.isValidElement<GenericProps<Element>|React.HTMLAttributes<HTMLElement>|React.SVGAttributes<SVGElement>>(dropdownListComponent)) throw Error('Invalid child element.');
+    const isReusableUiComponent : boolean = (typeof(dropdownListComponent.type) !== 'string');
+    
+    
+    
+    // jsx:
+    return (
+        <Collapse<TElement>
+            // other props:
+            {...restCollapseProps}
+            
+            
+            
+            // variants:
+            nude={props.nude ?? true}
+            
+            
+            
+            // classes:
+            mainClass={props.mainClass ?? styleSheet.main}
+            
+            
+            
+            // popups:
+            popupPlacement={props.popupPlacement ?? (isOrientationBlock ? 'bottom' : 'right')}
+            popupAutoFlip={props.popupAutoFlip ?? true}
+            popupAutoShift={props.popupAutoShift ?? true}
+            
+            
+            
+            // handlers:
+            onKeyDown={handleKeyDown}
+            onAnimationEnd={handleAnimationEnd}
+        >
+            {React.cloneElement<GenericProps<Element> & React.RefAttributes<Element> & DropdownListComponentUIProps<TDropdownListActiveChangeEvent>>(dropdownListComponent,
+                // props:
+                {
+                    // refs:
+                    [isReusableUiComponent ? 'elmRef' : 'ref'] : mergedDropdownListRef,
+                    
+                    
+                    
+                    // accessibilities:
+                    tabIndex : dropdownListComponent.props.tabIndex ?? tabIndex,
+                    
+                    
+                    
+                    // handlers:
+                    ...(isReusableUiComponent ? {
+                        onActiveChange : handleActiveChange,
+                    } : null),
+                }
+            )}
+        </Collapse>
+    );
+};
+export {
+    DropdownList,
+    DropdownList as default,
+}
+
+export type { OrientationName, OrientationVariant }
