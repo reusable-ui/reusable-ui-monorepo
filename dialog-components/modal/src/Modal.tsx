94c4e16ab8d73b2516521cd9c0aa7e48f337d11f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-19 06:24:55 +0700

    updated modal

--- dialog-components/modal/src/Modal.tsx
+++ dialog-components/modal/src/Modal.tsx
@@ -127,6 +127,123 @@ import {
 
 
 
+// utilities:
+const sortByTabIndex = (a: Element|undefined, b: Element|undefined) => {
+    if (!a || !b) return 0;
+    return ((a as HTMLElement|SVGElement).tabIndex ?? 0) - ((b as HTMLElement|SVGElement).tabIndex ?? 0);
+};
+export const getFocusableElements = (parentElement : Element): Element[] => {
+    return (
+        Array.from(parentElement.children)
+        .flatMap((liElement) => {
+            const focusableElements = (
+                Array.from(liElement.querySelectorAll(
+`:is(
+    a,
+    button,
+    textarea,
+    select,
+    details,
+    input:not([type="hidden"]),
+    [tabindex]:not([tabindex^="-"])
+):not(
+    :is(
+        [disabled]:not([disabled="false"]),
+        [aria-disabled]:not([aria-disabled="false"])
+    )
+)`              ))
+                .sort(sortByTabIndex)
+            );
+            return {
+                primary : focusableElements.at(0),
+                items   : focusableElements,
+            };
+        })
+        .sort((x, y) => sortByTabIndex(x.primary, y.primary))
+        .flatMap((item) => item.items)
+    )
+};
+export const setFocusAt    = (parentElement : Element, index: number,) => {
+    // get all possible focusable elements within <parentElement>:
+    const focusableElements = getFocusableElements(parentElement);
+    
+    
+    
+    // find the target element:
+    let targetElement = focusableElements.at(index);
+    
+    
+    
+    // set focus:
+    (targetElement as HTMLElement|SVGElement|undefined)?.focus?.();
+};
+export const setFocusFirst = (parentElement : Element) => {
+    setFocusAt(parentElement, 0);
+};
+export const setFocusLast  = (parentElement : Element) => {
+    setFocusAt(parentElement, -1);
+};
+export const setFocusPrev  = (parentElement : Element) => {
+    // get all possible focusable elements within <parentElement>:
+    const focusableElements = getFocusableElements(parentElement);
+    
+    
+    
+    // get the current focusable element (if any):
+    const currentFocusedElement = document.activeElement;
+    const currentFocusedElementIndex = (
+        currentFocusedElement
+        ?
+        focusableElements.indexOf(currentFocusedElement)
+        :
+        -1
+    );
+    
+    
+    
+    // set focus:
+    if (currentFocusedElementIndex < 0) {
+        // focus to the last focusable element:
+        (focusableElements.at(-1) as HTMLElement|SVGElement|undefined)?.focus();
+    }
+    else {
+        // focus to the prev focusable element:
+        // wrap to the last for the first:
+        (focusableElements.at(currentFocusedElementIndex -1) as HTMLElement|SVGElement|undefined)?.focus();
+    }
+};
+export const setFocusNext  = (parentElement : Element) => {
+    // get all possible focusable elements within <parentElement>:
+    const focusableElements = getFocusableElements(parentElement);
+    
+    
+    
+    // get the current focusable element (if any):
+    const currentFocusedElement = document.activeElement;
+    const currentFocusedElementIndex = (
+        currentFocusedElement
+        ?
+        focusableElements.indexOf(currentFocusedElement)
+        :
+        -1
+    );
+    
+    
+    
+    // set focus:
+    if (currentFocusedElementIndex < 0) {
+        // focus to the first focusable element:
+        (focusableElements.at(0) as HTMLElement|SVGElement|undefined)?.focus();
+    }
+    else {
+        // focus to the next focusable element:
+        // wrap to the first for the last:
+        ((focusableElements.at(currentFocusedElementIndex +1) ?? focusableElements.at(0)) as HTMLElement|SVGElement|undefined)?.focus();
+    }
+};
+
+
+
 // hooks:
 
 // accessibilities:
@@ -486,6 +603,10 @@ const Modal = <TElement extends Element = HTMLElement, TModalActiveChangeEvent e
                 // [esc] key pressed => request to hide the <Modal>:
                 handleActiveChange?.({ newActive: false, actionType: 'shortcut' } as TModalActiveChangeEvent);
             }
+            else if (isKeyOf('tab'))
+            {
+                setFocusNext(event.currentTarget);
+            }
             else if (
                 isKeyOf('pagedown'  ) ||
                 isKeyOf('pageup'    ) ||
@@ -495,7 +616,7 @@ const Modal = <TElement extends Element = HTMLElement, TModalActiveChangeEvent e
                 isKeyOf('arrowup'   ) ||
                 isKeyOf('arrowleft' ) ||
                 isKeyOf('arrowright') ||
-                isKeyOf('space')
+                isKeyOf('space'     )
             )
             {
                 // do nothing
