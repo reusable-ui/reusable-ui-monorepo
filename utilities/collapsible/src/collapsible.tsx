d79bce66c467882aa0ba8c9d275f265862ec7d60
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-22 04:42:50 +0700

    updated collapsible

--- utilities/collapsible/src/collapsible.tsx
+++ utilities/collapsible/src/collapsible.tsx
@@ -1,8 +1,121 @@
+// cssfn:
+import type {
+    // css known (standard) properties:
+    CssKnownProps,
+    
+    
+    
+    // cssfn properties:
+    CssRule,
+    
+    CssStyleCollection,
+}                           from '@cssfn/css-types'             // cssfn css specific types
+import {
+    // rules:
+    rule,
+    states,
+    keyframes,
+    
+    
+    
+    // styles:
+    style,
+    vars,
+    imports,
+}                           from '@cssfn/cssfn'                 // writes css in javascript
+import {
+    // style sheets:
+    createUseStyleSheet,
+}                           from '@cssfn/cssfn-react'           // writes css in react hook
+import {
+    // utilities:
+    cssVar,
+}                           from '@cssfn/css-var'               // strongly typed of css variables
+import {
+    cssConfig,
+    
+    
+    
+    // utilities:
+    usesCssProps,
+}                           from '@cssfn/css-config'            // reads/writes css variables configuration
+
 // reusable-ui:
 import type {
     // hooks:
     EventHandler,
 }                           from '@reusable-ui/hooks'           // react helper hooks
+import {
+    // types:
+    StateMixin,
+    
+    
+    
+    // hooks:
+    usesSizeVariant,
+    ThemeName,
+    usesThemeConditional,
+    outlinedOf,
+    mildOf,
+    usesAnim,
+    fallbackNoneFilter,
+    
+    
+    
+    // styles:
+    usesBasicLayout,
+    usesBasicVariants,
+    
+    
+    
+    // react components:
+    BasicProps,
+    Basic,
+}                           from '@reusable-ui/basic'           // a base component
+
+
+
+// hooks:
+
+// accessibilities:
+
+//#region expandable
+export interface ExpandableVars {
+    filter : any
+    anim   : any
+}
+const [expandables] = cssVar<ExpandableVars>();
+
+{
+    const [, , animRegistry] = usesAnim();
+    animRegistry.registerFilter(expandables.filter);
+    animRegistry.registerAnim(expandables.anim);
+}
+
+
+
+// .expanded will be added after expanding-animation done:
+const selectorIfExpanded   = '.expanded'
+// .expanding = styled expand, [open] = native expand:
+const selectorIfExpanding  = ':is(.expanding, [open]):not(.expanded)'
+// .collapsing will be added after loosing expand and will be removed after collapsing-animation done:
+const selectorIfCollapsing = '.collapsing'
+// if all above are not set => collapsed:
+const selectorIfCollapsed  = ':not(:is(.expanded, .expanding, [open], .collapsing))'
+
+
+
+export const ifExpanded          = (styles: CssStyleCollection): CssRule => rule(selectorIfExpanded  , styles);
+export const ifExpanding         = (styles: CssStyleCollection): CssRule => rule(selectorIfExpanding , styles);
+export const ifCollapsing        = (styles: CssStyleCollection): CssRule => rule(selectorIfCollapsing, styles);
+export const ifCollapsed         = (styles: CssStyleCollection): CssRule => rule(selectorIfCollapsed , styles);
+
+export const ifExpand            = (styles: CssStyleCollection): CssRule => rule([selectorIfExpanding, selectorIfExpanded                                           ], styles);
+export const ifCollapse          = (styles: CssStyleCollection): CssRule => rule([                                         selectorIfCollapsing, selectorIfCollapsed], styles);
+export const ifExpandCollapsing  = (styles: CssStyleCollection): CssRule => rule([selectorIfExpanding, selectorIfExpanded, selectorIfCollapsing                     ], styles);
+export const ifExpandingCollapse = (styles: CssStyleCollection): CssRule => rule([selectorIfExpanding,                     selectorIfCollapsing, selectorIfCollapsed], styles);
+
+//#endregion expandable
 
 
 
