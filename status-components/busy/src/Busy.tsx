661f8c6e2c0ff2e8a06c8d9137bd0640bd5fe338
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-23 22:07:27 +0700

    updated busy

--- status-components/busy/src/Busy.tsx
+++ status-components/busy/src/Busy.tsx
@@ -31,7 +31,7 @@ import {
 import type {
     // type:
     ExpandedChangeEvent,
-}                           from '@reusable-ui/expandable'      // a capability of UI to expand/reduce its size or toggle the visibility
+}                           from '@reusable-ui/expandables'     // a capability of UI to expand/reduce its size or toggle the visibility
 import {
     // hooks:
     usesSizeVariant,
