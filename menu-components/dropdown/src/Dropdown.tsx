7ccb70e872b39b708851054c84a37916e2ce5398
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-07-24 02:18:49 +0700

    updated dropdown

--- menu-components/dropdown/src/Dropdown.tsx
+++ menu-components/dropdown/src/Dropdown.tsx
@@ -182,7 +182,10 @@ export const useDropdownStyleSheet = createUseStyleSheet(() => ({
 export const [dropdowns, dropdownValues, cssDropdownConfig] = cssConfig(() => {
     return {
         // borders:
-        dropdownUiBoxShadow : [[0, 0, '10px', 'rgba(0,0,0,0.5)']] as CssKnownProps['boxShadow'],
+        // dropdownUiBoxShadow : [[0, 0, '10px', 'rgba(0,0,0,0.5)']] as CssKnownProps['boxShadow'], // doesn't work perfectly with borderRadius
+        filter: [
+            ['drop-shadow(', 0, 0, '10px', 'rgba(0,0,0,0.5)', ')'],
+        ] as CssKnownProps['filter'],
     };
 }, { prefix: 'ddwn' });
 
