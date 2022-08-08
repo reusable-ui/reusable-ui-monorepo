eab2bc4717b213af23a4ebf6b8a4d56990d713fd
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-08 09:58:57 +0700

    updated activatable

---
+++ states/activatable/src/activatable.ts
@@ -0,0 +1,243 @@
+// react:
+import {
+    // react:
+    default as React,
+    
+    
+    
+    // hooks:
+    useState,
+}                           from 'react'
+
+// cssfn:
+import type {
+    // types:
+    Factory,
+}                           from '@cssfn/types'                 // cssfn general types
+import type {
+    // css known (standard) properties:
+    CssKnownProps,
+    
+    
+    
+    // cssfn properties:
+    CssRule,
+    
+    CssStyleCollection,
+}                           from '@cssfn/css-types'             // cssfn css specific types
+import {
+    // rules:
+    rule,
+    states,
+    
+    
+    
+    // styles:
+    style,
+    vars,
+}                           from '@cssfn/cssfn'                 // writes css in javascript
+import {
+    // utilities:
+    CssVars,
+    cssVars,
+}                           from '@cssfn/css-vars'              // strongly typed of css variables
+
+// reusable-ui utilities:
+import {
+    // hooks:
+    useEvent,
+}                           from '@reusable-ui/hooks'           // react helper hooks
+import {
+    // hooks:
+    SemanticProps,
+    useSemantic,
+}                           from '@reusable-ui/semantics'       // a semantic management system for react web components
+import {
+    // hooks:
+    usePropActive,
+    
+    
+    
+    // react components:
+    AccessibilityProps,
+}                           from '@reusable-ui/accessibilities' // an accessibility management system
+
+// reusable-ui features:
+import {
+    // hooks:
+    usesAnimation,
+}                           from '@reusable-ui/animation'       // animation stuff of UI
+
+
+
+// hooks:
+
+// states:
+
+//#region activatable
+export interface ActivatableVars {
+    filter : any
+    anim   : any
+}
+const [activatableVars] = cssVars<ActivatableVars>();
+
+{
+    const {animationRegistry: {registerFilter, registerAnim}} = usesAnimation();
+    registerFilter(activatableVars.filter);
+    registerAnim(activatableVars.anim);
+}
+
+
+
+// .actived will be added after activating-animation done:
+const selectorIfActived     = '.actived'
+// [aria-checked],[aria-pressed],[aria-selected] = styled active, :checked = native active:
+const selectorIfActivating  = ':is([aria-checked]:not([aria-checked="false"]), [aria-pressed]:not([aria-pressed="false"]), [aria-selected]:not([aria-selected="false"]), :checked):not(.actived)'
+// .passivating will be added after loosing active and will be removed after deactivating-animation done:
+const selectorIfPassivating = '.passivating'
+// if all above are not set => passived:
+const selectorIfPassived    = ':not(:is(.actived, [aria-checked]:not([aria-checked="false"]), [aria-pressed]:not([aria-pressed="false"]), [aria-selected]:not([aria-selected="false"]), :checked, .passivating))'
+
+
+
+export const ifActived           = (styles: CssStyleCollection): CssRule => rule(selectorIfActived    , styles);
+export const ifActivating        = (styles: CssStyleCollection): CssRule => rule(selectorIfActivating , styles);
+export const ifPassivating       = (styles: CssStyleCollection): CssRule => rule(selectorIfPassivating, styles);
+export const ifPassived          = (styles: CssStyleCollection): CssRule => rule(selectorIfPassived   , styles);
+
+export const ifActive            = (styles: CssStyleCollection): CssRule => rule([selectorIfActivating, selectorIfActived                                           ], styles);
+export const ifPassive           = (styles: CssStyleCollection): CssRule => rule([                                         selectorIfPassivating, selectorIfPassived], styles);
+export const ifActivePassivating = (styles: CssStyleCollection): CssRule => rule([selectorIfActivating, selectorIfActived, selectorIfPassivating                    ], styles);
+
+
+
+export interface ActivatableStuff { activatableRule: Factory<CssRule>, activatableVars: CssVars<ActivatableVars> }
+export interface ActivatableConfig {
+    filterActive ?: CssKnownProps['filter'   ]
+    
+    animActive   ?: CssKnownProps['animation']
+    animPassive  ?: CssKnownProps['animation']
+}
+/**
+ * Adds a capability of UI to be highlighted/selected/activated.
+ * @param config  A configuration of `activatableRule`.
+ * @returns A `ActivatableStuff` represents an activatable state.
+ */
+export const usesActivatable = (config?: ActivatableConfig): ActivatableStuff => {
+    return {
+        activatableRule: () => style({
+            ...states([
+                ifActived({
+                    ...vars({
+                        [activatableVars.filter] : config?.filterActive,
+                    }),
+                }),
+                ifActivating({
+                    ...vars({
+                        [activatableVars.filter] : config?.filterActive,
+                        [activatableVars.anim  ] : config?.animActive,
+                    }),
+                }),
+                ifPassivating({
+                    ...vars({
+                        [activatableVars.filter] : config?.filterActive,
+                        [activatableVars.anim  ] : config?.animPassive,
+                    }),
+                }),
+            ]),
+        }),
+        activatableVars,
+    };
+};
+
+
+
+export interface ActivatableProps
+    extends
+        Partial<Pick<AccessibilityProps, 'active'|'inheritActive'>>
+{
+}
+
+const checkableCtrls = [
+    'checkbox',
+    'radio',
+];
+export const useActivatable = <TElement extends Element = HTMLElement>(props: ActivatableProps & SemanticProps) => {
+    // fn props:
+    const propActive  = usePropActive(props);
+    const {tag, role} = useSemantic(props);
+    
+    
+    
+    // states:
+    const [actived,   setActived  ] = useState<boolean>(propActive); // true => active, false => passive
+    const [animating, setAnimating] = useState<boolean|null>(null);  // null => no-animation, true => activating-animation, false => deactivating-animation
+    
+    
+    
+    /*
+     * state is active/passive based on [controllable active]
+     * [uncontrollable active] is not supported
+     */
+    const activeFn : boolean = propActive /*controllable*/;
+    
+    if (actived !== activeFn) { // change detected => apply the change & start animating
+        setActived(activeFn);   // remember the last change
+        setAnimating(activeFn); // start activating-animation/deactivating-animation
+    } // if
+    
+    
+    
+    // handlers:
+    const handleAnimationEnd = useEvent<React.AnimationEventHandler<TElement>>((event) => {
+        // conditions:
+        if (event.target !== event.currentTarget) return; // ignores bubbling
+        if (!/((?<![a-z])(active|passive)|(?<=[a-z])(Active|Passive))(?![a-z])/.test(event.animationName)) return; // ignores animation other than (active|passive)[Foo] or boo(Active|Passive)[Foo]
+        
+        
+        
+        // clean up finished animation
+        
+        setAnimating(null); // stop activating-animation/deactivating-animation
+    }, []);
+    
+    
+    
+    return {
+        active    : actived,
+        isVisible : actived || (animating !== null),
+        
+        class     : ((): string|null => {
+            // activating:
+            if (animating === true) return null; // uses :checked or [aria-checked] or [aria-pressed] or [aria-selected]
+            
+            // passivating:
+            if (animating === false) return 'passivating';
+            
+            // fully actived:
+            if (actived) return 'actived';
+            
+            // fully passived:
+            return null;
+        })(),
+        
+        props     : (() => {
+            if (!actived) return null;
+            
+            // use :checked if <input type="checkbox|radio">:
+            if ((tag === 'input') && checkableCtrls.includes((props as any).type)) return { checked: true };
+            
+            // use [aria-checked] if [role="checkbox|radio"]:
+            if (role && checkableCtrls.includes(role)) return { 'aria-checked': true };
+            
+            // use [aria-pressed] if <button> or [role="button"]:
+            if ((tag === 'button') || (role === 'button')) return { 'aria-pressed': true };
+            
+            // else, use [aria-selected]:
+            return { 'aria-selected' : true };
+        })(),
+        
+        handleAnimationEnd,
+    };
+};
+//#endregion activatable
