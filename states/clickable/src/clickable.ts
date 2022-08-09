a25947b98bf01d4f82cd0b6b979ae7841906b9f3
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-09 13:41:49 +0700

    updated clickable

--- states/clickable/src/clickable.ts
+++ states/clickable/src/clickable.ts
@@ -7,6 +7,8 @@ import {
     
     // hooks:
     useState,
+    useRef,
+    useEffect,
 }                           from 'react'
 
 // cssfn:
@@ -50,6 +52,7 @@ import {
 import {
     // hooks:
     usePropEnabled,
+    usePropReadOnly,
     
     
     
@@ -60,12 +63,14 @@ import {
 // reusable-ui features:
 import {
     // hooks:
-    usesRing,
-}                           from '@reusable-ui/ring'            // ring (focus indicator) color of UI
-import {
-    // hooks:
     usesAnimation,
 }                           from '@reusable-ui/animation'       // animation stuff of UI
+
+
+
+// defaults:
+const _defaultActionMouses : number[]|null = [0]       // left click
+const _defaultActionKeys   : string[]|null = ['space'] // space key
 
 
 
@@ -75,102 +80,75 @@ import {
 
 //#region clickable
 export interface ClickableVars {
-    boxShadow   : any
-    anim        : any
-    
-    
-    
-    /**
-     * final boxShadow single layer - at focused state.
-     */
-    boxShadowLy : any
+    filter : any
+    anim   : any
 }
 const [clickableVars] = cssVars<ClickableVars>();
 
 {
-    const {animationRegistry: {registerBoxShadow, registerAnim}} = usesAnimation();
-    registerBoxShadow(clickableVars.boxShadow);
+    const {animationRegistry: {registerFilter, registerAnim}} = usesAnimation();
+    registerFilter(clickableVars.filter);
     registerAnim(clickableVars.anim);
 }
 
 
 
-// .focused will be added after focusing-animation done:
-const selectorIfFocused  = '.focused'
-// .focusing = styled focus, :focus-within = native focus:
-// the .disabled, .disable are used to kill native :focus-within
-// the .focused, .blurring, .blurred are used to overwrite native :focus-within
-const selectorIfFocusing = ':is(.focusing, :focus-within:not(:is(.disabled, .disable, .focused, .blurring, .blurred)))'
-// .blurring will be added after loosing focus and will be removed after blurring-animation done:
-const selectorIfBlurring = '.blurring'
-// if all above are not set => blurred:
-// optionally use .blurred to overwrite native :focus-within
-const selectorIfBlurred  = ':is(:not(:is(.focused, .focusing, :focus-within:not(:is(.disabled, .disable)), .blurring)), .blurred)'
+// .pressed will be added after pressing-animation done:
+const selectorIfPressed   = '.pressed'
+// .pressing = styled press, :active = native press:
+// the .disabled, .disable are used to kill native :active
+// the .pressed, .releasing, .released are used to overwrite native :active
+// const selectorIfPressing  = ':is(.pressing, :active:not(:is(.disabled, .disable, .pressed, .releasing, .released)))'
+const selectorIfPressing  = '.pressing'
+// .releasing will be added after loosing press and will be removed after releasing-animation done:
+const selectorIfReleasing = '.releasing'
+// if all above are not set => released:
+// optionally use .released to overwrite native :active
+// const selectorIfReleased  = ':is(:not(:is(.pressed, .pressing, :active:not(:is(.disabled, .disable)), .releasing)), .released)'
+const selectorIfReleased  = ':not(:is(.pressed, .pressing, .releasing))'
 
-export const ifFocused       = (styles: CssStyleCollection): CssRule => rule(selectorIfFocused , styles);
-export const ifFocusing      = (styles: CssStyleCollection): CssRule => rule(selectorIfFocusing, styles);
-export const ifBlurring      = (styles: CssStyleCollection): CssRule => rule(selectorIfBlurring, styles);
-export const ifBlurred       = (styles: CssStyleCollection): CssRule => rule(selectorIfBlurred , styles);
+export const ifPressed        = (styles: CssStyleCollection): CssRule => rule(selectorIfPressed  , styles);
+export const ifPressing       = (styles: CssStyleCollection): CssRule => rule(selectorIfPressing , styles);
+export const ifReleasing      = (styles: CssStyleCollection): CssRule => rule(selectorIfReleasing, styles);
+export const ifReleased       = (styles: CssStyleCollection): CssRule => rule(selectorIfReleased , styles);
 
-export const ifFocus         = (styles: CssStyleCollection): CssRule => rule([selectorIfFocusing, selectorIfFocused                                       ], styles);
-export const ifBlur          = (styles: CssStyleCollection): CssRule => rule([                                       selectorIfBlurring, selectorIfBlurred], styles);
-export const ifFocusBlurring = (styles: CssStyleCollection): CssRule => rule([selectorIfFocusing, selectorIfFocused, selectorIfBlurring                   ], styles);
+export const ifPress          = (styles: CssStyleCollection): CssRule => rule([selectorIfPressing, selectorIfPressed                                         ], styles);
+export const ifRelease        = (styles: CssStyleCollection): CssRule => rule([                                       selectorIfReleasing, selectorIfReleased], styles);
+export const ifPressReleasing = (styles: CssStyleCollection): CssRule => rule([selectorIfPressing, selectorIfPressed, selectorIfReleasing                    ], styles);
 
 
 
 export interface ClickableStuff { clickableRule: Factory<CssRule>, clickableVars: CssVars<ClickableVars> }
 export interface ClickableConfig {
-    boxShadowFocus ?: CssKnownProps['boxShadow']
+    filterPress ?: CssKnownProps['filter'   ]
     
-    animFocus      ?: CssKnownProps['animation']
-    animBlur       ?: CssKnownProps['animation']
+    animPress   ?: CssKnownProps['animation']
+    animRelease ?: CssKnownProps['animation']
 }
 /**
- * Adds a capability of UI to be focused.
+ * Adds a capability of UI to be clicked.
  * @param config  A configuration of `clickableRule`.
  * @returns A `ClickableStuff` represents a clickable state.
  */
 export const usesClickable = (config?: ClickableConfig): ClickableStuff => {
-    // dependencies:
-    
-    // features:
-    const {ringVars} = usesRing();
-    
-    
-    
     return {
         clickableRule: () => style({
-            // compositions:
-            ...vars({
-                [clickableVars.boxShadowLy] : !config?.boxShadowFocus ? undefined : [[
-                    // combining: pos width spread color ...
-                    
-                    // boxShadowFocus pos, width, spread, etc:
-                    ...(Array.isArray(config.boxShadowFocus) ? config.boxShadowFocus : [config.boxShadowFocus as (CssKnownProps['boxShadow'] & Array<Array<any>>)[number][number]]),
-                    
-                    // ring color:
-                    ringVars.ring,
-                ]],
-            }),
-            
-            
-            
             ...states([
-                ifFocused({
+                ifPressed({
                     ...vars({
-                        [clickableVars.boxShadow] : clickableVars.boxShadowLy,
+                        [clickableVars.filter] : config?.filterPress,
                     }),
                 }),
-                ifFocusing({
+                ifPressing({
                     ...vars({
-                        [clickableVars.boxShadow] : clickableVars.boxShadowLy,
-                        [clickableVars.anim     ] : config?.animFocus,
+                        [clickableVars.filter] : config?.filterPress,
+                        [clickableVars.anim  ] : config?.animPress,
                     }),
                 }),
-                ifBlurring({
+                ifReleasing({
                     ...vars({
-                        [clickableVars.boxShadow] : clickableVars.boxShadowLy,
-                        [clickableVars.anim     ] : config?.animBlur,
+                        [clickableVars.filter] : config?.filterPress,
+                        [clickableVars.anim  ] : config?.animRelease,
                     }),
                 }),
             ]),
@@ -183,116 +161,173 @@ export const usesClickable = (config?: ClickableConfig): ClickableStuff => {
 
 export interface ClickableProps
     extends
-        Partial<Pick<AccessibilityProps, 'enabled'|'inheritEnabled'>>,
-        
-        // <HTMLControl>:
-        Pick<React.ButtonHTMLAttributes<Element>, 'tabIndex'>
+        Partial<Pick<AccessibilityProps, 'enabled'|'inheritEnabled'|'readOnly'|'inheritReadOnly'>>
 {
-    focused ?: boolean
+    // states:
+    pressed      ?: boolean
+    
+    
+    
+    // behaviors:
+    actionMouses ?: number[]|null
+    actionKeys   ?: string[]|null
 }
 export const useClickable = <TElement extends Element = HTMLElement>(props: ClickableProps) => {
     // fn props:
     const propEnabled           = usePropEnabled(props);
-    const isControllableFocused = (props.focused !== undefined);
+    const propReadOnly          = usePropReadOnly(props);       // supports for <Check>
+    const propEditable          = propEnabled && !propReadOnly; // supports for <Check>
+    const isControllablePressed = (props.pressed !== undefined);
+    
+    const actionMouses          = (props.actionMouses !== undefined) ? props.actionMouses : _defaultActionMouses;
+    const actionKeys            = (props.actionKeys   !== undefined) ? props.actionKeys   : _defaultActionKeys;
     
     
     
     // states:
-    const [focused,   setFocused  ] = useState<boolean>(props.focused ?? false); // true => focused, false => blurred
-    const [animating, setAnimating] = useState<boolean|null>(null);              // null => no-animation, true => focusing-animation, false => blurring-animation
+    const [pressed,   setPressed  ] = useState<boolean>(props.pressed ?? false); // true => pressed, false => released
+    const [animating, setAnimating] = useState<boolean|null>(null);              // null => no-animation, true => pressing-animation, false => releasing-animation
     
-    const [focusDn,   setFocusDn  ] = useState<boolean>(false);                  // uncontrollable (dynamic) state: true => user focused, false => user blurred
+    const [pressDn,   setPressDn  ] = useState<boolean>(false);                  // uncontrollable (dynamic) state: true => user pressed, false => user released
     
     
     
     // resets:
-    if (focusDn && (!propEnabled || isControllableFocused)) {
-        setFocusDn(false); // lost focus because the control is disabled or controllable [focused] is set, when the control is re-enabled => still lost focus
+    if (pressDn && (!propEditable || isControllablePressed)) {
+        setPressDn(false); // lost press because the control is not editable, when the control is re-editable => still lost press
     } // if
     
     
     
     /*
-     * state is always blur if disabled
-     * state is focused/blurred based on [controllable focused] (if set) and fallback to [uncontrollable focused]
+     * state is always released if (disabled || readOnly)
+     * state is pressed/released based on [controllable pressed] (if set) and fallback to [uncontrollable pressed]
      */
-    const focusedFn : boolean = propEnabled && (props.focused /*controllable*/ ?? focusDn /*uncontrollable*/);
+    const pressedFn : boolean = propEditable && (props.pressed /*controllable*/ ?? pressDn /*uncontrollable*/);
     
-    if (focused !== focusedFn) { // change detected => apply the change & start animating
-        setFocused(focusedFn);   // remember the last change
-        setAnimating(focusedFn); // start focusing-animation/blurring-animation
+    if (pressed !== pressedFn) { // change detected => apply the change & start animating
+        setPressed(pressedFn);   // remember the last change
+        setAnimating(pressedFn); // start pressing-animation/releasing-animation
     } // if
     
     
     
-    // handlers:
-    const handleFocus = useEvent<React.FocusEventHandler<TElement>>(() => {
-        // conditions:
-        if (!propEnabled)          return; // control is disabled => no response required
-        if (isControllableFocused) return; // controllable [focused] is set => no uncontrollable required
-        
-        
-        
-        setFocusDn(true);
-    }, [propEnabled, isControllableFocused]);
+    // dom effects:
     
-    const handleBlur  = useEvent<React.FocusEventHandler<TElement>>(() => {
+    const asyncHandleRelease = useRef<ReturnType<typeof setTimeout>|undefined>(undefined);
+    useEffect(() => {
+        // cleanups:
+        return () => {
+            // cancel out previously handleReleaseLate (if any):
+            if (asyncHandleRelease.current) clearTimeout(asyncHandleRelease.current);
+        };
+    }, []); // runs once on startup
+    
+    useEffect(() => {
         // conditions:
-        if (!propEnabled)          return; // control is disabled => no response required
-        if (isControllableFocused) return; // controllable [focused] is set => no uncontrollable required
+        if (!propEditable)         return; // control is not editable => no response required
+        if (isControllablePressed) return; // controllable [pressed] is set => no uncontrollable required
         
         
         
-        setFocusDn(false);
-    }, [propEnabled, isControllableFocused]);
+        // handlers:
+        const handleRelease = (): void => {
+            setPressDn(false);
+        };
+        const handleReleaseLate = (): void => {
+            // cancel out previously handleReleaseLate (if any):
+            if (asyncHandleRelease.current) clearTimeout(asyncHandleRelease.current);
+            
+            
+            
+            // setTimeout => make sure the `mouseup` event fires *after* the `click` event, so the user has a chance to change the `pressed` prop:
+            asyncHandleRelease.current = setTimeout(handleRelease, 0); // 0 = runs immediately after all micro tasks finished
+            /* do not use `Promise.resolve().then(handleRelease)` because it's not fired *after* the `click` event */
+        };
+        
+        
+        
+        // setups:
+        window.addEventListener('mouseup', handleReleaseLate);
+        window.addEventListener('keyup',   handleRelease);
+        
+        
+        
+        // cleanups:
+        return () => {
+            window.removeEventListener('mouseup', handleReleaseLate);
+            window.removeEventListener('keyup',   handleRelease);
+        };
+    }, [propEditable, isControllablePressed]);
+    
+    
+    
+    // handlers:
+    const handlePress        = useEvent<React.MouseEventHandler<TElement> & React.KeyboardEventHandler<TElement>>(() => {
+        // conditions:
+        if (!propEditable)         return; // control is not editable => no response required
+        if (isControllablePressed) return; // controllable [pressed] is set => no uncontrollable required
+        
+        
+        
+        setPressDn(true);
+    }, [propEditable, isControllablePressed]);
+    
+    const handleMouseDown    = useEvent<React.MouseEventHandler<TElement>>((event) => {
+        if (!actionMouses || actionMouses.includes(event.button)) handlePress(event);
+    }, [actionMouses, handlePress]);
+    
+    const handleKeyDown      = useEvent<React.KeyboardEventHandler<TElement>>((event) => {
+        if (!actionKeys || actionKeys.includes(event.code.toLowerCase()) || actionKeys.includes(event.key.toLowerCase())) handlePress(event);
+    }, [actionKeys, handlePress]);
     
     const handleAnimationEnd = useEvent<React.AnimationEventHandler<TElement>>((event) => {
         // conditions:
         if (event.target !== event.currentTarget) return; // ignores bubbling
-        if (!/((?<![a-z])(focus|blur)|(?<=[a-z])(Focus|Blur))(?![a-z])/.test(event.animationName)) return; // ignores animation other than (focus|blur)[Foo] or boo(Focus|Blur)[Foo]
+        if (!/((?<![a-z])(press|release)|(?<=[a-z])(Press|Release))(?![a-z])/.test(event.animationName)) return; // ignores animation other than (press|release)[Foo] or boo(Press|Release)[Foo]
         
         
         
         // clean up finished animation
         
-        setAnimating(null); // stop focusing-animation/blurring-animation
+        setAnimating(null); // stop pressing-animation/releasing-animation
     }, []);
     
     
     
     return {
-        focused,
+        pressed,
         
         class : ((): string|null => {
-            // focusing:
+            // pressing:
             if (animating === true) {
-                // focusing by controllable prop => use class .focusing
-                if (isControllableFocused) return 'focusing';
-                
-                // negative [tabIndex] => can't be focused by user input => treats <Control> as *wrapper* element => use class .focusing
-                if ((props.tabIndex ?? 0) < 0) return 'focusing';
-                
-                // otherwise use pseudo :focus-within
-                return null;
+                // // pressing by controllable prop => use class .pressing
+                // if (isControllablePressed) return 'pressing';
+                //
+                // // otherwise use pseudo :active
+                // return null;
+                // support for pressing by [space key] that not triggering :active
+                return 'pressing';
             } // if
             
-            // blurring:
-            if (animating === false) return 'blurring';
+            // releasing:
+            if (animating === false) return 'releasing';
             
-            // fully focused:
-            if (focused) return 'focused';
+            // fully pressed:
+            if (pressed) return 'pressed';
             
-            // fully blurred:
-            if (isControllableFocused) {
-                return 'blurred'; // blurring by controllable prop => use class .blurred to kill pseudo :focus-within
-            }
-            else {
-                return null; // discard all classes above
-            } // if
+            // fully released:
+            // if (isControllablePressed) {
+            //     return 'released'; // releasing by controllable prop => use class .released to kill pseudo :active
+            // }
+            // else {
+            //     return null; // discard all classes above
+            // } // if
+            return null; // discard all classes above
         })(),
         
-        handleFocus,
-        handleBlur,
+        handleMouseDown,
+        handleKeyDown,
         handleAnimationEnd,
     };
 };
