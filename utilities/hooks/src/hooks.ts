62ba70cbb1b0d1ea198c588d27d44cbfd87d702c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-13 21:38:13 +0700

    updated hooks

--- utilities/hooks/src/hooks.ts
+++ utilities/hooks/src/hooks.ts
@@ -15,7 +15,7 @@ import type {
     SingleOrArray,
 }                           from '@cssfn/types'                 // cssfn general types
 
-// reusable-ui:
+// reusable-ui utilities:
 import {
     // utilities:
     isClientSide,
