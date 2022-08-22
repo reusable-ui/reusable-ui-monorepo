efe5ce077007b9ca73ac22ade680262ef5180658
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-23 01:23:23 +0700

    added accordion

---
+++ composite-components/accordion/src/Accordion.tsx
@@ -0,0 +1,183 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// reusable-ui utilities:
+import {
+    // hooks:
+    usePropActive,
+}                           from '@reusable-ui/accessibilities' // an accessibility management system
+import {
+    // hooks:
+    DetermineCurrentPageProps,
+    useDetermineCurrentPage,
+}                           from '@reusable-ui/navigations'     // a set of navigation functions
+
+// reusable-ui components:
+import {
+    // styles:
+    ListStyle,
+    ListVariant,
+    
+    
+    
+    // react components:
+    ListItemProps,
+    ListItem,
+    
+    ListSeparatorItemProps,
+    ListSeparatorItem,
+    
+    ListProps,
+    List,
+    
+    ListItemComponentProps,
+    ListComponentProps,
+}                           from '@reusable-ui/list'            // represents a series of content
+import {
+    // react components:
+    Icon,
+}                           from '@reusable-ui/icon'            // an icon component
+
+
+
+// react components:
+export interface AccordionItemProps<TElement extends Element = HTMLElement>
+    extends
+        // bases:
+        ListItemProps<TElement>,
+        
+        // navigations:
+        DetermineCurrentPageProps,
+        
+        // components:
+        ListItemComponentProps<TElement>
+{
+    // accessibilities:
+    label ?: string
+}
+export const AccordionItem     = <TElement extends Element = HTMLElement>(props: AccordionItemProps<TElement>): JSX.Element|null => {
+    // rest props:
+    const {
+        // accessibilities:
+        label,
+        
+        
+        
+        // navigations:
+        caseSensitive : _caseSensitive, // remove
+        end           : _end,           // remove
+        
+        
+        
+        // components:
+        listItemComponent = (<ListItem<TElement> /> as React.ReactComponentElement<any, ListItemProps<TElement>>),
+    ...restListItemProps} = props;
+    
+    
+    
+    // fn props:
+    const propActive = usePropActive(props, null);
+    const activeDn   = useDetermineCurrentPage(props);
+    const activeFn   = (listItemComponent.props.active ?? propActive) /*controllable*/ ?? activeDn /*uncontrollable*/;
+    
+    
+    
+    // jsx:
+    /* <ListItem> */
+    return React.cloneElement<ListItemProps<TElement>>(listItemComponent,
+        // props:
+        {
+            // other props:
+            ...restListItemProps,
+            
+            
+            
+            // semantics:
+            'aria-current' : (activeFn || undefined) && (listItemComponent.props['aria-current'] ?? props['aria-current'] ?? 'page'),
+            'aria-label'   : listItemComponent.props['aria-label'] ?? label,
+            
+            
+            
+            // states:
+            active         : activeFn,
+        },
+        
+        
+        
+        // children:
+        listItemComponent.props.children ?? props.children,
+    );
+};
+
+export {
+    ListSeparatorItemProps,
+    ListSeparatorItem,
+}
+
+
+
+export interface AccordionProps<TElement extends Element = HTMLElement>
+    extends
+        // bases:
+        ListProps<TElement>,
+        
+        // components:
+        Omit<ListComponentProps<TElement>,
+            // children:
+            |'listItems' // we redefined `children` prop as <ListItem>(s)
+        >
+{
+    // accessibilities:
+    label ?: string
+}
+const Accordion = <TElement extends Element = HTMLElement>(props: AccordionProps<TElement>): JSX.Element|null => {
+    // rest props:
+    const {
+        // accessibilities:
+        label,
+        
+        
+        
+        // components:
+        listRef,
+        listOrientation,
+        listStyle,
+        listComponent = (<List<TElement> /> as React.ReactComponentElement<any, ListProps<TElement>>),
+    ...restListProps} = props;
+    
+    
+    
+    // jsx:
+    /* <List> */
+    return React.cloneElement<ListProps<TElement>>(listComponent,
+        // props:
+        {
+            // other props:
+            ...restListProps,
+            
+            
+            
+            // semantics:
+            'aria-label' : listComponent.props['aria-label'] ?? label,
+            
+            
+            
+            // behaviors:
+            actionCtrl   : listComponent.props.actionCtrl    ?? props.actionCtrl   ?? true,
+        },
+        
+        
+        
+        // children:
+        listComponent.props.children ?? props.children,
+    );
+};
+export {
+    Accordion,
+    Accordion as default,
+}
+
+export type { ListStyle, ListVariant }
