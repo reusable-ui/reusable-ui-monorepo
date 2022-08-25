c2a8227827b82e2cf27ff9400ae2b8a25bcc37c6
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-26 00:38:01 +0700

    added test-carousel

---
+++ tests/test-carousel/src/App.tsx
@@ -0,0 +1,55 @@
+import {
+    default as React,
+    useMemo,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Carousel,
+} from '@reusable-ui/carousel'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    const style = useMemo<React.CSSProperties>(() => ({
+        maxWidth  : '400px',
+        maxHeight : '300px',
+    }), []);
+    
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                <Carousel theme='primary' style={style}>
+                    <img key={0} alt='lorem image' src='/images/lorem-img/waves-800x600.jpg' />
+                    <img key={1} alt='lorem image' src='/images/lorem-img/leaf-800x700.jpg' />
+                    <img key={2} alt='lorem image' src='/images/lorem-img/building-800x500.jpg' />
+                    <img key={3} alt='lorem image' src='/images/lorem-img/street-800x800.jpg' />
+                    <img key={4} alt='lorem image' src='/images/lorem-img/flower-700x400.jpg' />
+                    <img key={5} alt='lorem image' src='/images/lorem-img/water-500x800.jpg' />
+                    <img key={6} alt='lorem image' src='/images/lorem-img/wood-700x600.jpg' />
+                </Carousel>
+            </div>
+        </>
+    );
+}
+
+export default App;
