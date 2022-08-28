3001cd033dcc3f776c1ab587cbf768a54004c5ab
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-08-28 20:16:01 +0700

    updated test-navbar-react-router

--- tests/test-navbar-react-router/src/App.tsx
+++ tests/test-navbar-react-router/src/App.tsx
@@ -1,6 +1,5 @@
 import {
     default as React,
-    useState,
 } from 'react';
 // import logo from './logo.svg';
 import './App.css';
@@ -28,20 +27,9 @@ import {
 
 
 function App() {
-    const [value, setValue] = useState(0);
-    const handleTriggerRerender = () => {
-        setValue(value + 1);
-    };
-    
-    
     return (
         <>
             <div className="App">
-                <article className='actions'>
-                    <button onClick={handleTriggerRerender}>
-                        Trigger re-render whole app
-                    </button>
-                </article>
                 <Navbar theme='primary' expanded={undefined}>{({
                     colorSystemProps,
                     navbarExpanded,
