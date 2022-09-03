9dcd03448d34f56897ed5d88237e1b76a47001df
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-03 17:10:38 +0700

    updated range

--- simple-components/range/src/Range.tsx
+++ simple-components/range/src/Range.tsx
@@ -240,7 +240,7 @@ export const usesRange = (config?: RangeConfig): RangeStuff => {
 
 // styles:
 export const inputElm      = ':where(:first-child)' // zero degree specificity to be easily overwritten
-export const trackElm      = '.track'               // any degree specificity, not intended to be overwritten
+export const trackElm      = '.track'               // one degree specificity to overwrite <Track>      component
 export const trackLowerElm = '.tracklower'          // one degree specificity to overwrite <Tracklower> component
 export const trackUpperElm = '.trackupper'          // one degree specificity to overwrite <Trackupper> component
 export const thumbElm      = '.thumb'               // one degree specificity to overwrite <Thumb>      component
