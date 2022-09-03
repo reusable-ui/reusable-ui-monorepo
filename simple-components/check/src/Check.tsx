9299d6046c6b3100fcf476a65eed3824abeb707e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-03 17:47:13 +0700

    updated check

--- simple-components/check/src/Check.tsx
+++ simple-components/check/src/Check.tsx
@@ -158,9 +158,9 @@ const _defaultMild     : boolean = false
 
 // styles:
 export const dummyElm = '::before'
-export const inputElm = ':first-child'
+export const inputElm = ':where(:first-child)'     // zero degree specificity to be easily overwritten
 export const checkElm = '::before'
-export const labelElm = ':nth-child(1n+2)'
+export const labelElm = ':where(:nth-child(1n+2))' // zero degree specificity to be easily overwritten
 
 export const usesCheckLayout = () => {
     // dependencies:
