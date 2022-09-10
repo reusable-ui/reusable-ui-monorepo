e4a227fd8f0645a7f3fb3e3fa29a37f18cf8fc80
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-10 22:32:50 +0700

    updated nudible

--- variants/nudible/src/nudible.ts
+++ variants/nudible/src/nudible.ts
@@ -58,11 +58,10 @@ const [nudibleVars] = cssVars<NudibleVars>();
 
 
 
-// parent is     `.nude` -or- current is     `.nude`:
-export const ifNude        = (styles: CssStyleCollection): CssRule => rule(              ':is(.nude&, &.nude)'                                 , styles); // specificityWeight = 1 + (parent's specificityWeight)
-// parent is `.not-nude` -or- current is `.not-nude`:
-export const ifNotNude     = (styles: CssStyleCollection): CssRule => rule(              ':is(.not-nude&, &.not-nude)'                         , styles); // specificityWeight = 1 + (parent's specificityWeight)
-export const ifInheritNude = (styles: CssStyleCollection): CssRule => rule(':where(&):not(:is(.nude&, &.nude, .not-nude&, &.not-nude))', styles); // specificityWeight = 1 + (parent's specificityWeight)
+// parent is     `.nude` -or-  current is     `.nude`:
+export const ifNude        = (styles: CssStyleCollection): CssRule => rule(              ':is(.nude&, &.nude)' , styles); // specificityWeight = 1 + (parent's specificityWeight)
+// parent is not `.nude` -and- current is not `.nude`:
+export const ifNotNude     = (styles: CssStyleCollection): CssRule => rule(':where(&):not(:is(.nude&, &.nude))', styles); // specificityWeight = 1 + (parent's specificityWeight)
 
 
 
@@ -72,7 +71,7 @@ export interface NudibleStuff { nudibleRule: Factory<CssRule>, nudibleVars: CssV
  * @param factory A callback to create a nudeification rules for each toggle state.
  * @returns A `NudibleStuff` represents the nudeification rules.
  */
-export const usesNudible = (factory : ((toggle: boolean|'inherit'|null) => CssStyleCollection) = nudeOf): NudibleStuff => {
+export const usesNudible = (factory : ((toggle: boolean|null) => CssStyleCollection) = nudeOf): NudibleStuff => {
     // dependencies:
     
     // features:
@@ -84,66 +83,37 @@ export const usesNudible = (factory : ((toggle: boolean|'inherit'|null) => CssSt
     return {
         nudibleRule: () => style({
             // toggling props:
-            ...style({
-                // backgrounds:
-                // discard background, no valid/invalid animation:
-                backg : [
-                    [
-                        nudibleVars.nudeSw, // the nude switching function
-                        'none',
-                    ],
-                    '!important',
-                ],
-                
-                
-                
-                // borders:
-                // discard border stroke:
-                [borderVars.borderWidth           ] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-                
-                // discard borderRadius:
-                // remove rounded corners on top:
-                [borderVars.borderStartStartRadius] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-                [borderVars.borderStartEndRadius  ] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-                // remove rounded corners on bottom:
-                [borderVars.borderEndStartRadius  ] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-                [borderVars.borderEndEndRadius    ] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-                
-                // no shadow & no focus animation:
-                boxShadow : [
-                    nudibleVars.nudeSw, // the nude switching function
-                    'none',
-                    '!important'
-                ],
-                
-                
-                
-                // spacings:
-                // discard padding:
-                [paddingVars.paddingInline] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-                [paddingVars.paddingBlock ] : [[
-                    nudibleVars.nudeSw, // the nude switching function
-                    '0px',
-                ]],
-            }),
+            ...variants([
+                ifNude({
+                    // backgrounds:
+                    // discard background, no valid/invalid animation:
+                    backg : [['none'], '!important'],
+                    
+                    
+                    
+                    // borders:
+                    // discard border stroke:
+                    [borderVars.borderWidth           ] : '0px',
+                    
+                    // discard borderRadius:
+                    // remove rounded corners on top:
+                    [borderVars.borderStartStartRadius] : '0px',
+                    [borderVars.borderStartEndRadius  ] : '0px',
+                    // remove rounded corners on bottom:
+                    [borderVars.borderEndStartRadius  ] : '0px',
+                    [borderVars.borderEndEndRadius    ] : '0px',
+                    
+                    // no shadow & no focus animation:
+                    boxShadow : [['none'], '!important'],
+                    
+                    
+                    
+                    // spacings:
+                    // discard padding:
+                    [paddingVars.paddingInline] : '0px',
+                    [paddingVars.paddingBlock ] : '0px',
+                }),
+            ]),
             
             
             
@@ -151,7 +121,6 @@ export const usesNudible = (factory : ((toggle: boolean|'inherit'|null) => CssSt
             ...variants([
                 ifNude(factory(true)),
                 ifNotNude(factory(false)),
-                ifInheritNude(factory('inherit')),
             ]),
         }),
         nudibleVars,
@@ -160,17 +129,16 @@ export const usesNudible = (factory : ((toggle: boolean|'inherit'|null) => CssSt
 
 /**
  * Creates an nudeification rules for the given `toggle` state.
- * @param toggle `true` to activate the nudeification -or- `false` to deactivate -or- `'inherit'` to inherit the nudeification from its ancestor -or- `null` to remove previously declared `nudeOf`.
+ * @param toggle `true` to activate the nudeification -or- `false` to deactivate -or- `null` to remove previously declared `nudeOf`.
  * @returns A `CssRule` represents an nudeification rules for the given `toggle` state.
  */
-export const nudeOf = (toggle: boolean|'inherit'|null): CssRule => style({
+export const nudeOf = (toggle: boolean|null): CssRule => style({
     ...vars({
         /*
-            *switch on/off/inherit* the `**Tg` prop.
+            *switch on/off* the `**Tg` prop.
             toggle:
                 true    => empty string      => do not alter the `**Tg`'s value => activates   `**Tg` variable.
                 false   => initial (invalid) => destroy      the `**Tg`'s value => deactivates `**Tg` variable.
-                inherit => inherit           => follows      the <ancestor> decision.
                 null    => null              => remove the prev declaration
         */
         [nudibleVars.nudeSw] : (typeof(toggle) === 'boolean') ? (toggle ? '' : 'initial') : toggle,
@@ -181,9 +149,9 @@ export const nudeOf = (toggle: boolean|'inherit'|null): CssRule => style({
 
 export interface NudibleProps {
     // variants:
-    nude ?: boolean|'inherit'
+    nude ?: boolean
 }
 export const useNudible = ({nude = _defaultNude}: NudibleProps) => ({
-    class: (nude === 'inherit') ? null : (nude ? 'nude' : 'not-nude'),
+    class: (nude || null) && 'nude',
 });
 //#endregion nudible
