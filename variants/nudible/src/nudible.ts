e1c4ab4aef8d656a5d8c84023bb9e22f6a33bc1a
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-10 17:44:53 +0700

    updated nudible

--- variants/nudible/src/nudible.ts
+++ variants/nudible/src/nudible.ts
@@ -18,7 +18,13 @@ import {
     
     // styles:
     style,
+    vars,
 }                           from '@cssfn/cssfn'                 // writes css in javascript
+import {
+    // utilities:
+    CssVars,
+    cssVars,
+}                           from '@cssfn/css-vars'              // strongly typed of css variables
 
 // reusable-ui features:
 import {
@@ -32,25 +38,41 @@ import {
 
 
 
+// defaults:
+const _defaultNude : Required<NudibleProps>['nude'] = false
+
+
+
 // hooks:
 
 // variants:
 
 //#region nudible
-// by design: ancestor(s)'s `.nude` does not affect current `.nude`
-// ancestor(s) not `.nude` -and- current not `.nude`:
-export const ifNotNude = (styles: CssStyleCollection): CssRule => rule(':where(&):not(:is(.nude&, &.nude))', styles); // specificityWeight = 1 + (parent's specificityWeight)
-// ancestor(s) is  `.nude` -or-  current is  `.nude`:
-export const ifNude    = (styles: CssStyleCollection): CssRule => rule(              ':is(.nude&, &.nude)' , styles); // specificityWeight = 1 + (parent's specificityWeight)
+export interface NudibleVars {
+    /**
+     * the nude switching function.
+     */
+    nudeSw : any
+}
+const [nudibleVars] = cssVars<NudibleVars>();
 
 
 
-export interface NudibleStuff { nudibleRule: Factory<CssRule> }
+// parent is     `.nude` -or- current is     `.nude`:
+export const ifNude        = (styles: CssStyleCollection): CssRule => rule(              ':is(.nude&, &.nude)'                                 , styles); // specificityWeight = 1 + (parent's specificityWeight)
+// parent is `.not-nude` -or- current is `.not-nude`:
+export const ifNotNude     = (styles: CssStyleCollection): CssRule => rule(              ':is(.not-nude&, &.not-nude)'                         , styles); // specificityWeight = 1 + (parent's specificityWeight)
+export const ifInheritNude = (styles: CssStyleCollection): CssRule => rule(':where(&):not(:is(.nude&, &.nude, .not-nude&, &.not-nude))', styles); // specificityWeight = 1 + (parent's specificityWeight)
+
+
+
+export interface NudibleStuff { nudibleRule: Factory<CssRule>, nudibleVars: CssVars<NudibleVars> }
 /**
  * Uses a toggleable nudeification (removes background, border & padding).
+ * @param factory A callback to create a nudeification rules for each toggle state.
  * @returns A `NudibleStuff` represents the nudeification rules.
  */
-export const usesNudible = (): NudibleStuff => {
+export const usesNudible = (factory : ((toggle: boolean|'inherit') => CssStyleCollection) = nudeOf): NudibleStuff => {
     // dependencies:
     
     // features:
@@ -61,43 +83,106 @@ export const usesNudible = (): NudibleStuff => {
     
     return {
         nudibleRule: () => style({
+            // toggling props:
+            ...style({
+                // backgrounds:
+                // discard background, no valid/invalid animation:
+                backg : [
+                    [
+                        nudibleVars.nudeSw, // the nude switching function
+                        'none',
+                    ],
+                    '!important',
+                ],
+                
+                
+                
+                // borders:
+                // discard border stroke:
+                [borderVars.borderWidth           ] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+                
+                // discard borderRadius:
+                // remove rounded corners on top:
+                [borderVars.borderStartStartRadius] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+                [borderVars.borderStartEndRadius  ] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+                // remove rounded corners on bottom:
+                [borderVars.borderEndStartRadius  ] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+                [borderVars.borderEndEndRadius    ] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+                
+                // no shadow & no focus animation:
+                boxShadow : [
+                    nudibleVars.nudeSw, // the nude switching function
+                    'none',
+                    '!important'
+                ],
+                
+                
+                
+                // spacings:
+                // discard padding:
+                [paddingVars.paddingInline] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+                [paddingVars.paddingBlock ] : [[
+                    nudibleVars.nudeSw, // the nude switching function
+                    '0px',
+                ]],
+            }),
+            
+            
+            
+            // toggling conditions:
             ...variants([
-                ifNude({
-                    // backgrounds:
-                    backg : [['none'], '!important'], // discard background, no valid/invalid animation
-                    
-                    
-                    
-                    // borders:
-                    [borderVars.borderWidth           ] : '0px', // discard border stroke
-                    
-                    // remove rounded corners on top:
-                    [borderVars.borderStartStartRadius] : '0px', // discard borderRadius
-                    [borderVars.borderStartEndRadius  ] : '0px', // discard borderRadius
-                    // remove rounded corners on bottom:
-                    [borderVars.borderEndStartRadius  ] : '0px', // discard borderRadius
-                    [borderVars.borderEndEndRadius    ] : '0px', // discard borderRadius
-                    
-                    boxShadow : ['none', '!important'],          // no shadow & no focus animation
-                    
-                    
-                    
-                    // spacings:
-                    [paddingVars.paddingInline] : '0px',         // discard padding
-                    [paddingVars.paddingBlock ] : '0px',         // discard padding
-                }),
+                ifNude(factory(true)),
+                ifNotNude(factory(false)),
+                ifInheritNude(factory('inherit')),
             ]),
         }),
+        nudibleVars,
     };
 };
+
+/**
+ * Creates an nudeification rules for the given `toggle` state.
+ * @param toggle `true` to activate the nudeification -or- `false` to deactivate -or- `'inherit'` to inherit the nudeification from its ancestor.
+ * @returns A `CssRule` represents an nudeification rules for the given `toggle` state.
+ */
+export const nudeOf = (toggle: boolean|'inherit'): CssRule => style({
+    ...vars({
+        /*
+            *switch on/off/inherit* the `**Tg` prop.
+            toggle:
+                true    => empty string      => do not alter the `**Tg`'s value => activates   `**Tg` variable.
+                false   => initial (invalid) => destroy      the `**Tg`'s value => deactivates `**Tg` variable.
+                inherit => inherit           => follows      the <ancestor> decision.
+        */
+        [nudibleVars.nudeSw] : (toggle === 'inherit') ? 'inherit' : (toggle ? '' : 'initial'),
+    }),
+});
 
 
 
 export interface NudibleProps {
     // variants:
-    nude ?: boolean
+    nude ?: boolean|'inherit'
 }
-export const useNudible = ({nude}: NudibleProps) => ({
-    class: nude ? 'nude' : null,
+export const useNudible = ({nude = _defaultNude}: NudibleProps) => ({
+    class: (nude === 'inherit') ? null : (nude ? 'nude' : 'not-nude'),
 });
 //#endregion nudible
