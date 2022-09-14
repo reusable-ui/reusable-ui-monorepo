29bf67eb4f1deeb7f621cae9621e552e1213063b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-15 06:01:25 +0700

    updated utilities

--- utilities/utilities/src/utilities.ts
+++ utilities/utilities/src/utilities.ts
@@ -26,22 +26,3 @@ export const isReusableUiComponent = <TProps = any, TConstructor extends React.J
         !isForwardRef(node)
     );
 };
-
-const isSingleValue = (expression: string|ReadonlyArray<string>): expression is string => (typeof(expression) === 'string') || (Array.isArray(expression) && (expression.length === 1));
-export const parseNumber = (expression: number|string|ReadonlyArray<string>|null|undefined): number|null => {
-    if (typeof(expression) === 'number') {
-        if (isNaN(expression)) return null;
-        return expression;
-    } // if
-    if (!expression) return null;
-    
-    
-    
-    if (!isSingleValue(expression)) return null;
-    
-    
-    
-    const result = Number.parseFloat(expression);
-    if (isNaN(result)) return null;
-    return result;
-};
