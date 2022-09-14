1134da186143fd626200ac664becc9d56d1664bc
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-15 05:58:13 +0700

    added numbers

---
+++ utilities/numbers/src/numbers.ts
@@ -0,0 +1,19 @@
+// utilities:
+const isSingleValue = (expression: string|ReadonlyArray<string>): expression is string => (typeof(expression) === 'string') || (Array.isArray(expression) && (expression.length === 1));
+export const parseNumber = (expression: number|string|ReadonlyArray<string>|null|undefined): number|null => {
+    if (typeof(expression) === 'number') {
+        if (isNaN(expression)) return null;
+        return expression;
+    } // if
+    if (!expression) return null;
+    
+    
+    
+    if (!isSingleValue(expression)) return null;
+    
+    
+    
+    const result = Number.parseFloat(expression);
+    if (isNaN(result)) return null;
+    return result;
+};
