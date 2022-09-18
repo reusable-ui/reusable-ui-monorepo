f2d5a199afbfd39d1b16ced27bacb1fae31a1275
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-18 23:52:15 +0700

    updated control

--- base-components/control/src/Control.tsx
+++ base-components/control/src/Control.tsx
@@ -33,52 +33,53 @@ import {
     usesCssProps,
 }                           from '@cssfn/css-config'            // reads/writes css variables configuration
 
-// reusable-ui utilities:
+// reusable-ui core:
 import {
-    // styles:
+    // removes browser's default stylesheet:
     stripoutControl,
-}                           from '@reusable-ui/stripouts'       // removes browser's default stylesheet
-import {
-    // hooks:
+    
+    
+    
+    // react helper hooks:
     useMergeEvents,
     useMergeClasses,
-}                           from '@reusable-ui/hooks'           // react helper hooks
-import {
-    // hooks:
+    
+    
+    
+    // an accessibility management system:
     usePropEnabled,
-}                           from '@reusable-ui/accessibilities' // an accessibility management system
-
-// reusable-ui features:
-import {
-    // hooks:
+    
+    
+    
+    // animation stuff of UI:
     usesAnimation,
-}                           from '@reusable-ui/animation'       // animation stuff of UI
-
-// reusable-ui variants:
-import {
-    // hooks:
+    
+    
+    
+    // size options of UI:
     usesResizable,
-}                           from '@reusable-ui/resizable'       // size options of UI
-
-// reusable-ui states:
-import {
-    // hooks:
+    
+    
+    
+    // a capability of UI to be disabled:
     ifDisable,
-}                           from '@reusable-ui/disableable'     // a capability of UI to be disabled
-import {
-    // hooks:
+    
+    
+    
+    // a capability of UI to be focused:
     ifBlurring,
     ifFocus,
     usesFocusable,
     FocusableProps,
     useFocusable,
-}                           from '@reusable-ui/focusable'       // a capability of UI to be focused
-import {
-    // hooks:
+    
+    
+    
+    // adds an interactive feel to a UI:
     usesInteractable,
     InteractableProps,
     useInteractable,
-}                           from '@reusable-ui/interactable'    // adds an interactive feel to a UI
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
 // reusable-ui components:
 import {
