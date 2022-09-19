0715e4115511c960b14b05d4464262cca8fed3ab
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-19 20:03:08 +0700

    updated alert

--- notification-components/alert/src/Alert.tsx
+++ notification-components/alert/src/Alert.tsx
@@ -37,31 +37,28 @@ import {
     usesPrefixedProps,
 }                           from '@cssfn/css-config'            // reads/writes css variables configuration
 
-// reusable-ui configs:
+// reusable-ui core:
 import {
-    // configs:
+    // a spacer (gap) management system:
     spacers,
-}                           from '@reusable-ui/spacers'         // a spacer (gap) management system
-
-// reusable-ui utilities:
-import {
-    // hooks:
+    
+    
+    
+    // react helper hooks:
     useEvent,
     useMergeEvents,
-}                           from '@reusable-ui/hooks'           // react helper hooks
-
-// reusable-ui variants:
-import {
-    // hooks:
+    
+    
+    
+    // size options of UI:
     usesResizable,
-}                           from '@reusable-ui/resizable'       // size options of UI
-
-// reusable-ui states:
-import type {
-    // type:
+    
+    
+    
+    // a capability of UI to expand/reduce its size or toggle the visibility:
     ExpandedChangeEvent,
     ToggleCollapsibleProps,
-}                           from '@reusable-ui/collapsible'     // a capability of UI to expand/reduce its size or toggle the visibility
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
 // reusable-ui components:
 import {
