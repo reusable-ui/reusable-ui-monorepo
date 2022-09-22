7f3fbf8d23faa40cde148eafb7f2d26e49022093
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-22 13:57:02 +0700

    updated padding

--- features/padding/src/padding.ts
+++ features/padding/src/padding.ts
@@ -1,27 +1,26 @@
 // cssfn:
-import type {
-    // types:
-    Factory,
-}                           from '@cssfn/types'                 // cssfn general types
-import type {
-    // css known (standard) properties:
-    CssKnownProps,
-    
-    
-    
-    // cssfn properties:
-    CssRule,
-}                           from '@cssfn/css-types'             // cssfn css specific types
 import {
-    // styles:
+    // cssfn general types:
+    Factory,
+    
+    
+    
+    // cssfn css specific types:
+    CssKnownProps,
+    CssRule,
+    
+    
+    
+    // writes css in javascript:
     style,
     vars,
-}                           from '@cssfn/cssfn'                 // writes css in javascript
-import {
-    // utilities:
+    
+    
+    
+    // strongly typed of css variables:
     CssVars,
     cssVars,
-}                           from '@cssfn/css-vars'              // strongly typed of css variables
+}                           from '@cssfn/core'                  // writes css in javascript
 
 
 
