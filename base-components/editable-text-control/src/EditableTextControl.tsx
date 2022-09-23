d53ae3563e02b970cdfe97d252cbf0944a19b28f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-23 14:48:16 +0700

    updated editable-text-control

--- base-components/editable-text-control/src/EditableTextControl.tsx
+++ base-components/editable-text-control/src/EditableTextControl.tsx
@@ -5,43 +5,30 @@ import {
 }                           from 'react'
 
 // cssfn:
-import type {
-    // css known (standard) properties:
-    CssKnownProps,
-}                           from '@cssfn/css-types'                 // cssfn css specific types
 import {
-    // rules:
+    // cssfn css specific types:
+    CssKnownProps,
+    
+    
+    
+    // writes css in javascript:
     states,
-    
-    
-    
-    //combinators:
     children,
-    
-    
-    
-    // styles:
     style,
     imports,
-    
-    
-    
-    // utilities:
     escapeSvg,
-}                           from '@cssfn/cssfn'                     // writes css in javascript
+    
+    
+    
+    // reads/writes css variables configuration:
+    cssConfig,
+    usesCssProps,
+    usesPrefixedProps,
+}                           from '@cssfn/core'                      // writes css in javascript
 import {
     // style sheets:
     dynamicStyleSheet,
 }                           from '@cssfn/cssfn-react'               // writes css in react hook
-import {
-    cssConfig,
-    
-    
-    
-    // utilities:
-    usesCssProps,
-    usesPrefixedProps,
-}                           from '@cssfn/css-config'                // reads/writes css variables configuration
 
 // reusable-ui core:
 import {
