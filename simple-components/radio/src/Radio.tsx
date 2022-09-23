d00dbd76b897492010c82e6152b31051f9103561
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-24 01:34:58 +0700

    updated radio

--- simple-components/radio/src/Radio.tsx
+++ simple-components/radio/src/Radio.tsx
@@ -11,38 +11,29 @@ import {
 }                           from 'react'
 
 // cssfn:
-import type {
-    // css known (standard) properties:
-    CssKnownProps,
-}                           from '@cssfn/css-types'             // cssfn css specific types
 import {
-    //combinators:
+    // cssfn css specific types:
+    CssKnownProps,
+    
+    
+    
+    // writes css in javascript:
     children,
-    
-    
-    
-    // styles:
     style,
     vars,
     imports,
-    
-    
-    
-    // utilities:
     escapeSvg,
-}                           from '@cssfn/cssfn'                 // writes css in javascript
+    
+    
+    
+    // reads/writes css variables configuration:
+    cssConfig,
+    usesCssProps,
+}                           from '@cssfn/core'                  // writes css in javascript
 import {
     // style sheets:
     dynamicStyleSheet,
 }                           from '@cssfn/cssfn-react'           // writes css in react hook
-import {
-    cssConfig,
-    
-    
-    
-    // utilities:
-    usesCssProps,
-}                           from '@cssfn/css-config'            // reads/writes css variables configuration
 
 // reusable-ui core:
 import {
