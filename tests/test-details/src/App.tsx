07dbb8fb0e0573b70e43ddac0c26c910f08b37fb
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-26 02:59:51 +0700

    added test-details

---
+++ tests/test-details/src/App.tsx
@@ -0,0 +1,60 @@
+import {
+    default as React,
+    useState,
+} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+    Details,
+} from '@reusable-ui/details'
+import {
+    Styles,
+    HeadPortal,
+} from '@cssfn/cssfn-react'
+
+
+
+interface ParagraphLoremProps {
+    words? : number
+}
+const lorems = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem accusantium et ipsam, architecto cupiditate recusandae dolorem itaque tempore expedita commodi eos doloremque molestias. Impedit doloribus maxime rem, iste quia consequuntur?'.split(' ');
+const ParagraphLorem = ({ words }: ParagraphLoremProps) => (
+    <p>
+        { (words ? lorems.slice(0, words) : lorems).join(' ') }
+    </p>
+);
+
+
+
+function App() {
+    const [value, setValue] = useState(0);
+    const handleTriggerRerender = () => {
+        setValue(value + 1);
+    };
+    
+    
+    
+    return (
+        <>
+            <HeadPortal>
+                <Styles />
+            </HeadPortal>
+            <div className="App">
+                <article className='actions'>
+                    <button onClick={handleTriggerRerender}>
+                        Trigger re-render whole app
+                    </button>
+                </article>
+                
+                <br />
+                
+                <Details theme='primary' label='An awesome thing'>
+                    <ParagraphLorem />
+                    <ParagraphLorem />
+                </Details>
+            </div>
+        </>
+    );
+}
+
+export default App;
