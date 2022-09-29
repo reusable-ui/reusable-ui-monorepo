cddd2a50f5966f1f8f042723991b54d55df4a402
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-09-30 02:09:39 +0700

    updated core

--- barrels/core/src/core.ts
+++ barrels/core/src/core.ts
@@ -1,53 +1,53 @@
 // reusable-ui configs:
-export * from '@reusable-ui/colors'             // a color management system
-export * from '@reusable-ui/borders'            // a border (stroke) management system
-export * from '@reusable-ui/spacers'            // a spacer (gap) management system
-export * from '@reusable-ui/breakpoints'        // a responsive management system
-export * from '@reusable-ui/typos'              // a typography management system
+/*peer*/ export * from '@reusable-ui/colors'                // a color management system
+/*peer*/ export * from '@reusable-ui/borders'               // a border (stroke) management system
+/*peer*/ export * from '@reusable-ui/spacers'               // a spacer (gap) management system
+/*peer*/ export * from '@reusable-ui/breakpoints'           // a responsive management system
+/*peer*/ export * from '@reusable-ui/typos'                 // a typography management system
 
 // reusable-ui utilities:
-export * from '@reusable-ui/numbers'            // a set of numeric utility functions
-export * from '@reusable-ui/nodes'              // a set of React node utility functions
-export * from '@reusable-ui/stripouts'          // removes browser's default stylesheet
-export * from '@reusable-ui/layouts'            // reusable common layouts
-export * from '@reusable-ui/hooks'              // react helper hooks
-export * from '@reusable-ui/semantics'          // a semantic management system for react web components
-export * from '@reusable-ui/accessibilities'    // an accessibility management system
-export * from '@reusable-ui/validations'        // a validation management system
-export * from '@reusable-ui/focuses'            // focusing functions
-export * from '@reusable-ui/client-sides'       // a set of client-side functions
-export * from '@reusable-ui/navigations'        // a set of navigation functions
+export * from '@reusable-ui/numbers'                        // a set of numeric utility functions
+export * from '@reusable-ui/nodes'                          // a set of React node utility functions
+export * from '@reusable-ui/stripouts'                      // removes browser's default stylesheet
+export * from '@reusable-ui/layouts'                        // reusable common layouts
+export * from '@reusable-ui/hooks'                          // react helper hooks
+export * from '@reusable-ui/semantics'                      // a semantic management system for react web components
+/*peer*/ export * from '@reusable-ui/accessibilities'       // an accessibility management system
+/*peer*/ export * from '@reusable-ui/validations'           // a validation management system
+export * from '@reusable-ui/focuses'                        // focusing functions
+export * from '@reusable-ui/client-sides'                   // a set of client-side functions
+export * from '@reusable-ui/navigations'                    // a set of navigation functions
 
 // reusable-ui features:
-export * from '@reusable-ui/background'         // background stuff of UI
-export * from '@reusable-ui/foreground'         // foreground (text color) stuff of UI
-export * from '@reusable-ui/border'             // border (stroke) stuff of UI
-export * from '@reusable-ui/ring'               // ring (focus indicator) color of UI
-export * from '@reusable-ui/animation'          // animation stuff of UI
-export * from '@reusable-ui/padding'            // padding (inner spacing) stuff of UI
-export * from '@reusable-ui/floatable'          // a capability of UI to float/overlay on the top/beside the another UI
-export * from '@reusable-ui/groupable'          // groups a list of UIs into a single UI
+/*peer*/ export * from '@reusable-ui/background'            // background stuff of UI
+/*peer*/ export * from '@reusable-ui/foreground'            // foreground (text color) stuff of UI
+/*peer*/ export * from '@reusable-ui/border'                // border (stroke) stuff of UI
+/*peer*/ export * from '@reusable-ui/ring'                  // ring (focus indicator) color of UI
+/*peer*/ export * from '@reusable-ui/animation'             // animation stuff of UI
+/*peer*/ export * from '@reusable-ui/padding'               // padding (inner spacing) stuff of UI
+export * from '@reusable-ui/floatable'                      // a capability of UI to float/overlay on the top/beside the another UI
+/*peer*/ export * from '@reusable-ui/groupable'             // groups a list of UIs into a single UI
 
 // reusable-ui variants:
-export * from '@reusable-ui/orientationable'    // a capability of UI to rotate its layout
-export * from '@reusable-ui/resizable'          // size options of UI
-export * from '@reusable-ui/themable'           // color options of UI
-export * from '@reusable-ui/gradientable'       // gradient variant of UI
-export * from '@reusable-ui/outlineable'        // outlined (background-less) variant of UI
-export * from '@reusable-ui/mildable'           // mild (soft color) variant of UI
-export * from '@reusable-ui/nudible'            // nude variant of UI
-export * from '@reusable-ui/colorable'          // colorize the UI based on its theme or the background theme
-export * from '@reusable-ui/basic-variants'     // basic variants of UI
+/*peer*/ export * from '@reusable-ui/orientationable'       // a capability of UI to rotate its layout
+/*peer*/ export * from '@reusable-ui/resizable'             // size options of UI
+/*peer*/ export * from '@reusable-ui/themable'              // color options of UI
+/*peer*/ export * from '@reusable-ui/gradientable'          // gradient variant of UI
+/*peer*/ export * from '@reusable-ui/outlineable'           // outlined (background-less) variant of UI
+/*peer*/ export * from '@reusable-ui/mildable'              // mild (soft color) variant of UI
+/*peer*/ export * from '@reusable-ui/nudible'               // nude variant of UI
+export * from '@reusable-ui/colorable'                      // colorize the UI based on its theme or the background theme
+/*peer*/ export * from '@reusable-ui/basic-variants'        // basic variants of UI
 
 // reusable-ui states:
-export * from '@reusable-ui/excitable'          // a capability of UI to highlight itself to attract user's attention
-export * from '@reusable-ui/collapsible'        // a capability of UI to expand/reduce its size or toggle the visibility
-export * from '@reusable-ui/disableable'        // a capability of UI to be disabled
-export * from '@reusable-ui/activatable'        // a capability of UI to be highlighted/selected/activated
-export * from '@reusable-ui/focusable'          // a capability of UI to be focused
-export * from '@reusable-ui/interactable'       // adds an interactive feel to a UI
-export * from '@reusable-ui/clickable'          // a capability of UI to be clicked
-export * from '@reusable-ui/active-as-click'    // shows the UI as clicked when activated
-export * from '@reusable-ui/invalidable'        // a possibility of UI having an invalid state.
-export * from '@reusable-ui/checkable'          // a capability of UI to be checked
-export * from '@reusable-ui/validation-icon'    // an icon for indicating a validity status
+export * from '@reusable-ui/excitable'                      // a capability of UI to highlight itself to attract user's attention
+export * from '@reusable-ui/collapsible'                    // a capability of UI to expand/reduce its size or toggle the visibility
+export * from '@reusable-ui/disableable'                    // a capability of UI to be disabled
+export * from '@reusable-ui/activatable'                    // a capability of UI to be highlighted/selected/activated
+export * from '@reusable-ui/focusable'                      // a capability of UI to be focused
+export * from '@reusable-ui/interactable'                   // adds an interactive feel to a UI
+export * from '@reusable-ui/clickable'                      // a capability of UI to be clicked
+export * from '@reusable-ui/active-as-click'                // shows the UI as clicked when activated
+export * from '@reusable-ui/invalidable'                    // a possibility of UI having an invalid state.
+export * from '@reusable-ui/checkable'                      // a capability of UI to be checked
+export * from '@reusable-ui/validation-icon'                // an icon for indicating a validity status
