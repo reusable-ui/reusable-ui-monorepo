b0afd7c78e3fc24adca09e25a32aee7a7bf34b81
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-13 14:05:02 +0700

    updated container

--- base-content-components/container/src/Container.tsx
+++ base-content-components/container/src/Container.tsx
@@ -320,29 +320,32 @@ export const useContainerStyleSheet = dynamicStyleSheet(() => ({
 export const [containers, containerValues, cssContainerConfig] = cssConfig(() => {
     return {
         // borders:
-        borderWidth      : borderStrokes.none   as CssKnownProps['borderWidth'  ], // override to <Basic>
-        borderRadius     : borderRadiuses.none  as CssKnownProps['borderRadius' ], // override to <Basic>
+        borderWidth       : borderStrokes.none   as CssKnownProps['borderWidth'  ], // override to <Basic>
+        borderRadius      : borderRadiuses.none  as CssKnownProps['borderRadius' ], // override to <Basic>
         
         
         
         // spacings:
-        paddingInline    : '12px'               as CssKnownProps['paddingInline'],
-        paddingBlock     :  '9px'               as CssKnownProps['paddingBlock' ],
+        paddingInline     : '12px'               as CssKnownProps['paddingInline'],
+        paddingBlock      :  '9px'               as CssKnownProps['paddingBlock' ],
         
-        paddingInlineSm  : '24px'               as CssKnownProps['paddingInline'],
-        paddingBlockSm   : '18px'               as CssKnownProps['paddingBlock' ],
+        paddingInlineSm   : '24px'               as CssKnownProps['paddingInline'],
+        paddingBlockSm    : '18px'               as CssKnownProps['paddingBlock' ],
         
-        paddingInlineMd  : '36px'               as CssKnownProps['paddingInline'],
-        paddingBlockMd   : '27px'               as CssKnownProps['paddingBlock' ],
+        paddingInlineMd   : '36px'               as CssKnownProps['paddingInline'],
+        paddingBlockMd    : '27px'               as CssKnownProps['paddingBlock' ],
         
-        paddingInlineLg  : '48px'               as CssKnownProps['paddingInline'],
-        paddingBlockLg   : '36px'               as CssKnownProps['paddingBlock' ],
+        paddingInlineLg   : '48px'               as CssKnownProps['paddingInline'],
+        paddingBlockLg    : '36px'               as CssKnownProps['paddingBlock' ],
         
-        paddingInlineXl  : '60px'               as CssKnownProps['paddingInline'],
-        paddingBlockXl   : '45px'               as CssKnownProps['paddingBlock' ],
+        paddingInlineXl   : '60px'               as CssKnownProps['paddingInline'],
+        paddingBlockXl    : '45px'               as CssKnownProps['paddingBlock' ],
         
-        paddingInlineXxl : '72px'               as CssKnownProps['paddingInline'],
-        paddingBlockXxl  : '54px'               as CssKnownProps['paddingBlock' ],
+        paddingInlineXxl  : '72px'               as CssKnownProps['paddingInline'],
+        paddingBlockXxl   : '54px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineXxxl : '84px'               as CssKnownProps['paddingInline'],
+        paddingBlockXxxl  : '63px'               as CssKnownProps['paddingBlock' ],
     };
 }, { prefix: 'con' });
 
