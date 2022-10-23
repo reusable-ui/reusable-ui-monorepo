161c566261b63159e9a54b7a93504cd195963b11
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-24 01:26:37 +0700

    updated test-editable-control

--- tests/test-editable-control/src/App.tsx
+++ tests/test-editable-control/src/App.tsx
@@ -1,5 +1,7 @@
 import {
     default as React,
+    useEffect,
+    useRef,
     useState,
 } from 'react';
 // import logo from './logo.svg';
@@ -14,11 +16,38 @@ import {
 
 
 
+const handleAnimationStart = (event: React.AnimationEvent<HTMLButtonElement>) => {
+    if (!event.animationName.startsWith('edit-')) return;
+    console.log('start: ', event.animationName, new Date().getTime(), event.currentTarget.classList.toString());
+};
+const handleAnimationEnd = (event: React.AnimationEvent<HTMLButtonElement>) => {
+    if (!event.animationName.startsWith('edit-')) return;
+    console.log('end: ', event.animationName, new Date().getTime(), event.currentTarget.classList.toString());
+};
+const handleAnimationCancel = (event: AnimationEvent) => {
+    if (!event.animationName.startsWith('edit-')) return;
+    console.log('!!! CANCEL !!!: ', event.animationName, new Date().getTime(), (event.currentTarget as HTMLButtonElement)?.classList?.toString());
+};
+
+
+
 function App() {
     const [value, setValue] = useState(0);
     const handleTriggerRerender = () => {
         setValue(value + 1);
     };
+    
+    const editRef = useRef<HTMLElement>(null);
+    useEffect(() => {
+        const editElm = editRef.current;
+        if (!editElm) return;
+        
+        editElm.addEventListener('animationcancel', handleAnimationCancel);
+        
+        return () => {
+            editElm.removeEventListener('animationcancel', handleAnimationCancel);
+        };
+    }, []);
     
     
     return (
@@ -33,7 +62,11 @@ function App() {
                     </button>
                 </article>
                 <article className='actions'>
-                    <EditableControl theme='primary' enableValidation={true} tag='input' {...{type: 'email', placeholder: 'your@email.com'}} />
+                    <EditableControl theme='primary' enableValidation={true} tag='input' {...{type: 'email', placeholder: 'your@email.com'}}
+                        elmRef={editRef}
+                        onAnimationStart={handleAnimationStart}
+                        onAnimationEnd={handleAnimationEnd}
+                    />
                 </article>
             </div>
         </>
