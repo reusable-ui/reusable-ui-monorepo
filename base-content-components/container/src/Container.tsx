f5552fc3ee7aa0c1242aeaa766a81cff31524263
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-10 23:14:25 +0700

    updated container

--- base-content-components/container/src/Container.tsx
+++ base-content-components/container/src/Container.tsx
@@ -91,6 +91,59 @@ export const defaultOrientationableOptions = defaultBlockOrientationableOptions;
 
 
 
+// configs:
+export const [containers, containerValues, cssContainerConfig] = cssConfig(() => {
+    return {
+        // borders:
+        borderWidth       : borderStrokes.none   as CssKnownProps['borderWidth'  ], // override to <Basic>
+        borderRadius      : borderRadiuses.none  as CssKnownProps['borderRadius' ], // override to <Basic>
+        
+        
+        
+        // spacings:
+        paddingInline     : '12px'               as CssKnownProps['paddingInline'],
+        paddingBlock      :  '9px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineSm   : '24px'               as CssKnownProps['paddingInline'],
+        paddingBlockSm    : '18px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineMd   : '36px'               as CssKnownProps['paddingInline'],
+        paddingBlockMd    : '27px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineLg   : '48px'               as CssKnownProps['paddingInline'],
+        paddingBlockLg    : '36px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineXl   : '60px'               as CssKnownProps['paddingInline'],
+        paddingBlockXl    : '45px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineXxl  : '72px'               as CssKnownProps['paddingInline'],
+        paddingBlockXxl   : '54px'               as CssKnownProps['paddingBlock' ],
+        
+        paddingInlineXxxl : '84px'               as CssKnownProps['paddingInline'],
+        paddingBlockXxxl  : '63px'               as CssKnownProps['paddingBlock' ],
+    };
+}, { prefix: 'con' });
+
+// configs adjusted by screen width:
+styleSheet({
+    ...atGlobal({
+        ...atRoot({
+            ...rules([
+                // the <Container> size is determined by screen width:
+                Object.keys(breakpoints)
+                .map((breakpointName) =>
+                    ifScreenWidthAtLeast(breakpointName, {
+                        // overwrites propName = propName{BreakpointName}:
+                        ...overwriteProps(containers, usesSuffixedProps(containers, breakpointName)),
+                    }),
+                ),
+            ]),
+        }, { specificityWeight: 2 }), // increase the specificity to win with the specificity in cssConfig's :root
+    }),
+});
+
+
+
 // styles:
 /**
  * Applies a responsive container layout.
@@ -313,59 +366,6 @@ export const useContainerStyleSheet = dynamicStyleSheet(() => ({
         usesContainerChildren(),
     ]),
 }), { id: 'dmgepbofol' }); // a unique salt for SSR support, ensures the server-side & client-side have the same generated class names
-
-
-
-// configs:
-export const [containers, containerValues, cssContainerConfig] = cssConfig(() => {
-    return {
-        // borders:
-        borderWidth       : borderStrokes.none   as CssKnownProps['borderWidth'  ], // override to <Basic>
-        borderRadius      : borderRadiuses.none  as CssKnownProps['borderRadius' ], // override to <Basic>
-        
-        
-        
-        // spacings:
-        paddingInline     : '12px'               as CssKnownProps['paddingInline'],
-        paddingBlock      :  '9px'               as CssKnownProps['paddingBlock' ],
-        
-        paddingInlineSm   : '24px'               as CssKnownProps['paddingInline'],
-        paddingBlockSm    : '18px'               as CssKnownProps['paddingBlock' ],
-        
-        paddingInlineMd   : '36px'               as CssKnownProps['paddingInline'],
-        paddingBlockMd    : '27px'               as CssKnownProps['paddingBlock' ],
-        
-        paddingInlineLg   : '48px'               as CssKnownProps['paddingInline'],
-        paddingBlockLg    : '36px'               as CssKnownProps['paddingBlock' ],
-        
-        paddingInlineXl   : '60px'               as CssKnownProps['paddingInline'],
-        paddingBlockXl    : '45px'               as CssKnownProps['paddingBlock' ],
-        
-        paddingInlineXxl  : '72px'               as CssKnownProps['paddingInline'],
-        paddingBlockXxl   : '54px'               as CssKnownProps['paddingBlock' ],
-        
-        paddingInlineXxxl : '84px'               as CssKnownProps['paddingInline'],
-        paddingBlockXxxl  : '63px'               as CssKnownProps['paddingBlock' ],
-    };
-}, { prefix: 'con' });
-
-// configs adjusted by screen width:
-styleSheet({
-    ...atGlobal({
-        ...atRoot({
-            ...rules([
-                // the <Container> size is determined by screen width:
-                Object.keys(breakpoints)
-                .map((breakpointName) =>
-                    ifScreenWidthAtLeast(breakpointName, {
-                        // overwrites propName = propName{BreakpointName}:
-                        ...overwriteProps(containers, usesSuffixedProps(containers, breakpointName)),
-                    }),
-                ),
-            ]),
-        }, { specificityWeight: 2 }), // increase the specificity to win with the specificity in cssConfig's :root
-    }),
-});
 
 
 
