10e97be26e1c0d481954d1159eac69bedce17f7b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-11 17:16:03 +0700

    updated progress

--- composite-components/progress/src/Progress.tsx
+++ composite-components/progress/src/Progress.tsx
@@ -275,6 +275,93 @@ export const useRunnable = ({running}: RunnableProps) => ({
 
 
 
+// configs:
+export const [progresses, progressValues, cssProgressConfig] = cssConfig(() => {
+    //#region keyframes
+    const [keyframesItemRunningRule, keyframesItemRunning] = keyframes({
+        from  : {
+            backgroundPositionX : ['1rem', 0],
+        },
+        to    : {
+            backgroundPositionX : [0, 0],
+        },
+    });
+    keyframesItemRunning.value = 'running'; // the @keyframes name should contain 'running' in order to be recognized by `useRunnable`
+    
+    
+    
+    const [keyframesItemRunningBlockRule, keyframesItemRunningBlock] = keyframes({
+        from  : {
+            backgroundPositionY : ['1rem', 0],
+        },
+        to    : {
+            backgroundPositionY : [0, 0],
+        },
+    });
+    keyframesItemRunningBlock.value = 'runningBlock'; // the @keyframes name should contain 'running' in order to be recognized by `useRunnable`
+    //#endregion keyframes
+    
+    
+    
+    return {
+        // sizes:
+        minInlineSize            : 'unset'      as CssKnownProps['minInlineSize'], // fills the entire parent's width:
+        minBlockSize             : 'auto'       as CssKnownProps['minBlockSize' ], // depends on ProgressBar's height
+        
+        minInlineSizeBlock       : 'auto'       as CssKnownProps['minInlineSize'], // depends on ProgressBar's width
+        minBlockSizeBlock        : '10rem'      as CssKnownProps['minBlockSize' ], // manually set the min height
+        
+        
+        
+        // backgrounds:
+        backgGrad      : basics.backgGrad       as CssKnownProps['backgroundImage'],
+        backgGradBlock : (() => {
+            const value = [[...(basicValues.backgGrad as Extract<CssKnownProps['backgroundImage'], any[][]>)[0]]] as Extract<CssKnownProps['backgroundImage'], any[][]>;
+            value[0][0] = value[0][0].toString().replace('180deg', '270deg');
+            return value;
+        })()                                    as CssKnownProps['backgroundImage'],
+        
+        barBackgStripedImg       : 'linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent)'   as CssKnownProps['backgroundImage'],
+        barBackgStripedSize      : '1rem'       as CssKnownProps['backgroundSize' ],
+        barBackgStripedSizeSm    : '0.25rem'    as CssKnownProps['backgroundSize' ],
+        barBackgStripedSizeLg    : '3rem'       as CssKnownProps['backgroundSize' ],
+        
+        
+        
+        barBoxSizing             : 'border-box' as CssKnownProps['boxSizing'    ], // the final size is including borders & paddings
+        
+        barMinInlineSize         : 'unset'      as CssKnownProps['minInlineSize'],
+        barMinBlockSize          : spacers.md   as CssKnownProps['minBlockSize' ],
+        barMinBlockSizeSm        : spacers.xs   as CssKnownProps['minBlockSize' ],
+        barMinBlockSizeLg        : spacers.xl   as CssKnownProps['minBlockSize' ],
+        
+        barMinBlockSizeBlock     : 'unset'      as CssKnownProps['minBlockSize' ],
+        barMinInlineSizeBlock    : spacers.md   as CssKnownProps['minInlineSize'],
+        barMinInlineSizeBlockSm  : spacers.xs   as CssKnownProps['minInlineSize'],
+        barMinInlineSizeBlockLg  : spacers.xl   as CssKnownProps['minInlineSize'],
+        
+        
+        
+        // spacings:
+        barPaddingInline         : '0px'        as CssKnownProps['paddingInline'],
+        barPaddingBlock          : '0px'        as CssKnownProps['paddingBlock' ],
+        
+        
+        
+        // animations:
+        ...keyframesItemRunningRule,
+        ...keyframesItemRunningBlockRule,
+        barAnimRunning           : [
+            ['1000ms', 'linear', 'both', 'infinite', keyframesItemRunning]
+        ]                                       as CssKnownProps['animation'],
+        barAnimRunningBlock      : [
+            ['1000ms', 'linear', 'both', 'infinite', keyframesItemRunningBlock]
+        ]                                       as CssKnownProps['animation'],
+    };
+}, { prefix: 'prgs' });
+
+
+
 // styles:
 export const usesProgressLayout = (options?: OrientationableOptions) => {
     // options:
@@ -518,93 +605,6 @@ export const useProgressBarVariant = (props: ProgressBarVariant) => {
         class: props.progressBarStyle ?? null,
     };
 };
-
-
-
-// configs:
-export const [progresses, progressValues, cssProgressConfig] = cssConfig(() => {
-    //#region keyframes
-    const [keyframesItemRunningRule, keyframesItemRunning] = keyframes({
-        from  : {
-            backgroundPositionX : ['1rem', 0],
-        },
-        to    : {
-            backgroundPositionX : [0, 0],
-        },
-    });
-    keyframesItemRunning.value = 'running'; // the @keyframes name should contain 'running' in order to be recognized by `useRunnable`
-    
-    
-    
-    const [keyframesItemRunningBlockRule, keyframesItemRunningBlock] = keyframes({
-        from  : {
-            backgroundPositionY : ['1rem', 0],
-        },
-        to    : {
-            backgroundPositionY : [0, 0],
-        },
-    });
-    keyframesItemRunningBlock.value = 'runningBlock'; // the @keyframes name should contain 'running' in order to be recognized by `useRunnable`
-    //#endregion keyframes
-    
-    
-    
-    return {
-        // sizes:
-        minInlineSize            : 'unset'      as CssKnownProps['minInlineSize'], // fills the entire parent's width:
-        minBlockSize             : 'auto'       as CssKnownProps['minBlockSize' ], // depends on ProgressBar's height
-        
-        minInlineSizeBlock       : 'auto'       as CssKnownProps['minInlineSize'], // depends on ProgressBar's width
-        minBlockSizeBlock        : '10rem'      as CssKnownProps['minBlockSize' ], // manually set the min height
-        
-        
-        
-        // backgrounds:
-        backgGrad      : basics.backgGrad       as CssKnownProps['backgroundImage'],
-        backgGradBlock : (() => {
-            const value = [[...(basicValues.backgGrad as Extract<CssKnownProps['backgroundImage'], any[][]>)[0]]] as Extract<CssKnownProps['backgroundImage'], any[][]>;
-            value[0][0] = value[0][0].toString().replace('180deg', '270deg');
-            return value;
-        })()                                    as CssKnownProps['backgroundImage'],
-        
-        barBackgStripedImg       : 'linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent)'   as CssKnownProps['backgroundImage'],
-        barBackgStripedSize      : '1rem'       as CssKnownProps['backgroundSize' ],
-        barBackgStripedSizeSm    : '0.25rem'    as CssKnownProps['backgroundSize' ],
-        barBackgStripedSizeLg    : '3rem'       as CssKnownProps['backgroundSize' ],
-        
-        
-        
-        barBoxSizing             : 'border-box' as CssKnownProps['boxSizing'    ], // the final size is including borders & paddings
-        
-        barMinInlineSize         : 'unset'      as CssKnownProps['minInlineSize'],
-        barMinBlockSize          : spacers.md   as CssKnownProps['minBlockSize' ],
-        barMinBlockSizeSm        : spacers.xs   as CssKnownProps['minBlockSize' ],
-        barMinBlockSizeLg        : spacers.xl   as CssKnownProps['minBlockSize' ],
-        
-        barMinBlockSizeBlock     : 'unset'      as CssKnownProps['minBlockSize' ],
-        barMinInlineSizeBlock    : spacers.md   as CssKnownProps['minInlineSize'],
-        barMinInlineSizeBlockSm  : spacers.xs   as CssKnownProps['minInlineSize'],
-        barMinInlineSizeBlockLg  : spacers.xl   as CssKnownProps['minInlineSize'],
-        
-        
-        
-        // spacings:
-        barPaddingInline         : '0px'        as CssKnownProps['paddingInline'],
-        barPaddingBlock          : '0px'        as CssKnownProps['paddingBlock' ],
-        
-        
-        
-        // animations:
-        ...keyframesItemRunningRule,
-        ...keyframesItemRunningBlockRule,
-        barAnimRunning           : [
-            ['1000ms', 'linear', 'both', 'infinite', keyframesItemRunning]
-        ]                                       as CssKnownProps['animation'],
-        barAnimRunningBlock      : [
-            ['1000ms', 'linear', 'both', 'infinite', keyframesItemRunningBlock]
-        ]                                       as CssKnownProps['animation'],
-    };
-}, { prefix: 'prgs' });
 
 
 
