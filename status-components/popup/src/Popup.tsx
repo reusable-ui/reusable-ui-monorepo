f4fcf9e964e37213ababd5ce9df43ce90ba5c32a
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-11 13:03:25 +0700

    updated popup

--- status-components/popup/src/Popup.tsx
+++ status-components/popup/src/Popup.tsx
@@ -73,6 +73,51 @@ import {
 
 
 
+// configs:
+export const [popups, popupValues, cssPopupConfig] = cssConfig(() => {
+    //#region keyframes
+    const frameCollapsed    = style({
+        opacity   : 0,
+        transform : 'scale(0)',
+    });
+    const frameIntermediate = style({
+        transform : 'scale(1.02)',
+    });
+    const frameExpanded     = style({
+        opacity   : 1,
+        transform : 'scale(1)',
+    });
+    const [keyframesExpandRule  , keyframesExpand  ] = keyframes({
+        from  : frameCollapsed,
+        '70%' : frameIntermediate,
+        to    : frameExpanded,
+    });
+    keyframesExpand.value   = 'expand';   // the @keyframes name should contain 'expand'   in order to be recognized by `useCollapsible`
+    const [keyframesCollapseRule, keyframesCollapse] = keyframes({
+        from  : frameExpanded,
+        '30%' : frameIntermediate,
+        to    : frameCollapsed,
+    });
+    keyframesCollapse.value = 'collapse'; // the @keyframes name should contain 'collapse' in order to be recognized by `useCollapsible`
+    //#endregion keyframes
+    
+    
+    
+    return {
+        // animations:
+        ...keyframesExpandRule,
+        ...keyframesCollapseRule,
+        animExpand       : [
+            ['300ms', 'ease-out', 'both', keyframesExpand  ],
+        ]                                                       as CssKnownProps['animation'],
+        animCollapse     : [
+            ['500ms', 'ease-out', 'both', keyframesCollapse],
+        ]                                                       as CssKnownProps['animation'],
+    };
+}, { prefix: 'pop' });
+
+
+
 // styles:
 export const usesPopupLayout = () => {
     return style({
@@ -143,51 +188,6 @@ export const usePopupStyleSheet = dynamicStyleSheet(() => ({
         usesPopupStates(),
     ]),
 }), { id: 'usjjnl1scl' }); // a unique salt for SSR support, ensures the server-side & client-side have the same generated class names
-
-
-
-// configs:
-export const [popups, popupValues, cssPopupConfig] = cssConfig(() => {
-    //#region keyframes
-    const frameCollapsed    = style({
-        opacity   : 0,
-        transform : 'scale(0)',
-    });
-    const frameIntermediate = style({
-        transform : 'scale(1.02)',
-    });
-    const frameExpanded     = style({
-        opacity   : 1,
-        transform : 'scale(1)',
-    });
-    const [keyframesExpandRule  , keyframesExpand  ] = keyframes({
-        from  : frameCollapsed,
-        '70%' : frameIntermediate,
-        to    : frameExpanded,
-    });
-    keyframesExpand.value   = 'expand';   // the @keyframes name should contain 'expand'   in order to be recognized by `useCollapsible`
-    const [keyframesCollapseRule, keyframesCollapse] = keyframes({
-        from  : frameExpanded,
-        '30%' : frameIntermediate,
-        to    : frameCollapsed,
-    });
-    keyframesCollapse.value = 'collapse'; // the @keyframes name should contain 'collapse' in order to be recognized by `useCollapsible`
-    //#endregion keyframes
-    
-    
-    
-    return {
-        // animations:
-        ...keyframesExpandRule,
-        ...keyframesCollapseRule,
-        animExpand       : [
-            ['300ms', 'ease-out', 'both', keyframesExpand  ],
-        ]                                                       as CssKnownProps['animation'],
-        animCollapse     : [
-            ['500ms', 'ease-out', 'both', keyframesCollapse],
-        ]                                                       as CssKnownProps['animation'],
-    };
-}, { prefix: 'pop' });
 
 
 
