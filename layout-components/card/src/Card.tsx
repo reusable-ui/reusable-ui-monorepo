c0984063bdd142d3d7dbcbad481c6b7060943354
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-11 12:50:02 +0700

    updated card

--- layout-components/card/src/Card.tsx
+++ layout-components/card/src/Card.tsx
@@ -126,6 +126,47 @@ const _defaultBodySemanticRole   : SemanticRole = ''        // no corresponding 
 
 
 
+// configs:
+export const [cards, cardValues, cssCardConfig] = cssConfig(() => {
+    return {
+        // borders:
+        borderStyle    : basics.borderStyle     as CssKnownProps['borderStyle' ],
+        borderWidth    : basics.borderWidth     as CssKnownProps['borderWidth' ],
+        borderColor    : basics.borderColor     as CssKnownProps['borderColor' ],
+        
+        borderRadius   : basics.borderRadius    as CssKnownProps['borderRadius'],
+        borderRadiusSm : basics.borderRadiusSm  as CssKnownProps['borderRadius'],
+        borderRadiusLg : basics.borderRadiusLg  as CssKnownProps['borderRadius'],
+        
+        
+        
+        // animations:
+        transition     : basics.transition      as CssKnownProps['transition'],
+        itemTransition : basics.transition      as CssKnownProps['transition'],
+        
+        
+        
+        // sizes:
+        boxSizing      : 'border-box'           as CssKnownProps['boxSizing'], // the final size is including borders & paddings
+        blockSize      : '100%'                 as CssKnownProps['blockSize'], // fills the entire parent's height if the parent has a specific height, otherwise no effect
+        
+        
+        
+        // typos:
+        overflowWrap   : 'break-word'           as CssKnownProps['overflowWrap'], // prevents a long word from breaking Card layout
+        
+        
+        
+        // captions:
+        captionFilter  : [[
+            'brightness(70%)',
+            'contrast(140%)',
+        ]]                                      as CssKnownProps['filter'],
+    };
+}, { prefix: 'card' });
+
+
+
 // styles:
 export const headerElm = '.header' // one degree specificity to overwrite <CardHeader> component
 export const footerElm = '.footer' // one degree specificity to overwrite <CardFooter> component
@@ -449,47 +490,6 @@ export const useCardVariant = (props: CardVariant) => {
         class: props.cardStyle ?? null,
     };
 };
-
-
-
-// configs:
-export const [cards, cardValues, cssCardConfig] = cssConfig(() => {
-    return {
-        // borders:
-        borderStyle    : basics.borderStyle     as CssKnownProps['borderStyle' ],
-        borderWidth    : basics.borderWidth     as CssKnownProps['borderWidth' ],
-        borderColor    : basics.borderColor     as CssKnownProps['borderColor' ],
-        
-        borderRadius   : basics.borderRadius    as CssKnownProps['borderRadius'],
-        borderRadiusSm : basics.borderRadiusSm  as CssKnownProps['borderRadius'],
-        borderRadiusLg : basics.borderRadiusLg  as CssKnownProps['borderRadius'],
-        
-        
-        
-        // animations:
-        transition     : basics.transition      as CssKnownProps['transition'],
-        itemTransition : basics.transition      as CssKnownProps['transition'],
-        
-        
-        
-        // sizes:
-        boxSizing      : 'border-box'           as CssKnownProps['boxSizing'], // the final size is including borders & paddings
-        blockSize      : '100%'                 as CssKnownProps['blockSize'], // fills the entire parent's height if the parent has a specific height, otherwise no effect
-        
-        
-        
-        // typos:
-        overflowWrap   : 'break-word'           as CssKnownProps['overflowWrap'], // prevents a long word from breaking Card layout
-        
-        
-        
-        // captions:
-        captionFilter  : [[
-            'brightness(70%)',
-            'contrast(140%)',
-        ]]                                      as CssKnownProps['filter'],
-    };
-}, { prefix: 'card' });
 
 
 
