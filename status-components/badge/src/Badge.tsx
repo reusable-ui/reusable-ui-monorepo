bcb39c27ecbddfb211da4bdee03ed124e677417b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-11 13:09:01 +0700

    updated badge

--- status-components/badge/src/Badge.tsx
+++ status-components/badge/src/Badge.tsx
@@ -94,6 +94,44 @@ import {
 
 
 
+// configs:
+export const [badges, badgeValues, cssBadgeConfig] = cssConfig(() => {
+    const basics = {
+        // spacings:
+        paddingInline   : '0.65em'                                          as CssKnownProps['paddingInline'],
+        paddingBlock    : '0.35em'                                          as CssKnownProps['paddingBlock' ],
+        
+        
+        
+        // typos:
+        whiteSpace      : 'normal'                                          as CssKnownProps['whiteSpace'    ],
+        fontSize        : '0.75em'                                          as CssKnownProps['fontSize'      ],
+        fontWeight      : typos.fontWeightBold                              as CssKnownProps['fontWeight'    ],
+    };
+    
+    
+    
+    return {
+        ...basics,
+        
+        
+        
+        // spacings:
+        paddingInlineSm : [['calc(', basics.paddingInline, '/', 1.25, ')']] as CssKnownProps['paddingInline'],
+        paddingBlockSm  : [['calc(', basics.paddingBlock , '/', 1.25, ')']] as CssKnownProps['paddingBlock' ],
+        paddingInlineLg : [['calc(', basics.paddingInline, '*', 1.25, ')']] as CssKnownProps['paddingInline'],
+        paddingBlockLg  : [['calc(', basics.paddingBlock , '*', 1.25, ')']] as CssKnownProps['paddingBlock' ],
+        
+        
+        
+        // typos:
+        fontSizeSm      : [['calc(', basics.fontSize     , '/', 1.25, ')']] as CssKnownProps['fontSize'      ],
+        fontSizeLg      : [['calc(', basics.fontSize     , '*', 1.25, ')']] as CssKnownProps['fontSize'      ],
+    };
+}, { prefix: 'bge' });
+
+
+
 // styles:
 export const usesBadgeLayout = () => {
     // dependencies:
@@ -255,44 +293,6 @@ export const useBadgeVariant = (props: BadgeVariant) => {
         class: props.badgeStyle ?? null,
     };
 };
-
-
-
-// configs:
-export const [badges, badgeValues, cssBadgeConfig] = cssConfig(() => {
-    const basics = {
-        // spacings:
-        paddingInline   : '0.65em'                                          as CssKnownProps['paddingInline'],
-        paddingBlock    : '0.35em'                                          as CssKnownProps['paddingBlock' ],
-        
-        
-        
-        // typos:
-        whiteSpace      : 'normal'                                          as CssKnownProps['whiteSpace'    ],
-        fontSize        : '0.75em'                                          as CssKnownProps['fontSize'      ],
-        fontWeight      : typos.fontWeightBold                              as CssKnownProps['fontWeight'    ],
-    };
-    
-    
-    
-    return {
-        ...basics,
-        
-        
-        
-        // spacings:
-        paddingInlineSm : [['calc(', basics.paddingInline, '/', 1.25, ')']] as CssKnownProps['paddingInline'],
-        paddingBlockSm  : [['calc(', basics.paddingBlock , '/', 1.25, ')']] as CssKnownProps['paddingBlock' ],
-        paddingInlineLg : [['calc(', basics.paddingInline, '*', 1.25, ')']] as CssKnownProps['paddingInline'],
-        paddingBlockLg  : [['calc(', basics.paddingBlock , '*', 1.25, ')']] as CssKnownProps['paddingBlock' ],
-        
-        
-        
-        // typos:
-        fontSizeSm      : [['calc(', basics.fontSize     , '/', 1.25, ')']] as CssKnownProps['fontSize'      ],
-        fontSizeLg      : [['calc(', basics.fontSize     , '*', 1.25, ')']] as CssKnownProps['fontSize'      ],
-    };
-}, { prefix: 'bge' });
 
 
 
