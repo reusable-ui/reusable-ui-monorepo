2cbf3ca81bc1a0239a61a82adfd3eb318c58c4d2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-11-20 11:08:27 +0700

    updated list

--- layout-components/list/src/List.tsx
+++ layout-components/list/src/List.tsx
@@ -292,15 +292,16 @@ export const [lists, listValues, cssListConfig] = cssConfig(() => {
 
 
 // styles:
-export type ListBasicStyle = 'regular'|'flat'|'flush'|'joined';
+export type ListBasicStyle    = 'regular'|'flat'|'flush'|'joined';
 export type ListSpecificStyle = 'content'|'button'|'tab'|'breadcrumb'|'bullet'|'numbered' // might be added more styles in the future
-export type ListStyle =
-    |ListBasicStyle
-    |ListSpecificStyle
-    |[ListBasicStyle]
-    |[ListSpecificStyle]
-    |[ListBasicStyle, ListSpecificStyle]
-    |[ListSpecificStyle, ListBasicStyle]
+export type ListCompositeStyle<TListBasicStyle extends string, TListSpecificStyle extends string> =
+    |TListBasicStyle
+    |TListSpecificStyle
+    |[TListBasicStyle]
+    |[TListSpecificStyle]
+    |[TListBasicStyle, TListSpecificStyle]
+    |[TListSpecificStyle, TListBasicStyle]
+export type ListStyle = ListCompositeStyle<ListBasicStyle, ListSpecificStyle>
 export interface ListVariant {
     listStyle ?: ListStyle
 }
