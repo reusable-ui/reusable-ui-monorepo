cbac06897f8f8c3ca2f8b25c509c65349c731f99
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-12-07 15:07:15 +0700

    updated alert

--- notification-components/alert/src/Alert.tsx
+++ notification-components/alert/src/Alert.tsx
@@ -151,47 +151,41 @@ export const usesAlertLayout = () => {
         ]),
         ...style({
             // layouts:
-            display         : 'grid',        // use css grid for layouting, so we can customize the desired area later.
+            display          : 'grid',        // use css grid for layouting, so we can customize the desired area later.
             
             // explicit areas:
             /*
                 just one explicit area: `body`
                 `icon` & `control` rely on implicit area
             */
-            gridTemplate    : [[
+            gridTemplate     : [[
                 '"body" auto', // fluid height
                 '/',
                 ' auto'        // fluid width
             ]],
             
             // implicit areas:
-            gridAutoFlow    : 'column',      // if child's gridArea was not specified => place it automatically at horz direction
-            gridAutoRows    : 'min-content', // other areas than `body` should take the minimum required height
-            gridAutoColumns : 'min-content', // other areas than `body` should take the minimum required width
+            gridAutoFlow     : 'column',      // if child's gridArea was not specified => place it automatically at horz direction
+            gridAutoRows     : 'min-content', // other areas than `body` should take the minimum required height
+            gridAutoColumns  : 'min-content', // other areas than `body` should take the minimum required width
             // the gridArea's size configured as *minimum* content's size required => no free space left to distribute => so (justify|algin)Content is *not required*
             
             // child default sizes:
-            justifyItems    : 'stretch',     // each section fills the entire area's width
-            alignItems      : 'stretch',     // each section fills the entire area's height
-            
-            
-            
-            // scrolls:
-            overflow        : 'hidden',     // force to overflowWrap
-            overflowWrap    : 'break-word', // break long word to the next line
+            justifyItems     : 'stretch',     // each section fills the entire area's width
+            alignItems       : 'stretch',     // each section fills the entire area's height
             
             
             
             // children:
             ...children(iconElm, {
                 // layouts:
-                gridArea    : '1 / -3', // the first row / the third column starting from the last
+                gridArea     : '1 / -3', // the first row / the third column starting from the last
                 
                 
                 
                 // sizes:
-                justifySelf : 'center', // align horizontally to center
-                alignSelf   : 'start',  // align vertically   to top
+                justifySelf  : 'center', // align horizontally to center
+                alignSelf    : 'start',  // align vertically   to top
                 
                 
                 
@@ -200,7 +194,13 @@ export const usesAlertLayout = () => {
             }),
             ...children(bodyElm, {
                 // layouts:
-                gridArea    : 'body',
+                gridArea     : 'body',
+                
+                
+                
+                // scrolls:
+                overflow     : 'hidden',     // force to overflowWrap
+                overflowWrap : 'break-word', // break long word to the next line
                 
                 
                 
@@ -209,13 +209,13 @@ export const usesAlertLayout = () => {
             }),
             ...children(controlElm, {
                 // layouts:
-                gridArea    : '1 / 2',  // the first row / the second column
+                gridArea     : '1 / 2',  // the first row / the second column
                 
                 
                 
                 // sizes:
-                justifySelf : 'center', // align horizontally to center
-                alignSelf   : 'start',  // align vertically   to top
+                justifySelf  : 'center', // align horizontally to center
+                alignSelf    : 'start',  // align vertically   to top
                 
                 
                 
