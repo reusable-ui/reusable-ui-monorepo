c7439822055e509796d447428c8d932e946db1ad
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-12-23 15:21:36 +0700

    updated tooltip

--- notification-components/tooltip/src/Tooltip.tsx
+++ notification-components/tooltip/src/Tooltip.tsx
@@ -308,8 +308,11 @@ export type CalculateArrowSize  = (props: ArrowProps) => Promise<ArrowSize>
 const isSizeClassName = (className: string) => className.startsWith('sz') && (className.at(2) === className.at(2)?.toUpperCase());
 const arrowSizeCache = new WeakMap<Element, readonly [string, string, ArrowSize]>();
 const defaultCalculateArrowSize : CalculateArrowSize = async ({ arrow, placement }) => {
-    const arrowPosition = getComputedStyle(arrow).position;
-    if (arrowPosition === 'static') return [0, 0]; // cssfn is not fully loaded => ignore calculation
+    const cached = arrowSizeCache.get(arrow);
+    if (!cached) {
+        const arrowPosition = getComputedStyle(arrow).position;
+        if (arrowPosition === 'static') return [0, 0]; // cssfn is not fully loaded => ignore calculation
+    } // if
     
     
     
@@ -319,7 +322,6 @@ const defaultCalculateArrowSize : CalculateArrowSize = async ({ arrow, placement
     
     
     
-    const cached = arrowSizeCache.get(arrow);
     if (cached) {
         const [cachedSizeName, cachedPlacement, cachedArrowSize] = cached;
         if ((cachedSizeName === sizeName) && (cachedPlacement === basePlacement)) return cachedArrowSize;
