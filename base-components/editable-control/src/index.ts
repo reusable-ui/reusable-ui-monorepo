b805ffec55b01a3d35838e565e93a6b082d3f118
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 04:51:09 +0700

    updated editable-control

--- base-components/editable-control/src/index.ts
+++ base-components/editable-control/src/index.ts
@@ -1,3 +1,4 @@
-export * from './styles/config.js'
-export * from './styles/styles.js'
-export * from './EditableControl.js'
+export *            from './styles/config.js'
+export *            from './styles/styles.js'
+export *            from './EditableControl.js'
+export {default}    from './EditableControl.js'
