ff98ed396d3ff9da7ed6f31768f35383eaafe80e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 04:29:30 +0700

    updated typos

--- configs/typos/src/configs/index.ts
+++ configs/typos/src/configs/index.ts
@@ -1,18 +1,19 @@
 // internals:
 
-export * from './typos.js'
-export * from './secondaries.js'
+export *            from './typos.js'
+export {default}    from './typos.js'
+export *            from './secondaries.js'
 
-export * from './paragraphs.js'
-export * from './leads.js'
+export *            from './paragraphs.js'
+export *            from './leads.js'
 
-export * from './headings.js'
-export * from './displays.js'
+export *            from './headings.js'
+export *            from './displays.js'
 
-export * from './blockquotes.js'
+export *            from './blockquotes.js'
 
-export * from './marks.js'
-export * from './codes.js'
-export * from './kbds.js'
+export *            from './marks.js'
+export *            from './codes.js'
+export *            from './kbds.js'
 
-export * from './horzRules.js'
+export *            from './horzRules.js'
--- configs/typos/src/index.ts
+++ configs/typos/src/index.ts
@@ -1,5 +1,6 @@
 // internals:
 
-export * from './configs/index.js'
-export * from './styles/index.js'
-import        './effects/index.js' // side effect
+export *            from './configs/index.js'
+export {default}    from './configs/index.js'
+export *            from './styles/index.js'
+import                   './effects/index.js' // side effect
