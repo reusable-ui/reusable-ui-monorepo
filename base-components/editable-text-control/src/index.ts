777ea1a31679b981ef05e2dfc92b242a81c48fde
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 04:53:51 +0700

    updated editable-text-control

--- base-components/editable-text-control/src/index.ts
+++ base-components/editable-text-control/src/index.ts
@@ -1,3 +1,4 @@
-export * from './styles/config.js'
-export * from './styles/styles.js'
-export * from './EditableTextControl.js'
+export *            from './styles/config.js'
+export *            from './styles/styles.js'
+export *            from './EditableTextControl.js'
+export {default}    from './EditableTextControl.js'
