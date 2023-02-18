77d4c6d42efde4ea6069ec944d3590b1b5cb6164
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 05:07:32 +0700

    updated label

--- simple-components/label/src/index.ts
+++ simple-components/label/src/index.ts
@@ -1,5 +1,6 @@
-export * from './variants/LabelVariant.js'
+export *            from './variants/LabelVariant.js'
 
-export * from './styles/config.js'
-export * from './styles/styles.js'
-export * from './Label.js'
+export *            from './styles/config.js'
+export *            from './styles/styles.js'
+export *            from './Label.js'
+export {default}    from './Label.js'
