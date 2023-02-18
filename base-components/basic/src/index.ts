0d3af94585522103495b153c93c8de8fd03b5aa8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 04:40:53 +0700

    updated basic

--- base-components/basic/src/index.ts
+++ base-components/basic/src/index.ts
@@ -1,3 +1,4 @@
-export * from './styles/config.js'
-export * from './styles/styles.js'
-export * from './Basic.js'
+export *            from './styles/config.js'
+export *            from './styles/styles.js'
+export *            from './Basic.js'
+export {default}    from './Basic.js'
