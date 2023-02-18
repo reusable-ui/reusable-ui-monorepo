3194afc7e7a8346b692dcd58da206b2b9c310a1c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 05:01:57 +0700

    updated content

--- base-content-components/content/src/index.ts
+++ base-content-components/content/src/index.ts
@@ -1,3 +1,4 @@
-export * from './styles/config.js'
-export * from './styles/styles.js'
-export * from './Content.js'
+export *            from './styles/config.js'
+export *            from './styles/styles.js'
+export *            from './Content.js'
+export {default}    from './Content.js'
