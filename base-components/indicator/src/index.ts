d8b4f124119b47c10ca4f53ffc477b8e49756580
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-19 04:42:36 +0700

    updated indicator

--- base-components/indicator/src/index.ts
+++ base-components/indicator/src/index.ts
@@ -1,3 +1,4 @@
-export * from './styles/config.js'
-export * from './styles/styles.js'
-export * from './Indicator.js'
+export *            from './styles/config.js'
+export *            from './styles/styles.js'
+export *            from './Indicator.js'
+export {default}    from './Indicator.js'
