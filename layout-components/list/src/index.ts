6df5e2f87db7de99cfcd0329abad5d73d2ebb7d5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-23 03:46:34 +0700

    updated list

--- layout-components/list/src/index.ts
+++ layout-components/list/src/index.ts
@@ -1,6 +1,7 @@
 export *            from './defaults.js'
 export *            from './variants/ListVariant.js'
 
+export *            from './styles/elements.js'
 export *            from './styles/config.js'
 export *            from './styles/styles.js'
 export *            from './List.js'
