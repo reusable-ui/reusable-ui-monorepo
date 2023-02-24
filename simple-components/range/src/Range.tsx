7c3c1a9044952684d9771caf08e123399c83fcd0
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-24 20:17:17 +0700

    updated range

--- simple-components/range/src/Range.tsx
+++ simple-components/range/src/Range.tsx
@@ -14,7 +14,7 @@ import {
 }                           from 'react'
 
 // cssfn:
-import {
+import type {
     // cssfn general types:
     Optional,
 }                           from '@cssfn/core'                          // writes css in javascript
