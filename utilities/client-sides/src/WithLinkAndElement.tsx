412e4bef3c6b15565a50cca9e4a4cff84d98de87
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-04-03 13:52:44 +0700

    updated client-sides

--- utilities/client-sides/src/WithLinkAndElement.tsx
+++ utilities/client-sides/src/WithLinkAndElement.tsx
@@ -49,7 +49,7 @@ export interface WithLinkAndElementProps {
      * Optional.
      *   
      * The `children` of `<Element>` that *may* contain `<Link>`.  
-     * It the `<Link>` exists, it will wrap the `<Element>`.  
+     * If the `<Link>` exists, it will wrap the `<Element>`.  
      * The rest `children` will be inside the `<Element>`.  
      *   
      * If not supplied, defaults to `<Element>`'s `children`.  
@@ -67,12 +67,12 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
         
         // children:
         children = elementComponent.props.children, // if not supplied, defaults to `<Element>`'s `children`
-    ...restLinkProps} = props;
+    ...restElementProps} = props;
     
     
     
     // convert the children to array (if necessary):
-    const childrenArray = (Array.isArray(children) ? (children as React.ReactNode[]) : React.Children.toArray(children));
+    const childrenArray : React.ReactNode[] = Array.isArray(children) ? (children as React.ReactNode[]) : React.Children.toArray(children);
     
     // inspect if <Element>'s children contain one/more <Link>:
     const linkComponent = childrenArray.find(isClientSideLink); // take the first <Link> (if any)
@@ -80,7 +80,11 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
     // if no contain <Link> => normal <Element>:
     if (!linkComponent) return React.cloneElement(elementComponent,
         // props:
-        undefined, // keeps the original <Element>'s props
+        {
+            // other props:
+            ...restElementProps,
+            ...elementComponent.props, // overwrites restElementProps (if any conflics)
+        },
         
         
         
@@ -91,9 +95,9 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
     
     
     /*
-        swaps between <Element> and <Link> while maintaining their's children, so:
-        declaration:
-        <Element>
+        merge <Link>'s children and <Element>'s children:
+        
+        declaration: [
             <c1>
             <c2>
             <Link>
@@ -102,23 +106,21 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
             </Link>
             <c5>
             <c6>
-        </Element>
-        rendered to:
-        <Link>
-            <Element>
-                <c1>
-                <c2>
-                <c3>
-                <c4>
-                <c5>
-                <c6>
-            </Element>
-        </Link>
+        ]
+        
+        rendered to: [
+            <c1>
+            <c2>
+            <c3>
+            <c4>
+            <c5>
+            <c6>
+        ]
     */
-    const mergedChildren = (
+    const mergedChildren : React.ReactNode[] = (
         childrenArray
-        .flatMap((child): React.ReactNode[] => { // merge <Link>'s children and <Element>'s children:
-            // current <Element>'s children:
+        .flatMap((child: React.ReactNode): React.ReactNode[] => {
+            // not a found <Link> => current <Element>'s children:
             if (child !== linkComponent) return [child];
             
             
@@ -126,9 +128,9 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
             // merge with <Link>'s children:
             return (
                 React.Children.toArray(linkComponent.props.children) // unwrap the <Link>
-                .map((grandChild) => { // fix the grandChild's key
+                .map((grandChild: React.ReactNode): React.ReactNode => {
                     if (!React.isValidElement(grandChild)) return grandChild;
-                    return React.cloneElement(grandChild, { key: `${child.key}-${grandChild.key}` });
+                    return React.cloneElement(grandChild, { key: `${child.key}-${grandChild.key}` }); // fix the grandChild's key
                 })
             );
         })
@@ -143,9 +145,14 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
     
     
     // jsx:
-    if (!propEnabled) return React.cloneElement(elementComponent, // if <Element> is disabled => no need to wrap with <Link>
+    // if <Element> is disabled => no need to wrap with <Link>:
+    if (!propEnabled) return React.cloneElement(elementComponent,
         // props:
-        undefined, // keeps the original <Element>'s props
+        {
+            // other props:
+            ...restElementProps,
+            ...elementComponent.props, // overwrites restElementProps (if any conflics)
+        },
         
         
         
@@ -153,15 +160,14 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
         mergedChildren, // overwrite the children
     );
     
+    
+    
+    // jsx:
+    // if <Element> is enabled => wraps with <Link>:
     const isNextJsLink = !!linkComponent.props.href;
     return React.cloneElement(linkComponent,
         // props:
         {
-            // other props:
-            ...restLinkProps,
-            
-            
-            
             // link props:
             ...( isNextJsLink  ? { legacyBehavior : true             } : undefined), // NextJs's <Link>
             ...(!isNextJsLink  ? { linkComponent  : elementComponent } : undefined), // ReactRouterCompat's <Link>
@@ -174,8 +180,10 @@ const WithLinkAndElement = (props: WithLinkAndElementProps): JSX.Element|null =>
         (
             !isNextJsLink
             ?
+            // for ReactRouterCompat's <Link> => uses the usual children:
             mergedChildren
             :
+            // for NextJs's <Link> => wraps the children with <Element>:
             React.forwardRef<Element, { outerRef?: React.Ref<Element>, children?: React.ReactNode }>((props, ref): JSX.Element|null => {
                 // rest props:
                 const {
