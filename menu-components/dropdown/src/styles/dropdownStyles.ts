bf5d99c8efe77baf55a8474ead045cb1e41e1e56
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-15 00:00:27 +0700

    updated Dropdown

--- menu-components/dropdown/src/styles/dropdownStyles.ts
+++ menu-components/dropdown/src/styles/dropdownStyles.ts
@@ -16,7 +16,12 @@ import {
 }                           from '@cssfn/core'                  // writes css in javascript
 
 // reusable-ui core:
-import type {
+import {
+    // a capability of UI to stack on top-most of another UI(s) regardless of DOM's stacking context:
+    globalStacks,
+    
+    
+    
     // a capability of UI to rotate its layout:
     OrientationableOptions,
 }                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
@@ -57,7 +62,7 @@ export const usesDropdownLayout = (options?: OrientationableOptions) => {
             
             // positions:
             ...rule('.overlay', {
-                zIndex: 1000,
+                zIndex: globalStacks.dropdown,
             }),
             
             
