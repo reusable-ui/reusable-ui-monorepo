267201efd2a318825dc777d1af7c0aae89dde927
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-15 13:50:12 +0700

    updated ButtonIcon

--- simple-components/button-icon/src/styles/config.ts
+++ simple-components/button-icon/src/styles/config.ts
@@ -29,46 +29,74 @@ import {
 
 // configs:
 export const [buttonIcons, buttonIconValues, cssButtonIconConfig] = cssConfig(() => {
-    return {
+    const bases = {
         // borders:
-        borderRadius      : basics.borderRadius     as CssKnownProps['borderRadius'],
-        borderRadiusXs    : basics.borderRadiusSm   as CssKnownProps['borderRadius'],
-        borderRadiusSm    : basics.borderRadiusSm   as CssKnownProps['borderRadius'],
-        borderRadiusLg    : basics.borderRadiusLg   as CssKnownProps['borderRadius'],
-        borderRadiusXl    : basics.borderRadiusLg   as CssKnownProps['borderRadius'],
+        borderRadiusXs    : basics.borderRadiusSm                                           as CssKnownProps['borderRadius' ],
+        borderRadiusSm    : basics.borderRadiusSm                                           as CssKnownProps['borderRadius' ],
+        borderRadiusMd    : basics.borderRadiusMd                                           as CssKnownProps['borderRadius' ],
+        borderRadiusLg    : basics.borderRadiusLg                                           as CssKnownProps['borderRadius' ],
+        borderRadiusXl    : basics.borderRadiusLg                                           as CssKnownProps['borderRadius' ],
         
         
         
         // spacings:
-        paddingInline     : basics.paddingInline    as CssKnownProps['paddingInline'],
-        paddingBlock      : basics.paddingBlock     as CssKnownProps['paddingBlock' ],
-        paddingInlineXs   : basics.paddingInlineSm  as CssKnownProps['paddingInline'],
-        paddingBlockXs    : basics.paddingBlockSm   as CssKnownProps['paddingBlock' ],
-        paddingInlineSm   : basics.paddingInlineSm  as CssKnownProps['paddingInline'],
-        paddingBlockSm    : basics.paddingBlockSm   as CssKnownProps['paddingBlock' ],
-        paddingInlineLg   : basics.paddingInlineLg  as CssKnownProps['paddingInline'],
-        paddingBlockLg    : basics.paddingBlockLg   as CssKnownProps['paddingBlock' ],
-        paddingInlineXl   : basics.paddingInlineLg  as CssKnownProps['paddingInline'],
-        paddingBlockXl    : basics.paddingBlockLg   as CssKnownProps['paddingBlock' ],
+        paddingInlineXs   : basics.paddingInlineSm                                          as CssKnownProps['paddingInline'],
+        paddingBlockXs    : basics.paddingBlockSm                                           as CssKnownProps['paddingBlock' ],
+        paddingInlineSm   : basics.paddingInlineSm                                          as CssKnownProps['paddingInline'],
+        paddingBlockSm    : basics.paddingBlockSm                                           as CssKnownProps['paddingBlock' ],
+        paddingInlineMd   : basics.paddingInlineMd                                          as CssKnownProps['paddingInline'],
+        paddingBlockMd    : basics.paddingBlockMd                                           as CssKnownProps['paddingBlock' ],
+        paddingInlineLg   : basics.paddingInlineLg                                          as CssKnownProps['paddingInline'],
+        paddingBlockLg    : basics.paddingBlockLg                                           as CssKnownProps['paddingBlock' ],
+        paddingInlineXl   : basics.paddingInlineLg                                          as CssKnownProps['paddingInline'],
+        paddingBlockXl    : basics.paddingBlockLg                                           as CssKnownProps['paddingBlock' ],
         
-        gapInline         : buttons.gapInline       as CssKnownProps['gapInline'],
-        gapBlock          : buttons.gapBlock        as CssKnownProps['gapBlock' ],
-        gapInlineXs       : buttons.gapInlineSm     as CssKnownProps['gapInline'],
-        gapBlockXs        : buttons.gapBlockSm      as CssKnownProps['gapBlock' ],
-        gapInlineSm       : buttons.gapInlineSm     as CssKnownProps['gapInline'],
-        gapBlockSm        : buttons.gapBlockSm      as CssKnownProps['gapBlock' ],
-        gapInlineLg       : buttons.gapInlineLg     as CssKnownProps['gapInline'],
-        gapBlockLg        : buttons.gapBlockLg      as CssKnownProps['gapBlock' ],
-        gapInlineXl       : buttons.gapInlineLg     as CssKnownProps['gapInline'],
-        gapBlockXl        : buttons.gapBlockLg      as CssKnownProps['gapBlock' ],
+        gapInlineXs       : buttons.gapInlineSm                                             as CssKnownProps['gapInline'    ],
+        gapBlockXs        : buttons.gapBlockSm                                              as CssKnownProps['gapBlock'     ],
+        gapInlineSm       : buttons.gapInlineSm                                             as CssKnownProps['gapInline'    ],
+        gapBlockSm        : buttons.gapBlockSm                                              as CssKnownProps['gapBlock'     ],
+        gapInlineMd       : buttons.gapInlineMd                                             as CssKnownProps['gapInline'    ],
+        gapBlockMd        : buttons.gapBlockMd                                              as CssKnownProps['gapBlock'     ],
+        gapInlineLg       : buttons.gapInlineLg                                             as CssKnownProps['gapInline'    ],
+        gapBlockLg        : buttons.gapBlockLg                                              as CssKnownProps['gapBlock'     ],
+        gapInlineXl       : buttons.gapInlineLg                                             as CssKnownProps['gapInline'    ],
+        gapBlockXl        : buttons.gapBlockLg                                              as CssKnownProps['gapBlock'     ],
         
         
         
         // typos:
-        fontSize          : typos.fontSizeMd                                                as CssKnownProps['fontSize'      ],
-        fontSizeXs        : typos.fontSizeSm                                                as CssKnownProps['fontSize'      ],
-        fontSizeSm        : [['calc((', typos.fontSizeSm, '+', typos.fontSizeMd, ')/2)']]   as CssKnownProps['fontSize'      ],
-        fontSizeLg        : typos.fontSizeLg                                                as CssKnownProps['fontSize'      ],
-        fontSizeXl        : typos.fontSizeXl                                                as CssKnownProps['fontSize'      ],
+        fontSizeXs        : typos.fontSizeSm                                                as CssKnownProps['fontSize'     ],
+        fontSizeSm        : [['calc((', typos.fontSizeSm, '+', typos.fontSizeMd, ')/2)']]   as CssKnownProps['fontSize'     ],
+        fontSizeMd        : typos.fontSizeMd                                                as CssKnownProps['fontSize'     ],
+        fontSizeLg        : typos.fontSizeLg                                                as CssKnownProps['fontSize'     ],
+        fontSizeXl        : typos.fontSizeXl                                                as CssKnownProps['fontSize'     ],
+    };
+    
+    
+    
+    const defaults = {
+        // borders:
+        borderRadius      : bases.borderRadiusMd                                            as CssKnownProps['borderRadius' ],
+        
+        
+        
+        // spacings:
+        paddingInline     : bases.paddingInlineMd                                           as CssKnownProps['paddingInline'],
+        paddingBlock      : bases.paddingBlockMd                                            as CssKnownProps['paddingBlock' ],
+        
+        gapInline         : bases.gapInlineMd                                               as CssKnownProps['gapInline'    ],
+        gapBlock          : bases.gapBlockMd                                                as CssKnownProps['gapBlock'     ],
+        
+        
+        
+        // typos:
+        fontSize          : bases.fontSizeMd                                                as CssKnownProps['fontSize'     ],
+    };
+    
+    
+    
+    return {
+        ...bases,
+        ...defaults,
     };
 }, { prefix: 'btni' });
