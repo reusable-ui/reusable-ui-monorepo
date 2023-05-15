3478ddab104b3f11d1a57a41b7c6856968d1eb79
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-15 17:28:07 +0700

    updated spacers

--- configs/spacers/src/spacers.ts
+++ configs/spacers/src/spacers.ts
@@ -21,24 +21,33 @@ export type CssLength = CssComplexValueOf<CssKnownValueOf<'gap'>>
 //#region configs
 export const [spacers, spacerValues, cssSpacerConfig] = cssConfig(() => {
     const bases = {
-        none    : '0rem'    as CssLength,
-        md      : '1rem'    as CssLength,
+        none    : '0rem'                                as CssLength,
+        md      : '1rem'                                as CssLength,
     };
     
-    const defaults = {
-        default : bases.md  as CssLength,
-    };
     
-    return {
-        ...bases,
-        ...defaults,
-        
+    
+    const subs = {
         xxs     : [['calc(', bases.md, '/', 8  , ')']]  as CssLength,
         xs      : [['calc(', bases.md, '/', 4  , ')']]  as CssLength,
         sm      : [['calc(', bases.md, '/', 2  , ')']]  as CssLength,
         
         lg      : [['calc(', bases.md, '*', 1.5, ')']]  as CssLength,
         xl      : [['calc(', bases.md, '*', 3  , ')']]  as CssLength,
+    };
+    
+    
+    
+    const defaults = {
+        default : bases.md                              as CssLength,
+    };
+    
+    
+    
+    return {
+        ...bases,
+        ...subs,
+        ...defaults,
     };
 }, { prefix: 'spc' });
 export { spacers as default }
