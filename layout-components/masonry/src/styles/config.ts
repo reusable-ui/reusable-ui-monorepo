8d0a4c2362eb8d282fb8d205c4e34577dc9a291e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-15 10:57:37 +0700

    updated Masonry

--- layout-components/masonry/src/styles/config.ts
+++ layout-components/masonry/src/styles/config.ts
@@ -19,24 +19,46 @@ import {
 
 // configs:
 export const [masonries, masonryValues, cssMasonryConfig] = cssConfig(() => {
-    return {
+    const bases = {
         // sizes:
-        itemRaiseRowHeight   : '1px'                as CssKnownProps['blockSize'],
-        itemRaiseRowHeightSm : '1px'                as CssKnownProps['blockSize'],
-        itemRaiseRowHeightLg : '2px'                as CssKnownProps['blockSize'],
+        itemRaiseRowHeightSm : '1px'                        as CssKnownProps['blockSize'],
+        itemRaiseRowHeightMd : '1px'                        as CssKnownProps['blockSize'],
+        itemRaiseRowHeightLg : '2px'                        as CssKnownProps['blockSize'],
         
-        itemMinColumnWidth   : 'calc(5 * 40px)'     as CssKnownProps['columnWidth'],
-        itemMinColumnWidthSm : 'calc(3 * 40px)'     as CssKnownProps['columnWidth'],
-        itemMinColumnWidthLg : 'calc(8 * 40px)'     as CssKnownProps['columnWidth'],
+        itemMinColumnWidthSm : 'calc(3 * 40px)'             as CssKnownProps['columnWidth'],
+        itemMinColumnWidthMd : 'calc(5 * 40px)'             as CssKnownProps['columnWidth'],
+        itemMinColumnWidthLg : 'calc(8 * 40px)'             as CssKnownProps['columnWidth'],
         
         
         
         // spacings:
-        gapInline            : spacers.sm           as CssKnownProps['gapInline'],
-        gapInlineSm          : spacers.xs           as CssKnownProps['gapInline'],
-        gapInlineLg          : spacers.md           as CssKnownProps['gapInline'],
-        gapBlock             : spacers.sm           as CssKnownProps['gapBlock' ],
-        gapBlockSm           : spacers.xs           as CssKnownProps['gapBlock' ],
-        gapBlockLg           : spacers.md           as CssKnownProps['gapBlock' ],
+        gapInlineSm          : spacers.xs                   as CssKnownProps['gapInline'],
+        gapInlineMd          : spacers.sm                   as CssKnownProps['gapInline'],
+        gapInlineLg          : spacers.md                   as CssKnownProps['gapInline'],
+        gapBlockSm           : spacers.xs                   as CssKnownProps['gapBlock' ],
+        gapBlockMd           : spacers.sm                   as CssKnownProps['gapBlock' ],
+        gapBlockLg           : spacers.md                   as CssKnownProps['gapBlock' ],
+    };
+    
+    
+    
+    const defaults = {
+        // sizes:
+        itemRaiseRowHeight   : bases.itemRaiseRowHeightMd   as CssKnownProps['blockSize'],
+        
+        itemMinColumnWidth   : bases.itemMinColumnWidthMd   as CssKnownProps['columnWidth'],
+        
+        
+        
+        // spacings:
+        gapInline            : bases.gapInlineMd            as CssKnownProps['gapInline'],
+        gapBlock             : bases.gapBlockMd             as CssKnownProps['gapBlock' ],
+    };
+    
+    
+    
+    return {
+        ...bases,
+        ...defaults,
     };
 }, { prefix: 'msry' });
