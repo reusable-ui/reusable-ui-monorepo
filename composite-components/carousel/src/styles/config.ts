7f2da6846ac344cfc6dcb2979e8479c030231e57
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-15 14:29:13 +0700

    updated Carousel

--- composite-components/carousel/src/styles/config.ts
+++ composite-components/carousel/src/styles/config.ts
@@ -19,19 +19,36 @@ import {
 
 // configs:
 export const [carousels, carouselValues, cssCarouselConfig] = cssConfig(() => {
-    return {
-        // borders:
-        navBtnBorderWidth   : '0px'                     as CssKnownProps['borderWidth' ],
-        navBtnBorderRadius  : '0px'                     as CssKnownProps['borderRadius'],
-        
-        
-        
+    const bases = {
         // spacings:
-        paddingInline       : '0px'                     as CssKnownProps['paddingInline'],
-        paddingBlock        : '0px'                     as CssKnownProps['paddingBlock' ],
+        paddingInline       : '0px'                     as CssKnownProps['paddingInline' ],
+        paddingBlock        : '0px'                     as CssKnownProps['paddingBlock'  ],
         
-        navMarginBlockEnd   : contents.paddingBlock     as CssKnownProps['marginBlockEnd'],
+        
+        
+        // navs:
         navMarginBlockEndSm : contents.paddingBlockSm   as CssKnownProps['marginBlockEnd'],
+        navMarginBlockEndMd : contents.paddingBlockMd   as CssKnownProps['marginBlockEnd'],
         navMarginBlockEndLg : contents.paddingBlockLg   as CssKnownProps['marginBlockEnd'],
+        
+        
+        
+        // navBtns:
+        navBtnBorderWidth   : '0px'                     as CssKnownProps['borderWidth'   ],
+        navBtnBorderRadius  : '0px'                     as CssKnownProps['borderRadius'  ],
+    };
+    
+    
+    
+    const defaults = {
+        // navs:
+        navMarginBlockEnd   : bases.navMarginBlockEndMd as CssKnownProps['marginBlockEnd'],
+    };
+    
+    
+    
+    return {
+        ...bases,
+        ...defaults,
     };
 }, { prefix: 'crsl' });
