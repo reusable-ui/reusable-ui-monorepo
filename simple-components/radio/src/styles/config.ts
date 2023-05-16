18d052263e6d2b0b0e71d09724bd1e87b05b1c2d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-16 16:49:05 +0700

    updated Radio

--- simple-components/radio/src/styles/config.ts
+++ simple-components/radio/src/styles/config.ts
@@ -21,6 +21,6 @@ export const [radios, radioValues, cssRadioConfig] = cssConfig(() => {
     return {
         // animations:
         // forked from Bootstrap 5:
-        indicator : `url("data:image/svg+xml,${escapeSvg("<svg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'><circle r='2' fill='#000'/></svg>")}")` as CssKnownProps['maskImage'],
+        indicator : `url("data:image/svg+xml,${escapeSvg("<svg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'><circle r='2' fill='#000'/></svg>")}")`   as CssKnownProps['maskImage'],
     };
 }, { prefix: 'rad' });
