dec311a6e8bb2a990f02055d009e48313012340f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-16 21:09:00 +0700

    updated Tooltip

--- notification-components/tooltip/src/styles/config.ts
+++ notification-components/tooltip/src/styles/config.ts
@@ -21,22 +21,22 @@ import {
 export const [tooltips, tooltipValues, cssTooltipConfig] = cssConfig(() => {
     const bases = {
         // sizes:
-        arrowInlineSizeMd     : '0.8rem'                                                                    as CssKnownProps['inlineSize'],
-        arrowBlockSizeMd      : '0.8rem'                                                                    as CssKnownProps['blockSize' ],
+        arrowInlineSizeMd     : '0.8rem'                                                                    as CssKnownProps['inlineSize'     ],
+        arrowBlockSizeMd      : '0.8rem'                                                                    as CssKnownProps['blockSize'      ],
         
-     // arrowClipPath         : 'polygon(100% 0, 100% 100%, 0 100%)'                                        as CssKnownProps['clipPath'  ],
-        arrowClipPath         : 'polygon(200% -100%, 200% 200%, -100% 200%)'                                as CssKnownProps['clipPath'  ], // compensates for boxShadow
+     // arrowClipPath         : 'polygon(100% 0, 100% 100%, 0 100%)'                                        as CssKnownProps['clipPath'       ],
+        arrowClipPath         : 'polygon(200% -100%, 200% 200%, -100% 200%)'                                as CssKnownProps['clipPath'       ], // compensates for boxShadow
         
-        arrowTransform        : 'none'                                                                      as CssKnownProps['transform' ],
-        arrowTransformTop     : [['scaleX(0.7)', 'translateY(calc((50% - 0.8px) *  1))', 'rotate(45deg)' ]] as CssKnownProps['transform' ],
-        arrowTransformRight   : [['scaleY(0.7)', 'translateX(calc((50% - 0.8px) * -1))', 'rotate(135deg)']] as CssKnownProps['transform' ],
-        arrowTransformBottom  : [['scaleX(0.7)', 'translateY(calc((50% - 0.8px) * -1))', 'rotate(225deg)']] as CssKnownProps['transform' ],
-        arrowTransformLeft    : [['scaleY(0.7)', 'translateX(calc((50% - 0.8px) *  1))', 'rotate(315deg)']] as CssKnownProps['transform' ],
+        arrowTransform        : 'none'                                                                      as CssKnownProps['transform'      ],
+        arrowTransformTop     : [['scaleX(0.7)', 'translateY(calc((50% - 0.8px) *  1))', 'rotate(45deg)' ]] as CssKnownProps['transform'      ],
+        arrowTransformRight   : [['scaleY(0.7)', 'translateX(calc((50% - 0.8px) * -1))', 'rotate(135deg)']] as CssKnownProps['transform'      ],
+        arrowTransformBottom  : [['scaleX(0.7)', 'translateY(calc((50% - 0.8px) * -1))', 'rotate(225deg)']] as CssKnownProps['transform'      ],
+        arrowTransformLeft    : [['scaleY(0.7)', 'translateX(calc((50% - 0.8px) *  1))', 'rotate(315deg)']] as CssKnownProps['transform'      ],
         
         
         
         // borders:
-        boxShadow             : [[0, 0, '10px', 'rgba(0,0,0,0.5)']]                                         as CssKnownProps['boxShadow'],
+        boxShadow             : [[0, 0, '10px', 'rgba(0,0,0,0.5)']]                                         as CssKnownProps['boxShadow'      ],
         
         
         
@@ -50,33 +50,33 @@ export const [tooltips, tooltipValues, cssTooltipConfig] = cssConfig(() => {
         
         
         // typos:
-        whiteSpace            : 'normal'                                                                    as CssKnownProps['whiteSpace'],
-        fontSizeSm            : typos.fontSizeSm                                                            as CssKnownProps['fontSize'  ],
-        fontSizeMd            : [['calc((', typos.fontSizeSm, '+', typos.fontSizeMd, ')/2)']]               as CssKnownProps['fontSize'  ],
-        fontSizeLg            : typos.fontSizeMd                                                            as CssKnownProps['fontSize'  ],
+        whiteSpace            : 'normal'                                                                    as CssKnownProps['whiteSpace'     ],
+        fontSizeSm            : typos.fontSizeSm                                                            as CssKnownProps['fontSize'       ],
+        fontSizeMd            : [['calc((', typos.fontSizeSm, '+', typos.fontSizeMd, ')/2)']]               as CssKnownProps['fontSize'       ],
+        fontSizeLg            : typos.fontSizeMd                                                            as CssKnownProps['fontSize'       ],
     };
     
     
     
     const subs = {
         // sizes:
-        arrowInlineSizeSm     : [['calc((', bases.arrowInlineSizeMd, ')*0.75)']]                            as CssKnownProps['inlineSize'],
-        arrowBlockSizeSm      : [['calc((', bases.arrowBlockSizeMd , ')*0.75)']]                            as CssKnownProps['blockSize' ],
-        arrowInlineSizeLg     : [['calc((', bases.arrowInlineSizeMd, ')*1.50)']]                            as CssKnownProps['inlineSize'],
-        arrowBlockSizeLg      : [['calc((', bases.arrowBlockSizeMd , ')*1.50)']]                            as CssKnownProps['blockSize' ],
+        arrowInlineSizeSm     : [['calc((', bases.arrowInlineSizeMd, ')*0.75)']]                            as CssKnownProps['inlineSize'     ],
+        arrowBlockSizeSm      : [['calc((', bases.arrowBlockSizeMd , ')*0.75)']]                            as CssKnownProps['blockSize'      ],
+        arrowInlineSizeLg     : [['calc((', bases.arrowInlineSizeMd, ')*1.50)']]                            as CssKnownProps['inlineSize'     ],
+        arrowBlockSizeLg      : [['calc((', bases.arrowBlockSizeMd , ')*1.50)']]                            as CssKnownProps['blockSize'      ],
     };
     
     
     
     const defaults = {
         // sizes:
-        arrowInlineSize       : bases.arrowInlineSizeMd                                                     as CssKnownProps['inlineSize'],
-        arrowBlockSize        : bases.arrowBlockSizeMd                                                      as CssKnownProps['blockSize' ],
+        arrowInlineSize       : bases.arrowInlineSizeMd                                                     as CssKnownProps['inlineSize'     ],
+        arrowBlockSize        : bases.arrowBlockSizeMd                                                      as CssKnownProps['blockSize'      ],
         
         
         
         // typos:
-        fontSize              : bases.fontSizeMd                                                            as CssKnownProps['fontSize'  ],
+        fontSize              : bases.fontSizeMd                                                            as CssKnownProps['fontSize'       ],
     };
     
     
