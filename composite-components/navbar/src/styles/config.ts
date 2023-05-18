88e21bcfc58c94f3588cd78419e6faad24628290
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-18 23:47:30 +0700

    updated Navbar

--- composite-components/navbar/src/styles/config.ts
+++ composite-components/navbar/src/styles/config.ts
@@ -29,86 +29,110 @@ import {
 
 // configs:
 export const [navbars, navbarValues, cssNavbarConfig] = cssConfig(() => {
-    return {
+    const bases = {
         // layouts:
-        display                   : 'grid'                      as CssKnownProps['display'         ],
-        gridAutoFlow              : 'column'                    as CssKnownProps['gridAutoFlow'    ], // the items (logo, toggler, etc) placed automatically to a new column
-        gridAutoColumns           : 'auto'                      as CssKnownProps['gridAutoColumns' ], // each items (logo, toggler, etc) take a space automatically
-        gridTemplateRows          : [[
+        display                     : 'grid'                            as CssKnownProps['display'         ],
+        gridAutoFlow                : 'column'                          as CssKnownProps['gridAutoFlow'    ], // the items (logo, toggler, etc) placed automatically to a new column
+        gridAutoColumns             : 'auto'                            as CssKnownProps['gridAutoColumns' ], // each items (logo, toggler, etc) take a space automatically
+        gridTemplateRows            : [[
             '1fr', // limiting only one row, so the items (logo, toggler, etc) placed automatically to a new column
-        ]]                                                      as CssKnownProps['gridTemplateRows'],
-        justifyContent            : 'space-between'             as CssKnownProps['justifyContent'  ], // separates each items as far as possible
-        alignContent              : 'center'                    as CssKnownProps['alignContent'    ], // the excess vertical space placed at the top & bottom
-        justifyItems              : 'center'                    as CssKnownProps['justifyItems'    ], // prevents from stretching
-        alignItems                : 'center'                    as CssKnownProps['alignItems'      ], // prevents from stretching
+        ]]                                                              as CssKnownProps['gridTemplateRows'],
+        justifyContent              : 'space-between'                   as CssKnownProps['justifyContent'  ], // separates each items as far as possible
+        alignContent                : 'center'                          as CssKnownProps['alignContent'    ], // the excess vertical space placed at the top & bottom
+        justifyItems                : 'center'                          as CssKnownProps['justifyItems'    ], // prevents from stretching
+        alignItems                  : 'center'                          as CssKnownProps['alignItems'      ], // prevents from stretching
         
         
         
         // positions:
-        zIndex                    : globalStacks.sticky         as CssKnownProps['zIndex'          ],
-        position                  : 'sticky'                    as CssKnownProps['position'        ],
-        insetBlockStart           : '0px'                       as CssKnownProps['insetBlockStart' ],
+        zIndex                      : globalStacks.sticky               as CssKnownProps['zIndex'          ],
+        position                    : 'sticky'                          as CssKnownProps['position'        ],
+        insetBlockStart             : '0px'                             as CssKnownProps['insetBlockStart' ],
         
         
         
         // sizes:
-        boxSizing                 : 'content-box'               as CssKnownProps['boxSizing'       ],
-        blockSize                 : 'auto'                      as CssKnownProps['blockSize'       ],
+        boxSizing                   : 'content-box'                     as CssKnownProps['boxSizing'       ],
+        blockSize                   : 'auto'                            as CssKnownProps['blockSize'       ],
         
         
         
         // borders:
-        borderWidth               : '0px'                       as CssKnownProps['borderWidth'     ],
-        borderRadius              : '0px'                       as CssKnownProps['borderRadius'    ],
-        boxShadow                 : [
+        borderWidth                 : '0px'                             as CssKnownProps['borderWidth'     ],
+        borderRadius                : '0px'                             as CssKnownProps['borderRadius'    ],
+        boxShadow                   : [
             [0, 0, '10px', 'rgba(0,0,0,0.5)'],
-        ]                                                       as CssKnownProps['boxShadow'       ],
+        ]                                                               as CssKnownProps['boxShadow'       ],
         
         
         
         // spacings:
-        paddingInline             : containers.paddingInline    as CssKnownProps['paddingInline'   ],
-        paddingBlock              : basics.paddingBlock         as CssKnownProps['paddingBlock'    ],
-        gapInline                 : basics.paddingInline        as CssKnownProps['gapInline'       ],
-        gapBlock                  : basics.paddingBlock         as CssKnownProps['gapBlock'        ],
+        paddingInline               : containers.paddingInline          as CssKnownProps['paddingInline'   ],
+        paddingBlock                : basics.paddingBlock               as CssKnownProps['paddingBlock'    ],
+        gapInline                   : basics.paddingInline              as CssKnownProps['gapInline'       ],
+        gapBlock                    : basics.paddingBlock               as CssKnownProps['gapBlock'        ],
         
         
         
         // lists:
-        listGridArea              : '2/1/2/3'                   as CssKnownProps['gridArea'        ],
-        listGridAreaExpand        : 'unset'                     as CssKnownProps['gridArea'        ],
+        listGridAreaCollapse        : '2/1/2/3'                         as CssKnownProps['gridArea'        ],
+        listGridAreaExpand          : 'unset'                           as CssKnownProps['gridArea'        ],
         
-        listDisplay               : 'flex'                      as CssKnownProps['display'         ],
-        listFlexDirection         : 'column'                    as CssKnownProps['flexDirection'   ],
-        listFlexDirectionExpand   : 'row'                       as CssKnownProps['flexDirection'   ],
-        listJustifySelf           : 'stretch'                   as CssKnownProps['justifySelf'     ],
-        listAlignSelf             : 'stretch'                   as CssKnownProps['alignSelf'       ],
+        listDisplay                 : 'flex'                            as CssKnownProps['display'         ],
+        listFlexDirectionCollapse   : 'column'                          as CssKnownProps['flexDirection'   ],
+        listFlexDirectionExpand     : 'row'                             as CssKnownProps['flexDirection'   ],
+        listJustifySelf             : 'stretch'                         as CssKnownProps['justifySelf'     ],
+        listAlignSelf               : 'stretch'                         as CssKnownProps['alignSelf'       ],
         
-        listMarginInline          : [[
+        listMarginInlineCollapse    : [[
             'calc(0px - ', containers.paddingInline, ')',
-        ]]                                                      as CssKnownProps['marginInline'    ],
-        listMarginInlineExpand    : 'unset'                     as CssKnownProps['marginInline'    ],
-        listMarginBlock           : 'unset'                     as CssKnownProps['marginBlock'     ],
-        listMarginBlockExpand     : [[
+        ]]                                                              as CssKnownProps['marginInline'    ],
+        listMarginBlockCollapse     : 'unset'                           as CssKnownProps['marginBlock'     ],
+        listMarginInlineExpand      : 'unset'                           as CssKnownProps['marginInline'    ],
+        listMarginBlockExpand       : [[
             'calc(0px - ', basics.paddingBlock, ')',
-        ]]                                                      as CssKnownProps['marginBlock'     ],
+        ]]                                                              as CssKnownProps['marginBlock'     ],
         
         // floating lists:
-        listPosition              : 'absolute'                  as CssKnownProps['position'        ],
-        listPositionExpand        : 'unset'                     as CssKnownProps['position'        ],
-        listInsetInline           : '0px'                       as CssKnownProps['insetInline'     ],
-        listInsetInlineExpand     : 'unset'                     as CssKnownProps['insetInline'     ],
-        listInsetBlockStart       : basics.paddingBlock         as CssKnownProps['insetBlockStart' ],
-        listInsetBlockStartExpand : 'unset'                     as CssKnownProps['insetBlockStart' ],
+        listPositionCollapse        : 'absolute'                        as CssKnownProps['position'        ],
+        listPositionExpand          : 'unset'                           as CssKnownProps['position'        ],
+        listInsetInlineCollapse     : '0px'                             as CssKnownProps['insetInline'     ],
+        listInsetBlockStartCollapse : basics.paddingBlock               as CssKnownProps['insetBlockStart' ],
+        listInsetInlineExpand       : 'unset'                           as CssKnownProps['insetInline'     ],
+        listInsetBlockStartExpand   : 'unset'                           as CssKnownProps['insetBlockStart' ],
         
         
         
         // menus:
-        menuDisplay               : 'flex'                      as CssKnownProps['display'         ],
-        menuFlexDirection         : 'row'                       as CssKnownProps['flexDirection'   ],
-        menuJustifyContent        : 'center'                    as CssKnownProps['justifyContent'  ],
-        menuAlignItems            : 'center'                    as CssKnownProps['alignItems'      ],
-        menuFlexWrap              : 'nowrap'                    as CssKnownProps['flexWrap'        ],
-        menuWhiteSpace            : 'nowrap'                    as CssKnownProps['whiteSpace'      ],
+        menuDisplay                 : 'flex'                            as CssKnownProps['display'         ],
+        menuFlexDirection           : 'row'                             as CssKnownProps['flexDirection'   ],
+        menuJustifyContent          : 'center'                          as CssKnownProps['justifyContent'  ],
+        menuAlignItems              : 'center'                          as CssKnownProps['alignItems'      ],
+        menuFlexWrap                : 'nowrap'                          as CssKnownProps['flexWrap'        ],
+        menuWhiteSpace              : 'nowrap'                          as CssKnownProps['whiteSpace'      ],
+    };
+    
+    
+    
+    const defaults = {
+        // lists:
+        listGridArea                : bases.listGridAreaCollapse        as CssKnownProps['gridArea'        ],
+        
+        listFlexDirection           : bases.listFlexDirectionCollapse   as CssKnownProps['flexDirection'   ],
+        
+        listMarginInline            : bases.listMarginInlineCollapse    as CssKnownProps['marginInline'    ],
+        listMarginBlock             : bases.listMarginBlockCollapse     as CssKnownProps['marginBlock'     ],
+        
+        // floating lists:
+        listPosition                : bases.listPositionCollapse        as CssKnownProps['position'        ],
+        listInsetInline             : bases.listInsetInlineCollapse     as CssKnownProps['insetInline'     ],
+        listInsetBlockStart         : bases.listInsetBlockStartCollapse as CssKnownProps['insetBlockStart' ],
+    };
+    
+    
+    
+    return {
+        ...bases,
+        ...defaults,
     };
 }, { prefix: 'navb' });
