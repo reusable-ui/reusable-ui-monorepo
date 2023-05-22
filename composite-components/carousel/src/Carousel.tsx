3006d2668e1ba4dc30719b60903bd1f678838ba4
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-22 10:42:59 +0700

    updated Carousel

--- composite-components/carousel/src/Carousel.tsx
+++ composite-components/carousel/src/Carousel.tsx
@@ -93,7 +93,7 @@ const _defaultNextButtonClasses      : Optional<string>[] = ['nextBtn']
 const _defaultNavscrollClasses       : Optional<string>[] = ['nav']
 
 const _defaultTouchMovementThreshold : number = 20  /* pixel */
-const _defaultTouchDurationThreshold : number = 200 /* milliseconds */
+const _defaultTouchDurationThreshold : number = 300 /* milliseconds */
 
 
 
@@ -780,9 +780,16 @@ const Carousel = <TElement extends HTMLElement = HTMLElement>(props: CarouselPro
         
         
         
-        // temporary disable the snapScroll:
+        // temporary disable the snapScroll on listElm so it can follow our scroll implementation:
         listElm.style.scrollSnapType = 'none';
         listElm.style.scrollBehavior = 'auto';
+        
+        // temporary disable the snapScroll on dummyListElm so it can sync listElm's scroll position:
+        const dummyListElm = dummyListRefInternal.current;
+        if (dummyListElm) { // dummyListElm must be exist for syncing
+            dummyListElm.style.scrollSnapType = 'none';
+            dummyListElm.style.scrollBehavior = 'auto';
+        } // if
     });
     const listHandleTouchMove     = useEvent<React.TouchEventHandler<TElement>>((event) => {
         // conditions:
@@ -922,9 +929,15 @@ const Carousel = <TElement extends HTMLElement = HTMLElement>(props: CarouselPro
         const listStyle  = getComputedStyle(listElm);
         const frameWidth = listElm.clientWidth - (Number.parseInt(listStyle.paddingLeft) || 0) - (Number.parseInt(listStyle.paddingRight ) || 0);
         if (!((listElm.scrollLeft % frameWidth) >= 0.5)) { // scrolling fragment is (almost) zero => it's the moment of a scrolling end
-            // restore the CSS snapScroll:
+            // restore the CSS snapScroll on listElm:
             listElm.style.scrollSnapType = '';
             listElm.style.scrollBehavior = '';
+            
+            // restore the CSS snapScroll on listElm:
+            if (dummyListElm) { // dummyListElm must be exist for syncing
+                dummyListElm.style.scrollSnapType = '';
+                dummyListElm.style.scrollBehavior = '';
+            } // if
         } // if
     });
     
