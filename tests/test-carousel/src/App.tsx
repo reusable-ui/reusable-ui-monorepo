00ada14809e59be13342f60ff62e6bffd33801a5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-26 12:08:45 +0700

    updated tests

--- tests/test-carousel/src/App.tsx
+++ tests/test-carousel/src/App.tsx
@@ -38,6 +38,16 @@ function App() {
                         Trigger re-render whole app
                     </button>
                 </article>
+                <Carousel theme='primary' style={style} infiniteLoop={false}>
+                    <img alt='lorem image' src='/images/lorem-img/waves-800x600.jpg' />
+                    <img alt='lorem image' src='/images/lorem-img/leaf-800x700.jpg' />
+                    <img alt='lorem image' src='/images/lorem-img/building-800x500.jpg' />
+                    <img alt='lorem image' src='/images/lorem-img/street-800x800.jpg' />
+                    <img alt='lorem image' src='/images/lorem-img/flower-700x400.jpg' />
+                    <img alt='lorem image' src='/images/lorem-img/water-500x800.jpg' />
+                    <img alt='lorem image' src='/images/lorem-img/wood-700x600.jpg' />
+                </Carousel>
+                <br />
                 <Carousel theme='primary' style={style} infiniteLoop={true}>
                     <img alt='lorem image' src='/images/lorem-img/waves-800x600.jpg' />
                     <img alt='lorem image' src='/images/lorem-img/leaf-800x700.jpg' />
