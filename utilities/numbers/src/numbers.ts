321e1665cb7da71c4d5bbccfafe3542a389e02f8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-05-31 01:22:58 +0700

    added clamp()

--- utilities/numbers/src/numbers.ts
+++ utilities/numbers/src/numbers.ts
@@ -28,3 +28,37 @@ export const decimalify = <TNumber extends number|null|undefined>(number: TNumbe
     const roundedNumber = Math.round(number * 1000000000000000) / 1000000000000000;
     return Number.parseFloat(roundedNumber.toFixed(11)) as TNumber;
 };
+
+export const clamp = (min: number, value: number, max: number, step?: number): number => {
+    // defaults:
+    min  ??= Number.MIN_SAFE_INTEGER;
+    max  ??= Number.MAX_SAFE_INTEGER;
+    step ??= 0;
+    const negative : boolean = (max < min);
+    
+    
+    
+    // make sure the requested value is between the min value & max value:
+    value     = Math.min(Math.max(
+        value
+    , (negative ? max : min)), (negative ? min : max));
+    
+    // if step was specified => stepping the value starting from min value:
+    if (step > 0) {
+        let steps    = Math.round((value - min) / step); // get the_nearest_stepped_value
+        
+        // make sure the_nearest_stepped_value is not exceeded the max value:
+        let maxSteps = (max - min) / step;
+        maxSteps     = negative ? Math.ceil(maxSteps) : Math.floor(maxSteps); // remove the decimal fraction
+        
+        // re-align the steps:
+        steps        = negative ? Math.max(steps, maxSteps) : Math.min(steps, maxSteps);
+        
+        // calculate the new value:
+        value        = min + (steps * step);
+    } // if
+    
+    
+    
+    return decimalify(value);
+};
