47b3fd4422a77ee88e996d8b3e661f214e861016
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-06-05 11:21:05 +0700

    updated performScrolling()

--- composite-components/carousel/src/Carousel.tsx
+++ composite-components/carousel/src/Carousel.tsx
@@ -524,9 +524,9 @@ const Carousel = <TElement extends HTMLElement = HTMLElement, TScrollIndexChange
     };
     const performScrolling                = (currentItemIndex: number|undefined, futureItemIndex: number): void => {
         // conditions:
-        const minItemIndex = 0;
-        const maxItemIndex = (itemsCount - 1);
-        if ((futureItemIndex < minItemIndex) || (futureItemIndex > maxItemIndex)) return; // out of range => ignore
+        const minMovementItemIndex = 0;
+        const maxMovementItemIndex = (itemsCount - 1);
+        if ((futureItemIndex < minMovementItemIndex) || (futureItemIndex > maxMovementItemIndex)) return; // out of movement range => ignore
         
         if ((currentItemIndex !== undefined) && (currentItemIndex === futureItemIndex)) return; // no diff => ignore
         
