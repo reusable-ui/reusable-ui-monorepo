27112aebf1135105b8be6b338a4cc9f1a7d150b5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-06-09 23:12:11 +0700

    updated styles

--- composite-components/carousel/src/styles/styles.ts
+++ composite-components/carousel/src/styles/styles.ts
@@ -119,12 +119,11 @@ export const usesCarouselListLayout = (options?: ContentChildrenMediaOptions) =>
             // overscrollBehavior      : 'none',        // do not forward scroll action to the parent element
             
             // buggy on Chrome:
-         // touchAction    : 'pan-y pinch-zoom',        // no swipeLeft/swipeRight handling, we use our scroll implementation
+            touchAction    : 'pan-y pinch-zoom',        // no swipeLeft/swipeRight handling, we use our scroll implementation
             // workaround for the buggy with similar behavior:
-            touchAction    : 'pinch-zoom',
-            pointerEvents  : 'none',       // prevents  user interaction (the interaction will be bubbled to <parent> element)
+            pointerEvents  : 'none',                    // prevents  user interaction (the interaction will be bubbled to <parent> element)
             ...children(itemElm, {
-                pointerEvents : 'initial', // preserves user interaction // cancel out *inherited* `pointerEvents`
+                pointerEvents : 'initial',              // preserves user interaction // cancel out *inherited* `pointerEvents`
             }),
             
             
