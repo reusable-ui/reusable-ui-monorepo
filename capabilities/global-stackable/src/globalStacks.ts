fcd3eb285bbd55b099f2b21b19d4054bc44ef336
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-08 10:36:52 +0700

    updated global-stackable

--- capabilities/global-stackable/src/globalStacks.ts
+++ capabilities/global-stackable/src/globalStacks.ts
@@ -7,24 +7,24 @@ import type {
 
 
 export type GlobalStacks = {
-    dropdown      : CssKnownProps['zIndex'],
-    
     sticky        : CssKnownProps['zIndex'],
     fixed         : CssKnownProps['zIndex'],
     
     modalBackdrop : CssKnownProps['zIndex'],
     modalDialog   : CssKnownProps['zIndex'],
     
+    dropdown      : CssKnownProps['zIndex'],
+    
     tooltip       : CssKnownProps['zIndex'],
 }
 export const globalStacks : GlobalStacks = {
-    dropdown      : 1000,
+    sticky        : 1020, // a <Modal> should above (covering) the <Navbar>
+    fixed         : 1030, // a <Modal> should above (covering) the <Navbar>
     
-    sticky        : 1020,
-    fixed         : 1030,
+    modalBackdrop : 1050, // a <ModalBackdrop> is above (on the top) of <MainPage> that may contain <Navbar>, so placed a higher position than <Navbar>
+    modalDialog   : 1055, // a <ModalDialog> is above (on the top) of <ModalBackdrop>
     
-    modalBackdrop : 1050,
-    modalDialog   : 1055,
+    dropdown      : 1060, // a <Dropdown> may inside (on the top) of <Modal> and may contain nested <Component>s including <Tooltip>, so placed it between <Modal> and <Tooltip>
     
-    tooltip       : 1080,
+    tooltip       : 1080, // [<Popup>, <Badge>, <Busy>, <Alert>, <Tooltip>] are rarely contain <Dropdown>, so placed it at highest position
 };
