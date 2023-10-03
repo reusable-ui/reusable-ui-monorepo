90fe8058e3ef8261d09ba93054c32eb2071a9898
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-03 12:36:07 +0700

    updated ExclusiveAccordion

--- composite-components/exclusive-accordion/src/ExclusiveAccordion.tsx
+++ composite-components/exclusive-accordion/src/ExclusiveAccordion.tsx
@@ -2,10 +2,20 @@
 import {
     // react:
     default as React,
+    
+    
+    
+    // hooks:
+    useMemo,
 }                           from 'react'
 
 // reusable-ui core:
-import type {
+import {
+    // a set of React node utility functions:
+    flattenChildren,
+    
+    
+    
     // a capability of UI to expand/reduce its size or toggle the visibility:
     ExpandedChangeEvent,
 }                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
@@ -94,6 +104,57 @@ const ExclusiveAccordion = <TElement extends Element = HTMLElement, TExclusiveEx
     
     
     
+    // children:
+    const accordionComponentChildren = accordionComponent.props.children;
+    const mutatedChildren = useMemo<React.ReactNode|React.ReactNode[]>(() => {
+        return accordionComponentChildren ?? flattenChildren(children).map<React.ReactNode>((accordionItem, listIndex) => {
+            // conditions:
+            if (!React.isValidElement<AccordionItemProps<Element, ExpandedChangeEvent>>(accordionItem)) return accordionItem; // not an <AccordionItem> => place it anyway
+            
+            
+            
+            // props:
+            const accordionItemProps = accordionItem.props;
+            
+            
+            
+            // jsx:
+            return (
+                <AccordionItemWithState<Element, TExclusiveExpandedChangeEvent>
+                    // other props:
+                    {...accordionItemProps} // steals all accordionItem's props, so the <Owner> can recognize the <AccordionItemWithState> as <TheirChild>
+                    
+                    
+                    
+                    // positions:
+                    listIndex={listIndex}
+                    
+                    
+                    
+                    // components:
+                    accordionItemComponent={
+                        // clone accordionItem element with (almost) blank props:
+                        <accordionItem.type
+                            // identifiers:
+                            key={accordionItem.key}
+                            
+                            
+                            
+                            //#region restore conflicting props
+                            {...{
+                                ...(('listIndex'              in accordionItemProps) ? { listIndex              : accordionItemProps.listIndex              } : undefined),
+                                ...(('accordionItemComponent' in accordionItemProps) ? { accordionItemComponent : accordionItemProps.accordionItemComponent } : undefined),
+                            }}
+                            //#endregion restore conflicting props
+                        />
+                    }
+                />
+            );
+        });
+    }, [accordionComponentChildren, children]);
+    
+    
+    
     // jsx:
     return (
         <ExclusiveAccordionStateProvider<TExclusiveExpandedChangeEvent>
@@ -114,50 +175,7 @@ const ExclusiveAccordion = <TElement extends Element = HTMLElement, TExclusiveEx
                 
                 
                 // children:
-                React.Children.map<React.ReactNode, React.ReactNode>(accordionComponent.props.children ?? children, (accordionItem, listIndex) => {
-                    // conditions:
-                    if (!React.isValidElement<AccordionItemProps<Element, ExpandedChangeEvent>>(accordionItem)) return accordionItem; // not an <AccordionItem> => ignore
-                    
-                    
-                    
-                    // props:
-                    const accordionItemProps = accordionItem.props;
-                    
-                    
-                    
-                    // jsx:
-                    return (
-                        <AccordionItemWithState<Element, TExclusiveExpandedChangeEvent>
-                            // other props:
-                            {...accordionItemProps} // steals all accordionItem's props, so the <Owner> can recognize the <AccordionItemWithState> as <TheirChild>
-                            
-                            
-                            
-                            // positions:
-                            listIndex={listIndex}
-                            
-                            
-                            
-                            // components:
-                            accordionItemComponent={
-                                // clone accordionItem element with (almost) blank props:
-                                <accordionItem.type
-                                    // identifiers:
-                                    key={accordionItem.key}
-                                    
-                                    
-                                    
-                                    //#region restore conflicting props
-                                    {...{
-                                        ...(('listIndex'              in accordionItemProps) ? { listIndex              : accordionItemProps.listIndex              } : undefined),
-                                        ...(('accordionItemComponent' in accordionItemProps) ? { accordionItemComponent : accordionItemProps.accordionItemComponent } : undefined),
-                                    }}
-                                    //#endregion restore conflicting props
-                                />
-                            }
-                        />
-                    );
-                }),
+                mutatedChildren,
             )}
         </ExclusiveAccordionStateProvider>
     );
