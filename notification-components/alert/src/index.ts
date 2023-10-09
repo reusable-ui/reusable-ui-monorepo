22e08105004e190eb7ece470f958239df61c7d7d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-09 16:21:15 +0700

    updated Alert

--- notification-components/alert/src/index.ts
+++ notification-components/alert/src/index.ts
@@ -1,4 +1,4 @@
-// export *            from './styles/elements.js' // private only, not for (bare) export
+// export *            from './styles/elements.js' // not implicitly exported
 export *            from './styles/config.js'
 export *            from './styles/styles.js'
 export *            from './Alert.js'
