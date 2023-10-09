b76491ec5df63290ee611a5ce6c93602ff538717
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-09 16:24:47 +0700

    updated Carousel

--- composite-components/carousel/src/index.ts
+++ composite-components/carousel/src/index.ts
@@ -1,7 +1,7 @@
 export *            from './features/carousel.js'
 export *            from './variants/CarouselVariant.js'
 
-// export *            from './styles/elements.js' // private only, not for (bare) export
+// export *            from './styles/elements.js' // not implicitly exported
 export *            from './styles/config.js'
 export *            from './styles/styles.js'
 export *            from './Carousel.js'
