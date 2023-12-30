1fa2c55ab05e30fd7ea1cf85d29e545595520cd7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-30 12:02:09 +0700

    updated TData constraint

--- utility-components/dialog-message/src/DialogMessageContext.tsx
+++ utility-components/dialog-message/src/DialogMessageContext.tsx
@@ -45,25 +45,25 @@ const notNestedError = async (): Promise<any|undefined> => {
 // contexts:
 export interface DialogMessageApi {
     // dialogs:
-    showDialog              <TData extends any = 'ok'>(dialogComponent           : React.ReactComponentElement<any, ModalBaseProps<Element, ModalExpandedChangeWithAnswerEvent<TData>>>): Promise<TData|undefined>
+    showDialog              <TData extends keyof any = any >(dialogComponent           : React.ReactComponentElement<any, ModalBaseProps<Element, ModalExpandedChangeWithAnswerEvent<TData>>>): Promise<TData|undefined>
     
-    showMessage             <TData extends any = 'ok'>(dialogMessage             : DialogMessage<TData>                                                                                ): Promise<TData|undefined>
-    showMessage             <TData extends any = 'ok'>(message                   : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
+    showMessage             <TData extends keyof any = 'ok'>(dialogMessage             : DialogMessage<TData>                                                                                ): Promise<TData|undefined>
+    showMessage             <TData extends keyof any = 'ok'>(message                   : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
     
-    showMessageError        <TData extends any = 'ok'>(dialogMessageError        : DialogMessageError<TData>                                                                           ): Promise<TData|undefined>
-    showMessageError        <TData extends any = 'ok'>(error                     : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
+    showMessageError        <TData extends keyof any = 'ok'>(dialogMessageError        : DialogMessageError<TData>                                                                           ): Promise<TData|undefined>
+    showMessageError        <TData extends keyof any = 'ok'>(error                     : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
     
-    showMessageFieldError   <TData extends any = 'ok'>(dialogMessageFieldError   : DialogMessageFieldError<TData>                                                                      ): Promise<TData|undefined>
-    showMessageFieldError   <TData extends any = 'ok'>(fieldErrors               : FieldErrorList                                                 , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
+    showMessageFieldError   <TData extends keyof any = 'ok'>(dialogMessageFieldError   : DialogMessageFieldError<TData>                                                                      ): Promise<TData|undefined>
+    showMessageFieldError   <TData extends keyof any = 'ok'>(fieldErrors               : FieldErrorList                                                 , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
     
-    showMessageFetchError   <TData extends any = 'ok'>(dialogMessageFetchError   : DialogMessageFetchError<TData>                                                                      ): Promise<TData|undefined>
-    showMessageFetchError   <TData extends any = 'ok'>(fetchError                : any                                                            , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
+    showMessageFetchError   <TData extends keyof any = 'ok'>(dialogMessageFetchError   : DialogMessageFetchError<TData>                                                                      ): Promise<TData|undefined>
+    showMessageFetchError   <TData extends keyof any = 'ok'>(fetchError                : any                                                            , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
     
-    showMessageSuccess      <TData extends any = 'ok'>(dialogMessageSuccess      : DialogMessageSuccess<TData>                                                                         ): Promise<TData|undefined>
-    showMessageSuccess      <TData extends any = 'ok'>(success                   : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
+    showMessageSuccess      <TData extends keyof any = 'ok'>(dialogMessageSuccess      : DialogMessageSuccess<TData>                                                                         ): Promise<TData|undefined>
+    showMessageSuccess      <TData extends keyof any = 'ok'>(success                   : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
     
-    showMessageNotification <TData extends any = 'ok'>(dialogMessageNotification : DialogMessageNotification<TData>                                                                    ): Promise<TData|undefined>
-    showMessageNotification <TData extends any = 'ok'>(notification              : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
+    showMessageNotification <TData extends keyof any = 'ok'>(dialogMessageNotification : DialogMessageNotification<TData>                                                                    ): Promise<TData|undefined>
+    showMessageNotification <TData extends keyof any = 'ok'>(notification              : React.ReactNode                                                , options?: ShowMessageOptions<TData>): Promise<TData|undefined>
 }
 export const DialogMessageContext = createContext<DialogMessageApi>({
     // dialogs:
