cbc10c748483ef3fd5d9799a7ec1d0d9ca53b7b8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-04-29 17:01:33 +0700

    refactor ElementWithAutoActive to simplify props handling and improve JSX structure

--- utilities/navigations/src/ElementWithAutoActive.tsx
+++ utilities/navigations/src/ElementWithAutoActive.tsx
@@ -49,83 +49,30 @@ const ElementWithAutoActive = (props: ElementWithAutoActiveProps): JSX.Element|n
         
         // components:
         elementComponent,
-    ...restElementProps} = props;
-    
-    
-    
-    // props:
-    const elementComponentProps : typeof elementComponent.props = {
-        // other props:
-        ...restElementProps,
-        ...elementComponent.props, // overwrites restElementProps (if any conflics)
-    };
+    } = props;
     
     
     
     // jsx:
-    const elementWithAutoActiveImpl = (
-        <ElementWithAutoActiveImpl
-            // other props:
-            {...elementComponentProps} // steals all elementComponent's props, so the <Owner> can recognize the <ElementWithAutoActiveImpl> as <TheirChild>
-            
-            
-            
-            // navigations:
-            caseSensitive={caseSensitive}
-            end={end}
-            
-            
-            
-            // components:
-            elementComponent={ // the underlying `<Element>` to be manipulated of `[active]` & `[aria-current]` props, based on the current page url
-                // clone elementComponent element with (almost) blank props:
-                <elementComponent.type
-                    // identifiers:
-                    key={elementComponent.key}
-                    
-                    
-                    
-                    //#region restore conflicting props
-                    {...{
-                        ...(('caseSensitive'    in elementComponentProps) ? { caseSensitive    : elementComponentProps.caseSensitive    } : undefined),
-                        ...(('end'              in elementComponentProps) ? { end              : elementComponentProps.end              } : undefined),
-                        ...(('elementComponent' in elementComponentProps) ? { elementComponent : elementComponentProps.elementComponent } : undefined),
-                        ...(('childrenOrigin'   in elementComponentProps) ? { childrenOrigin   : elementComponentProps.childrenOrigin   } : undefined),
-                    }}
-                    //#endregion restore conflicting props
-                />
-            }
-            
-            
-            
-            // children:
-            /* detect for `<Link>` component for `[to]`/`[href]` prop, for determining the current page */
-            childrenOrigin={elementComponentProps.children} // copy the children before will be mutated by another <SomeWithSomething>
-        />
-    );
-    const elementWithAutoActiveImplProps = elementWithAutoActiveImpl.props;
-    
     return (
         <ElementWithMaybeLink
-            // other props:
-            {...elementWithAutoActiveImplProps} // steals all elementWithAutoActiveImpl's props, so the <Owner> can recognize the <ElementWithMaybeLink> as <TheirChild>
-            
-            
-            
             // components:
-            elementComponent={ // the underlying `<Element>` to be `<Link>`-ed
-                // clone elementWithAutoActiveImpl element with (almost) blank props:
-                <elementWithAutoActiveImpl.type
-                    // identifiers:
-                    key={elementWithAutoActiveImpl.key}
+            elementComponent={
+                <ElementWithAutoActiveImpl
+                    // navigations:
+                    caseSensitive={caseSensitive}
+                    end={end}
                     
                     
                     
-                    //#region restore conflicting props
-                    {...{
-                        ...(('elementComponent' in elementWithAutoActiveImplProps) ? { elementComponent : elementWithAutoActiveImplProps.elementComponent } : undefined),
-                    }}
-                    //#endregion restore conflicting props
+                    // components:
+                    elementComponent={elementComponent}
+                    
+                    
+                    
+                    // children:
+                    /* detect for `<Link>` component for `[to]`/`[href]` prop, for determining the current page */
+                    childrenOrigin={elementComponent.props.children} // copy the children before will be mutated by another <SomeWithSomething>
                 />
             }
         />
