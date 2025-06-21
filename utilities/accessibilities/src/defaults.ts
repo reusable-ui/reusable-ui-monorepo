15a269494b49b17f027c378ac22b0b05ce4dff56
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-06-22 03:30:30 +0700

    refactor(defaults): add type annotations for default accessibility constants

--- utilities/accessibilities/src/defaults.ts
+++ utilities/accessibilities/src/defaults.ts
@@ -1,31 +1,31 @@
 /**
  * The default enabled state when `enabled` is not provided.
  */
-export const DEFAULT_ENABLED           = true
+export const DEFAULT_ENABLED           : boolean = true
 
 /**
  * The default read-only state when `readOnly` is not provided.
  */
-export const DEFAULT_READ_ONLY         = false
+export const DEFAULT_READ_ONLY         : boolean = false
 
 /**
  * The default active state when `active` is not provided.
  */
-export const DEFAULT_ACTIVE            = false
+export const DEFAULT_ACTIVE            : boolean = false
 
 
 
 /**
  * The default cascading behavior for `enabled` when `cascadeEnabled` is not provided.
  */
-export const DEFAULT_CASCADE_ENABLED   = true
+export const DEFAULT_CASCADE_ENABLED   : boolean = true
 
 /**
  * The default cascading behavior for `readOnly` when `cascadeReadOnly` is not provided.
  */
-export const DEFAULT_CASCADE_READ_ONLY = true
+export const DEFAULT_CASCADE_READ_ONLY : boolean = true
 
 /**
  * The default cascading behavior for `active` when `cascadeActive` is not provided.
  */
-export const DEFAULT_CASCADE_ACTIVE    = false
+export const DEFAULT_CASCADE_ACTIVE    : boolean = false
