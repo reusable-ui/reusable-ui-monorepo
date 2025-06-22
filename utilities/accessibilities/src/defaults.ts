8e95fd7ff41c5bd73b03ce280cccc69cda167eff
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-06-23 02:20:03 +0700

    refactor(accessibilities): improve comments for default state values for clarity and consistency

--- utilities/accessibilities/src/defaults.ts
+++ utilities/accessibilities/src/defaults.ts
@@ -1,31 +1,46 @@
 /**
- * The default enabled state when `enabled` is not provided.
+ * Default value for `enabled` state when not explicitly defined.
+ * 
+ * Indicates that the component is enabled by default, unless overridden.
  */
 export const DEFAULT_ENABLED           : boolean = true
 
 /**
- * The default read-only state when `readOnly` is not provided.
+ * Default value for `readOnly` state when not explicitly defined.
+ * 
+ * Indicates that the component is editable by default, unless overridden.
  */
 export const DEFAULT_READ_ONLY         : boolean = false
 
 /**
- * The default active state when `active` is not provided.
+ * Default value for `active` state when not explicitly defined.
+ * 
+ * Indicates that the component is inactive by default, unless overridden.
  */
 export const DEFAULT_ACTIVE            : boolean = false
 
 
 
 /**
- * The default cascading behavior for `enabled` when `cascadeEnabled` is not provided.
+ * The default cascading behavior for `enabled` state when `cascadeEnabled` is not provided.
+ * 
+ * Enables cascading disabled state behavior by default,
+ * allowing ancestor disabled state settings to propagate down the component tree.
  */
 export const DEFAULT_CASCADE_ENABLED   : boolean = true
 
 /**
- * The default cascading behavior for `readOnly` when `cascadeReadOnly` is not provided.
+ * The default cascading behavior for `readOnly` state when `cascadeReadOnly` is not provided.
+ * 
+ * Enables cascading read-only state (prevents user editing) behavior by default,
+ * allowing ancestor read-only state settings to propagate down the component tree.
  */
 export const DEFAULT_CASCADE_READ_ONLY : boolean = true
 
 /**
- * The default cascading behavior for `active` when `cascadeActive` is not provided.
+ * The default cascading behavior for `active` state when `cascadeActive` is not provided.
+ * 
+ * Disables cascading active state (e.g., checked, selected, toggled, or chosen) behavior by default,
+ * prevents ancestor active state settings to propagate down the component tree.
  */
 export const DEFAULT_CASCADE_ACTIVE    : boolean = false
