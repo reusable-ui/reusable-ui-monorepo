6475797a21285003a53eb0eead0236db8d2794fc
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-02-04 20:41:34 +0700

    chore(links): remove node-fetch and unused jest setup

--- routes/links/jest.config.js
+++ routes/links/jest.config.js
@@ -2,5 +2,4 @@ import jestRoot from '../../jest.config.js'
 export default {
     ...jestRoot,
     testEnvironment: 'jsdom', // Required for testing React hooks
-    setupFiles: ['./jest.setup.js'],
 }
--- routes/links/jest.setup.js
+++
@@ -1,6 +0,0 @@
-import { TextEncoder, TextDecoder } from 'util';
-import { Request } from 'node-fetch'
-
-globalThis.TextEncoder = TextEncoder;
-globalThis.TextDecoder = TextDecoder;
-globalThis.Request     = globalThis.Request ?? Request;
