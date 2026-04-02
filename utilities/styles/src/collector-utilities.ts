4fc9c54b9398f83139736fd9192ca7ae57f9a229
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-04-02 15:59:14 +0700

    fix(collector-utilities): update unused type parameter with underscore

--- utilities/styles/src/collector-utilities.ts
+++ utilities/styles/src/collector-utilities.ts
@@ -21,7 +21,7 @@ const hasOwn = {}.hasOwnProperty;
 /**
  * Generic collector interface for accumulating values of type `TCollected`.
  */
-interface Collector<TCollected, TValue> {
+interface Collector<_TCollected, TValue> {
     /**
      * Appends a value to the collector.
      * @param value - The value to add to the collection.
