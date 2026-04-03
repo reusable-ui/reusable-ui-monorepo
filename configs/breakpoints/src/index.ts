b2164fba30fcba68cd2a0f8147036b7f62fab770
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-04-04 03:44:11 +0700

    refactor(breakpoints): move css selectors to `css-selectors` module

--- configs/breakpoints/src/index.ts
+++ configs/breakpoints/src/index.ts
@@ -1,7 +1,7 @@
 export *            from './types.js'
 export *            from './utilities.js'
 
+export *            from './css-selectors.js'
+
 export *            from './breakpoints.js'
 export { default }  from './breakpoints.js'
-
-export *            from './declarators.js'
