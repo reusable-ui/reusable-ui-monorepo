9ddd1e61e854a5935c128bfe72e7a0faf62b5613
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-04-15 20:02:13 +0700

    fix(colors): add eslint comment to suppress CSS variable convention

--- configs/colors/src/color-params.ts
+++ configs/colors/src/color-params.ts
@@ -232,7 +232,7 @@ const config = cssConfig(() => {
  * }
  * ```
  */
-export const colorParamVars        = config[0];
+export const colorParamVars        = config[0]; // eslint-disable-line css-variables/enforce-variable-conventions
 
 /**
  * A `Vals<>` object represents **structured CSS expressions**, allowing direct retrieval and modification.
