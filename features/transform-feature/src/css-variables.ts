80e6e240f665911a644e6787edc4d205851f6350
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 04:43:04 +0700

    refactor(transform-feature): update transform feature prefix using centralized default

--- features/transform-feature/src/css-variables.ts
+++ features/transform-feature/src/css-variables.ts
@@ -4,6 +4,11 @@ import {
     cssVars,
 }                           from '@cssfn/core'                  // Writes css in javascript.
 
+// Reusable-ui defaults:
+import {
+    defaultTransformFeaturePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Types:
 import {
     type TransformFeatureVars,
@@ -17,4 +22,4 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [transformFeatureVars] = cssVars<TransformFeatureVars>({ prefix: 'tm', minify: false });
+export const [transformFeatureVars] = cssVars<TransformFeatureVars>({ prefix: defaultTransformFeaturePrefix, minify: false });
