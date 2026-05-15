d34b2556148cb4f9e8476b354437a4986be5df8e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 03:17:47 +0700

    refactor(theme-variant): update theme variant prefix using centralized default

--- variants/theme-variant/src/css-variables.ts
+++ variants/theme-variant/src/css-variables.ts
@@ -4,6 +4,11 @@ import {
     cssVars,
 }                           from '@cssfn/core'          // Writes css in javascript.
 
+// Reusable-ui defaults:
+import {
+    defaultThemeVariantPrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Types:
 import {
     type ThemeVariantVars,
@@ -19,4 +24,4 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [themeVariantVars] = cssVars<ThemeVariantVars>({ prefix: 't', minify: false });
+export const [themeVariantVars] = cssVars<ThemeVariantVars>({ prefix: defaultThemeVariantPrefix, minify: false });
