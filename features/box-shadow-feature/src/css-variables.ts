8ab445de921403d235cba9d44e70a80ce3201ecd
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 04:45:30 +0700

    refactor(box-shadow-feature): update box shadow feature prefix using centralized default

--- features/box-shadow-feature/src/css-variables.ts
+++ features/box-shadow-feature/src/css-variables.ts
@@ -4,6 +4,11 @@ import {
     cssVars,
 }                           from '@cssfn/core'                  // Writes css in javascript.
 
+// Reusable-ui defaults:
+import {
+    defaultBoxShadowFeaturePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Types:
 import {
     type BoxShadowFeatureVars,
@@ -17,4 +22,4 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [boxShadowFeatureVars] = cssVars<BoxShadowFeatureVars>({ prefix: 'bw', minify: false });
+export const [boxShadowFeatureVars] = cssVars<BoxShadowFeatureVars>({ prefix: defaultBoxShadowFeaturePrefix, minify: false });
