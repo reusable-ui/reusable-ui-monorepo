d537ca398a3e859b5e10db376741854ca2481b9d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 04:24:33 +0700

    refactor(foreground-feature): update foreground feature prefix using centralized default

--- features/foreground-feature/src/css-variables.ts
+++ features/foreground-feature/src/css-variables.ts
@@ -4,6 +4,11 @@ import {
     cssVars,
 }                           from '@cssfn/core'          // Writes css in javascript.
 
+// Reusable-ui defaults:
+import {
+    defaultForegroundFeaturePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Types:
 import {
     type ForegroundFeatureVars,
@@ -17,4 +22,4 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [foregroundFeatureVars] = cssVars<ForegroundFeatureVars>({ prefix: 'fg', minify: false });
+export const [foregroundFeatureVars] = cssVars<ForegroundFeatureVars>({ prefix: defaultForegroundFeaturePrefix, minify: false });
