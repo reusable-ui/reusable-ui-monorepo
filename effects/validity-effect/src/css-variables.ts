5829d1098a0d5986ffa63aeed483faa86f5adf93
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 14:02:52 +0700

    refactor(validity-effect): update validity effect prefix using centralized default

--- effects/validity-effect/src/css-variables.ts
+++ effects/validity-effect/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type ValidityEffectVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultValidityEffectPrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 
 
 /**
@@ -17,4 +22,4 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [validityEffectVars] = cssVars<ValidityEffectVars>({ prefix: 'vae', minify: false });
+export const [validityEffectVars] = cssVars<ValidityEffectVars>({ prefix: defaultValidityEffectPrefix, minify: false });
