c4c43b6db3aeaf540d6ca7674db03476d5755f23
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 14:16:14 +0700

    refactor(view-effect): update view effect prefix using centralized default

--- effects/view-effect/src/css-variables.ts
+++ effects/view-effect/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type ViewEffectVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultViewEffectPrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     transformRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [viewEffectVars] = cssVars<ViewEffectVars>({ prefix: 'vie', minify: false });
+export const [viewEffectVars] = cssVars<ViewEffectVars>({ prefix: defaultViewEffectPrefix, minify: false });
 
 // Register the view transform globally for composing a unified transform stack across effect packages:
 transformRegistry.registerTransform(viewEffectVars.viewTransform);
