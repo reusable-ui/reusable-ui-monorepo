cb1fe4b953bc181d21db19f014b6643a84eb5522
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 13:47:15 +0700

    refactor(view-state): update view state prefix using centralized default

--- states/view-state/src/css-variables.ts
+++ states/view-state/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type ViewStateVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultViewStatePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     animationRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [viewStateVars] = cssVars<ViewStateVars>({ prefix: 'vi', minify: false });
+export const [viewStateVars] = cssVars<ViewStateVars>({ prefix: defaultViewStatePrefix, minify: false });
 
 // Register the view-related animations globally for composing a unified animation stack across state packages:
 animationRegistry.registerAnimation(viewStateVars.animationViewAdvancing);
