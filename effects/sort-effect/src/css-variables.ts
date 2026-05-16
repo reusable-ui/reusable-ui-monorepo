439dde2b4e3fc9e52edefe0d406778b5434d7a18
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 14:07:26 +0700

    refactor(sort-effect): update sort effect prefix using centralized default

--- effects/sort-effect/src/css-variables.ts
+++ effects/sort-effect/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type SortEffectVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultSortEffectPrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     transformRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [sortEffectVars] = cssVars<SortEffectVars>({ prefix: 'soe', minify: false });
+export const [sortEffectVars] = cssVars<SortEffectVars>({ prefix: defaultSortEffectPrefix, minify: false });
 
 // Register the sort transform globally for composing a unified transform stack across effect packages:
 transformRegistry.registerTransform(sortEffectVars.sortTransform);
