713823fff51b9c1089793346bb223a64d80af8c7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 14:12:10 +0700

    refactor(collapse-effect): update collapse effect prefix using centralized default

--- effects/collapse-effect/src/css-variables.ts
+++ effects/collapse-effect/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type CollapseEffectVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultCollapseEffectPrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     transformRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [collapseEffectVars] = cssVars<CollapseEffectVars>({ prefix: 'coe', minify: false });
+export const [collapseEffectVars] = cssVars<CollapseEffectVars>({ prefix: defaultCollapseEffectPrefix, minify: false });
 
 // Register the overshoot transform globally for composing a unified transform stack across effect packages:
 transformRegistry.registerTransform(collapseEffectVars.overshootTransform);
