12fcd9623de71ab6909350821f8660ccc2a64224
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 13:29:18 +0700

    refactor(validity-state): update validity state prefix using centralized default

--- states/validity-state/src/css-variables.ts
+++ states/validity-state/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type ValidityStateVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultValidityStatePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     animationRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [validityStateVars] = cssVars<ValidityStateVars>({ prefix: 'va', minify: false });
+export const [validityStateVars] = cssVars<ValidityStateVars>({ prefix: defaultValidityStatePrefix, minify: false });
 
 // Register the validity-related animations globally for composing a unified animation stack across state packages:
 animationRegistry.registerAnimation(validityStateVars.animationValidating);
