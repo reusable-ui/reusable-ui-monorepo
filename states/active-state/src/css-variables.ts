784d5c5fa0f96151a8a3bb30b02ad35405259479
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 13:44:59 +0700

    refactor(active-state): update active state prefix using centralized default

--- states/active-state/src/css-variables.ts
+++ states/active-state/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type ActiveStateVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultActiveStatePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     animationRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [activeStateVars] = cssVars<ActiveStateVars>({ prefix: 'ac', minify: false });
+export const [activeStateVars] = cssVars<ActiveStateVars>({ prefix: defaultActiveStatePrefix, minify: false });
 
 // Register the activate/deactivate-related animations globally for composing a unified animation stack across state packages:
 animationRegistry.registerAnimation(activeStateVars.animationActivating);
