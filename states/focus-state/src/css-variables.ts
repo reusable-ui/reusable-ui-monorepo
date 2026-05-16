6eb7b9b60dd706e3ba85ef637ff62596318c1b18
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 13:20:03 +0700

    refactor(focus-state): update focus state prefix using centralized default

--- states/focus-state/src/css-variables.ts
+++ states/focus-state/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type FocusStateVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultFocusStatePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     animationRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [focusStateVars] = cssVars<FocusStateVars>({ prefix: 'fo', minify: false });
+export const [focusStateVars] = cssVars<FocusStateVars>({ prefix: defaultFocusStatePrefix, minify: false });
 
 // Register the focus/blur-related animations globally for composing a unified animation stack across state packages:
 animationRegistry.registerAnimation(focusStateVars.animationFocusing);
