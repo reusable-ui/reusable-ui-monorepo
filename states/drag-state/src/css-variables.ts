ee031cc5edc343c139ce182b39787be4514aa68f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-05-16 13:32:24 +0700

    refactor(drag-state): update drag state prefix using centralized default

--- states/drag-state/src/css-variables.ts
+++ states/drag-state/src/css-variables.ts
@@ -9,6 +9,11 @@ import {
     type DragStateVars,
 }                           from './types.js'
 
+// Reusable-ui defaults:
+import {
+    defaultDragStatePrefix,
+}                           from '@reusable-ui/css-prefix-default'  // A centralized default CSS variable prefixes across the Reusable-UI core system, ensuring unique, predictable, and consistent prefixes.
+
 // Reusable-ui features:
 import {
     animationRegistry,
@@ -22,7 +27,7 @@ import {
  * These variables are shared across server and client environments to ensure
  * consistent CSS variable names during SSR and hydration.
  */
-export const [dragStateVars] = cssVars<DragStateVars>({ prefix: 'dr', minify: false });
+export const [dragStateVars] = cssVars<DragStateVars>({ prefix: defaultDragStatePrefix, minify: false });
 
 // Register the drag/drop-related animations globally for composing a unified animation stack across state packages:
 animationRegistry.registerAnimation(dragStateVars.animationDragging);
