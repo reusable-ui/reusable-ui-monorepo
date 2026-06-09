fd71716ac6d4580ff73b5de2111bf6d3853e733d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-09 18:31:01 +0700

    refactor(ring-feature): rename module types → css-types

--- features/ring-feature/src/css-hooks.ts
+++ features/ring-feature/src/css-hooks.ts
@@ -14,7 +14,7 @@ import {
 import {
     type CssRingFeatureOptions,
     type CssRingFeature,
-}                           from './types.js'
+}                           from './css-types.js'
 
 // CSS Variables:
 import {
