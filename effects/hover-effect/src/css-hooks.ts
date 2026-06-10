44704d8500b449e8ba469a959364e891135251bb
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-10 13:41:33 +0700

    refactor(hover-effect): move css-related types from module types → css-types

--- effects/hover-effect/src/css-hooks.ts
+++ effects/hover-effect/src/css-hooks.ts
@@ -14,7 +14,7 @@ import {
 import {
     type CssHoverEffectOptions,
     type CssHoverEffect,
-}                           from './types.js'
+}                           from './css-types.js'
 
 // CSS Variables:
 import {
