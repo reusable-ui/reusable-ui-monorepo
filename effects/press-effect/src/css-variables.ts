8a973d7ffe3082dbd461efd8335844b3ac17f1fd
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-10 13:45:27 +0700

    refactor(press-effect): move css-related types from module types → css-types

--- effects/press-effect/src/css-variables.ts
+++ effects/press-effect/src/css-variables.ts
@@ -7,7 +7,7 @@ import {
 // Types:
 import {
     type PressEffectVars,
-}                           from './types.js'
+}                           from './css-types.js'
 
 // Reusable-ui defaults:
 import {
