cc83905ed16a9e9b1458abab8cb4eff2a761748c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-10 10:43:17 +0700

    refactor(press-state): move css-related types from module types → css-types

--- states/press-state/src/css-hooks.ts
+++ states/press-state/src/css-hooks.ts
@@ -2,7 +2,7 @@
 import {
     type CssPressStateOptions,
     type CssPressState,
-}                           from './types.js'
+}                           from './css-types.js'
 
 // CSS Variables:
 import {
--- states/press-state/src/css-variables.ts
+++ states/press-state/src/css-variables.ts
@@ -7,7 +7,7 @@ import {
 // Types:
 import {
     type PressStateVars,
-}                           from './types.js'
+}                           from './css-types.js'
 
 // Reusable-ui defaults:
 import {
