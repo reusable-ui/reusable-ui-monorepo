ce7658d057ae309496dc335300e1038af43618bf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-13 13:31:59 +0700

    refactor(disabled-state): rename module css-variables → css-internal-variables and update imports in related files

--- states/disabled-state/src/css-hooks.ts
+++ states/disabled-state/src/css-hooks.ts
@@ -7,7 +7,7 @@ import {
 // CSS Variables:
 import {
     disabledStateVars,
-}                           from './css-variables.js'
+}                           from './css-internal-variables.js'
 
 // CSS Selectors:
 import {
