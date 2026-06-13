9f44264def6fb69e788b19dfe1beaaa3bf5c80e2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-13 13:58:16 +0700

    refactor(disabled-effect): rename module css-variables → css-internal-variables and update imports in related files

--- effects/disabled-effect/src/css-hooks.ts
+++ effects/disabled-effect/src/css-hooks.ts
@@ -19,7 +19,7 @@ import {
 // CSS Variables:
 import {
     disabledEffectVars,
-}                           from './css-variables.js'
+}                           from './css-internal-variables.js'
 
 // Reusable-ui states:
 import {
