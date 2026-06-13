7e9bfe1f2bca3d042c0473764eb29d6a7409c9cf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-13 14:37:36 +0700

    refactor(focus-state): rename module focus-observer → internal-observer-hooks and update imports in related files

--- states/focus-state/src/focus-state.ts
+++ states/focus-state/src/focus-state.ts
@@ -27,7 +27,7 @@ import {
 // Hooks:
 import {
     useFocusObserverState,
-}                           from './focus-observer.js'
+}                           from './internal-observer-hooks.js'
 
 // Reusable-ui states:
 import {
