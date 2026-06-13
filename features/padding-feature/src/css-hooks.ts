89a7d32ea711d1755fc77c84463a0c87c1cf7b34
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-13 12:43:36 +0700

    refactor(padding-feature): rename module css-variables → css-internal-variables and update imports in related files

--- features/padding-feature/src/css-hooks.ts
+++ features/padding-feature/src/css-hooks.ts
@@ -19,7 +19,7 @@ import {
 // CSS Variables:
 import {
     paddingFeatureVars,
-}                           from './css-variables.js'
+}                           from './css-internal-variables.js'
 
 // Reusable-ui configs:
 import {
