fe7222c8c6fd6f3cfdee329b008f86458dda9555
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-13 13:48:13 +0700

    refactor(active-state): rename module css-variables → css-internal-variables and update imports in related files

--- states/active-state/src/css-hooks.ts
+++ states/active-state/src/css-hooks.ts
@@ -7,7 +7,7 @@ import {
 // CSS Variables:
 import {
     activeStateVars,
-}                           from './css-variables.js'
+}                           from './css-internal-variables.js'
 
 // CSS Selectors:
 import {
