fac8fd5cdb09346d6ef5022fb834862bda28413a
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-15 15:31:22 +0700

    refactor(bare-variant): move client-specific hooks to client-hooks for consistency

--- variants/bare-variant/__tests__/bare-variant.test.tsx
+++ variants/bare-variant/__tests__/bare-variant.test.tsx
@@ -14,7 +14,7 @@ import {
 } from '../dist/internal-defaults.js'
 import {
     useBareVariant,
-} from '../dist/bare-variant.js'
+} from '../dist/client-hooks.js'
 
 import { renderHook, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
