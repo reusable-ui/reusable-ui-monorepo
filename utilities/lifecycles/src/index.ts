cd054ac7f89ce36a73c8d52d89c709e15a77654b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2026-06-17 23:48:59 +0700

    refactor(lifecycles): remove useIsomorphicRef and related documentation as it has been moved to @reusable-ui/isomorphics

--- utilities/lifecycles/src/index.ts
+++ utilities/lifecycles/src/index.ts
@@ -1,2 +1,1 @@
-export * from './general-hooks.js'
 export * from './client-hooks.js'
--- utilities/lifecycles/src/internal-client-hooks.ts
+++
@@ -1,7 +0,0 @@
-'use client' // The exported hooks are client side only.
-
-// React:
-export {
-    // Hooks:
-    useRef as useOriginalReactRef,
-}                           from 'react'
